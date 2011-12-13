8f2a77b936e183952883810234f828213c2d6e4a
DavidWalz <walz@physik.rwth-aachen.de>
2011-12-13 09:42:07 +0100

    limit nex step in electron pair production

--- include/mpc/interaction/ElectronPairProduction.h
+++ include/mpc/interaction/ElectronPairProduction.h
@@ -9,12 +9,14 @@ namespace mpc {
 
 class ElectronPairProduction: public Module {
 private:
-	double y[69]; // tabulated electron pair production energy loss rate for protons in [J / m]
+	double y[69]; // tabulated electron pair production energy loss rate for protons in [J/m]
 	double x[69]; // tabulated energy in [J]
-	double dlx; // value of logarithmic spacing
+	double dlx; // (logarithmic) spacing
+	double maxRelLoss; // approximate maximum relative energy loss per step
 
 public:
-	ElectronPairProduction() {
+	ElectronPairProduction(double maxRelLoss) {
+		this->maxRelLoss = maxRelLoss;
 		// load energy loss rate table
 		std::ifstream infile("data/pair_rate_cmbir.table");
 		char str[256];
@@ -24,7 +26,7 @@ public:
 		for (int i = 0; i < 70; i++) {
 			infile >> a >> b;
 			x[i] = a * eV; // convert [eV] -> [J]
-			y[i] = b * eV / Mpc; // convert [eV / Mpc] -> [J / m]
+			y[i] = b * eV / Mpc; // convert [eV/Mpc] -> [J/m]
 		}
 		infile.close();
 		dlx = log10(x[69] / x[0]) / 69;
@@ -43,17 +45,21 @@ public:
 		// index of next lower energy bin
 		int i = floor(log10(E / x[0]) / dlx);
 
-		// energy loss for nucleus of mass M
-		// dE(E) = m_p / m * Z^2 * loss_rate(E) * step
 		// linear interpolation: y(x) = y0 + dy/dx * (x-x0)
 		double rate = y[i] + (y[i + 1] - y[i]) / (x[i + 1] - x[i]) * (E - x[i]);
+
+		// energy loss for nucleus of mass M
+		// dE(E) = m_p / m * Z^2 * loss_rate(E) * step
 		double dE = Z * Z * mass_proton / mass * rate * step;
 
 		std::cout << E / EeV << std::endl;
 		std::cout << dE / EeV << std::endl << std::endl;
 
-		// update particle
 		candidate->current.setEnergy(E - dE);
+
+		// try to keep fractional energy loss smaller than maxRelLoss
+		// nextStep / step * dE / E < maxRelLoss
+		candidate->limitNextStep(step * E / dE * maxRelLoss);
 	}
 };
 

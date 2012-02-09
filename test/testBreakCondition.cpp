22f72db6df9d288210504cfc4d833c58c075f7e3
DavidWalz <walz@physik.rwth-aachen.de>
2012-02-09 15:01:08 +0100

    changed boundaries

--- test/testBreakCondition.cpp
+++ test/testBreakCondition.cpp
@@ -4,58 +4,36 @@
 
 namespace mpc {
 
-TEST(testBreakCondition, MinimumEnergy) {
-	MinimumEnergy minEnergy(5 * EeV);
+TEST(testMinimumEnergy, Continue) {
+	MinimumEnergy minEnergy(5);
 	Candidate candidate;
-
-	candidate.setStatus(Candidate::Active);
-	candidate.current.setEnergy(5.1 * EeV);
+	candidate.current.setEnergy(5.1);
 	minEnergy.process(&candidate);
 	EXPECT_EQ(candidate.getStatus(), Candidate::Active);
+}
 
-	candidate.setStatus(Candidate::Active);
-	candidate.current.setEnergy(4.9 * EeV);
+TEST(testMinimumEnergy, Break) {
+	MinimumEnergy minEnergy(5);
+	Candidate candidate;
+	candidate.current.setEnergy(4.9);
 	minEnergy.process(&candidate);
 	EXPECT_EQ(candidate.getStatus(), Candidate::Stopped);
 }
 
-TEST(testBreakCondition, MaximumTrajectoryLength) {
-	MaximumTrajectoryLength maxLength(10 * Mpc);
+TEST(testMaximumTrajectoryLength, Continue) {
+	MaximumTrajectoryLength maxLength(10);
 	Candidate candidate;
-
-	candidate.setStatus(Candidate::Active);
-	candidate.setTrajectoryLength(9.9 * Mpc);
+	candidate.setTrajectoryLength(9.9);
 	maxLength.process(&candidate);
 	EXPECT_EQ(candidate.getStatus(), Candidate::Active);
-
-	candidate.setStatus(Candidate::Active);
-	candidate.setTrajectoryLength(10 * Mpc);
-	maxLength.process(&candidate);
-	EXPECT_EQ(candidate.getStatus(), Candidate::Stopped);
 }
 
-TEST(testBreakCondition, LargeObserverSphere) {
-	LargeObserverSphere obs(Vector3(0, 0, 0), 10);
+TEST(testMaximumTrajectoryLength, Break) {
+	MaximumTrajectoryLength maxLength(10);
 	Candidate candidate;
-
-	candidate.current.setPosition(Vector3(0, 0, 0));
-	obs.process(&candidate);
-	EXPECT_EQ(candidate.getStatus(), Candidate::Active);
-
-	candidate.setStatus(Candidate::Active);
-	candidate.current.setPosition(Vector3(10., 0, 0));
-	obs.process(&candidate);
-	EXPECT_EQ(candidate.getStatus(), Candidate::Detected);
-
-	candidate.setStatus(Candidate::Active);
-	candidate.current.setPosition(Vector3(0, -10., 0));
-	obs.process(&candidate);
-	EXPECT_EQ(candidate.getStatus(), Candidate::Detected);
-
-	candidate.setNextStep(10);
-	candidate.current.setPosition(Vector3(0, 0, 8));
-	obs.process(&candidate);
-	EXPECT_DOUBLE_EQ(candidate.getNextStep(), 2);
+	candidate.setTrajectoryLength(10.1);
+	maxLength.process(&candidate);
+	EXPECT_EQ(candidate.getStatus(), Candidate::Stopped);
 }
 
 TEST(testBreakCondition, SmallObserverSphere) {
@@ -77,32 +55,86 @@ TEST(testBreakCondition, SmallObserverSphere) {
 	EXPECT_DOUBLE_EQ(candidate.getNextStep(), 1);
 }
 
-TEST(testBreakCondition, SimulationBox) {
-	SimulationBox simBox(Vector3(0, 0, 0), 10, 1);
+TEST(testCubicBoundary, Inside) {
+	CubicBoundary cube(Vector3(0, 0, 0), 10, Candidate::Stopped);
 	Candidate candidate;
-
 	candidate.current.setPosition(Vector3(9, 5, 5));
-	simBox.process(&candidate);
+	cube.process(&candidate);
 	EXPECT_EQ(candidate.getStatus(), Candidate::Active);
+}
 
-	candidate.setStatus(Candidate::Active);
+TEST(testCubicBoundary, Above) {
+	CubicBoundary cube(Vector3(0, 0, 0), 10, Candidate::Stopped);
+	Candidate candidate;
 	candidate.current.setPosition(Vector3(10.1, 5, 5));
-	simBox.process(&candidate);
-	EXPECT_EQ(candidate.getStatus(), Candidate::OutOfBounds);
+	cube.process(&candidate);
+	EXPECT_EQ(candidate.getStatus(), Candidate::Stopped);
+}
 
-	candidate.setStatus(Candidate::Active);
-	candidate.current.setPosition(Vector3(5, -0.1, 1));
-	simBox.process(&candidate);
-	EXPECT_EQ(candidate.getStatus(), Candidate::OutOfBounds);
+TEST(testCubicBoundary, Below) {
+	CubicBoundary cube(Vector3(0, 0, 0), 10, Candidate::Stopped);
+	Candidate candidate;
+	candidate.current.setPosition(Vector3(5, -0.1, 5));
+	cube.process(&candidate);
+	EXPECT_EQ(candidate.getStatus(), Candidate::Stopped);
+}
 
-	candidate.setNextStep(10);
-	candidate.current.setPosition(Vector3(9.5, 5, 5));
-	simBox.process(&candidate);
-	EXPECT_DOUBLE_EQ(candidate.getNextStep(), 1.5);
-
+TEST(testCubicBoundary, LimitStep) {
+	CubicBoundary cube(Vector3(0, 0, 0), 10, 1, Candidate::Stopped);
+	Candidate candidate;
 	candidate.setNextStep(10);
 	candidate.current.setPosition(Vector3(5, 5, 0.5));
-	simBox.process(&candidate);
+	cube.process(&candidate);
+	EXPECT_DOUBLE_EQ(candidate.getNextStep(), 1.5);
+}
+
+TEST(testSphericalBoundary, Inside) {
+	SphericalBoundary sphere(Vector3(0, 0, 0), 10, Candidate::Stopped);
+	Candidate candidate;
+	candidate.current.setPosition(Vector3(9, 0, 0));
+	sphere.process(&candidate);
+	EXPECT_EQ(candidate.getStatus(), Candidate::Active);
+}
+
+TEST(testSphericalBoundary, Outside) {
+	SphericalBoundary sphere(Vector3(0, 0, 0), 10, Candidate::Stopped);
+	Candidate candidate;
+	candidate.current.setPosition(Vector3(0, -10.1, 0));
+	sphere.process(&candidate);
+	EXPECT_EQ(candidate.getStatus(), Candidate::Stopped);
+}
+
+TEST(testSphericalBoundary, LimitStep) {
+	SphericalBoundary sphere(Vector3(0, 0, 0), 10, 1, Candidate::Stopped);
+	Candidate candidate;
+	candidate.setNextStep(2);
+	candidate.current.setPosition(Vector3(0, 0, 9.5));
+	sphere.process(&candidate);
+	EXPECT_DOUBLE_EQ(candidate.getNextStep(), 1.5);
+}
+
+TEST(testEllipsoidalBoundary, Inside) {
+	EllipsoidalBoundary ellipsoid(Vector3(-5, 0, 0), Vector3(5, 0, 0), 15, Candidate::Stopped);
+	Candidate candidate;
+	candidate.current.setPosition(Vector3(3, 2, 0));
+	ellipsoid.process(&candidate);
+	EXPECT_EQ(candidate.getStatus(), Candidate::Active);
+}
+
+TEST(testEllipsoidalBoundary, Outside) {
+	EllipsoidalBoundary ellipsoid(Vector3(-5, 0, 0), Vector3(5, 0, 0), 15, Candidate::Stopped);
+	Candidate candidate;
+	candidate.current.setPosition(Vector3(0, 25, 0));
+	ellipsoid.process(&candidate);
+	EXPECT_EQ(candidate.getStatus(), Candidate::Stopped);
+}
+
+TEST(testEllipsoidalBoundary, LimitStep) {
+	EllipsoidalBoundary ellipsoid(Vector3(-5, 0, 0), Vector3(5, 0, 0), 15, 0.5, Candidate::Stopped);
+	Candidate candidate;
+	candidate.setNextStep(2);
+	candidate.current.setPosition(Vector3(7, 0, 0));
+	ellipsoid.process(&candidate);
 	EXPECT_DOUBLE_EQ(candidate.getNextStep(), 1.5);
 }
 

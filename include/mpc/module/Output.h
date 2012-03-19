66a5ae0efe584fa1ebb0678f07425c7fa93919f7
geromueller <post@geromueller.de>
2012-03-19 12:52:46 +0100

    refactor output

--- include/mpc/module/Output.h
+++ include/mpc/module/Output.h
@@ -2,27 +2,9 @@
 #define OUTPUT_H_
 
 #include "mpc/Module.h"
-#include <iostream>
-#include <iomanip>
 #include <fstream>
-#include <sstream>
 
 namespace mpc {
-
-std::string getOutputString(ParticleState particle) {
-	std::stringstream s;
-	s << particle.getId() << ", ";
-	s << particle.getEnergy() / EeV << ", ";
-	Vector3 pos = particle.getPosition() / Mpc;
-	s << pos.x() << ", ";
-	s << pos.y() << ", ";
-	s << pos.z() << ", ";
-	Vector3 dir = particle.getDirection();
-	s << dir.x() << ", ";
-	s << dir.y() << ", ";
-	s << dir.z();
-	return s.str();
-}
 
 /**
  @class TrajectoryOutput
@@ -33,24 +15,10 @@ private:
 	std::ofstream outfile;
 
 public:
-	TrajectoryOutput(std::string name) {
-		outfile.open(name.c_str());
-		outfile
-				<< "# Age, HepId, E, posX, posY, posZ, dirX, dirY, dirZ, event\n";
-	}
-
-	~TrajectoryOutput() {
-		outfile.close();
-	}
-
-	void process(Candidate *candidate) {
-		outfile << candidate->getTrajectoryLength() / Mpc << ", "
-				<< getOutputString(candidate->current) << "\n";
-	}
-
-	std::string getDescription() const {
-		return "Trajectory output";
-	}
+	TrajectoryOutput(std::string name);
+	~TrajectoryOutput();
+	void process(Candidate *candidate);
+	std::string getDescription() const;
 };
 
 /**
@@ -63,33 +31,10 @@ private:
 	Candidate::Status flag;
 
 public:
-	FlaggedOutput(std::string name, Candidate::Status flag) {
-		this->flag = flag;
-		outfile.open(name.c_str());
-		outfile << "(initial) Age, Id, E, x, y, z, dirX, dirY, dirZ, ";
-		outfile << "(flagged) Age, Id, E, x, y, z, dirX, dirY, dirZ\n";
-	}
-
-	~FlaggedOutput() {
-		outfile.close();
-	}
-
-	void process(Candidate *candidate) const {
-		if (candidate->getStatus() != flag)
-			return;
-		// initial state
-		outfile << "0, ";
-		outfile << getOutputString(candidate->initial);
-		outfile << ", ";
-		// final state
-		outfile << candidate->getTrajectoryLength() / Mpc << ", ";
-		outfile << getOutputString(candidate->current);
-		outfile << "\n";
-	}
-
-	std::string getDescription() const {
-		return "Output, flag: ";
-	}
+	FlaggedOutput(std::string name, Candidate::Status flag);
+	~FlaggedOutput();
+	void process(Candidate *candidate) const;
+	std::string getDescription() const;
 };
 
 /**
@@ -98,20 +43,8 @@ public:
  */
 class ShellOutput: public Module {
 public:
-	void process(Candidate *candidate) const {
-		std::cout << std::fixed << std::showpoint << std::setprecision(2)
-				<< std::setw(6);
-		std::cout << candidate->getTrajectoryLength() / Mpc << " Mpc,  ";
-		std::cout << candidate->current.getId() << ",  ";
-		std::cout << candidate->current.getEnergy() / EeV << " EeV,  ";
-		std::cout << candidate->current.getPosition() / Mpc << " Mpc, Status: ";
-		std::cout << candidate->getStatus();
-		std::cout << std::endl;
-	}
-
-	std::string getDescription() const {
-		return "ShellOutput";
-	}
+	void process(Candidate *candidate) const;
+	std::string getDescription() const;
 };
 
 } // namespace mpc
---
+++ src/module/Output.cpp
@@ -0,0 +1,109 @@
+#include "mpc/module/Output.h"
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <kiss/convert.h>
+
+namespace mpc {
+
+std::string getOutputString(ParticleState particle) {
+	std::stringstream s;
+	s << particle.getId() << ", ";
+	s << particle.getEnergy() / EeV << ", ";
+	Vector3 pos = particle.getPosition() / Mpc;
+	s << pos.x() << ", ";
+	s << pos.y() << ", ";
+	s << pos.z() << ", ";
+	Vector3 dir = particle.getDirection();
+	s << dir.x() << ", ";
+	s << dir.y() << ", ";
+	s << dir.z();
+	return s.str();
+}
+
+TrajectoryOutput::TrajectoryOutput(std::string name) {
+	outfile.open(name.c_str());
+	outfile << "# Age, HepId, E, posX, posY, posZ, dirX, dirY, dirZ, event\n";
+}
+
+TrajectoryOutput::~TrajectoryOutput() {
+	outfile.close();
+}
+
+void TrajectoryOutput::process(Candidate *candidate) {
+	outfile << candidate->getTrajectoryLength() / Mpc << ", "
+			<< getOutputString(candidate->current) << "\n";
+}
+
+std::string TrajectoryOutput::getDescription() const {
+	return "Trajectory output";
+}
+
+FlaggedOutput::FlaggedOutput(std::string name, Candidate::Status flag) {
+	this->flag = flag;
+	outfile.open(name.c_str());
+	outfile
+			<< "id, x, y, z, E, phi, theta, distance, i_id, i_x, i_y, i_z, i_E, i_phi, i_theta\n";
+}
+
+FlaggedOutput::~FlaggedOutput() {
+	outfile.close();
+}
+
+void FlaggedOutput::process(Candidate *candidate) const {
+	if (candidate->getStatus() != flag)
+		return;
+
+	outfile << candidate->current.getId() << ", ";
+	outfile << candidate->current.getPosition().x() << ", ";
+	outfile << candidate->current.getPosition().y() << ", ";
+	outfile << candidate->current.getPosition().z() << ", ";
+	outfile << candidate->current.getEnergy() << ", ";
+	outfile << candidate->current.getDirection().phi() << ", ";
+	outfile << candidate->current.getDirection().theta() << ", ";
+	outfile << candidate->getTrajectoryLength() << ", ";
+	outfile << candidate->initial.getId() << ", ";
+	outfile << candidate->initial.getPosition().x() << ", ";
+	outfile << candidate->initial.getPosition().y() << ", ";
+	outfile << candidate->initial.getPosition().z() << ", ";
+	outfile << candidate->initial.getEnergy() << ", ";
+	outfile << candidate->initial.getDirection().phi() << ", ";
+	outfile << candidate->initial.getDirection().theta() << ", ";
+	outfile << "\n";
+}
+
+std::string FlaggedOutput::getDescription() const {
+	switch (flag) {
+	case Candidate::Active:
+		return "FlaggedOutput, Active";
+	case Candidate::Detected:
+		return "FlaggedOutput, Detected";
+	case Candidate::OutOfBounds:
+		return "FlaggedOutput, OutOfBounds";
+	case Candidate::Stopped:
+		return "FlaggedOutput, Stopped";
+	case Candidate::UserDefined:
+	default:
+		return "FlaggedOutput, user defined (" + kiss::str(flag) + ")";
+	}
+
+}
+
+void ShellOutput::process(Candidate *candidate) const {
+	std::cout << std::fixed << std::showpoint << std::setprecision(2)
+			<< std::setw(6);
+	std::cout << candidate->getTrajectoryLength() / Mpc << " Mpc,  ";
+	std::cout << candidate->current.getId() << ",  ";
+	std::cout << candidate->current.getEnergy() / EeV << " EeV,  ";
+	std::cout << candidate->current.getPosition() / Mpc << " Mpc, Status: ";
+	std::cout << candidate->getStatus();
+	std::cout << std::endl;
+}
+
+std::string ShellOutput::getDescription() const {
+	return "ShellOutput";
+}
+
+} // namespace mpc

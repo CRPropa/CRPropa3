492ec2b9f69cad7a7432a000b7c2027de1156352
DavidWalz <walz@physik.rwth-aachen.de>
2012-04-13 16:31:26 +0200

    add nuclear mass test script

---
+++ test/python/testNuclearMass.py
@@ -0,0 +1,33 @@
+from mpc import *
+from pylab import *
+
+D = zeros((27, 31))
+D2 = zeros((27, 31))
+
+for z in range(0, 27):
+    for n in range(0, 31):
+        try:
+            D[z, n] = getNucleusMass(getNucleusId(n + z, z)) / amu
+            D2[z, n] = D[z, n] - (z + n) - z * mass_electron / amu
+        except:
+            continue
+
+# nuclear masses
+figure()
+im = imshow(ma.masked_array(D, D==0), aspect='equal', interpolation='nearest', origin='lower')
+cbar = colorbar(im)
+cbar.set_label('Mass [amu]')
+xlabel('Neutrons')
+ylabel('Protons')
+grid()
+savefig('testNuclearMass_mass.png')
+
+# deviation from m = (Z + N) * amu - Z * m_e
+figure()
+im = imshow(ma.masked_array(D2, D==0), aspect='equal', interpolation='nearest', origin='lower')
+cbar = colorbar(im)
+cbar.set_label('$\Delta$Mass [amu]')
+xlabel('Neutrons')
+ylabel('Protons')
+grid()
+savefig('testNuclearMass_massImprovement.png')

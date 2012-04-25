60db38e9c9d4b47184f81aba3aeccc50825781d0
DavidWalz <walz@physik.rwth-aachen.de>
2012-04-25 14:49:56 +0200

    update PhotoPion data tables, implemtation and tests

--- test/python/testPhotoPionProduction.py
+++ test/python/testPhotoPionProduction.py
@@ -1,18 +1,18 @@
 from mpc import *
 from pylab import *
 import ROOT
+ROOT.gROOT.SetBatch(True)
 
-def getSlope(interaction, energy, charge, name):
+
+def getSlope(module, energy, charge, name):
     c = Candidate()
     c.current.setId(getNucleusId(1, charge))
     c.current.setEnergy(energy)
-    s = InteractionState()
-
+    
     l = []
-    for i in range(10000):
-        c.clearInteractionStates()
-        interaction.process(c)
-        c.getInteractionState('PhotoPionProduction:'+name, s)
+    for i in range(5000):
+        s = InteractionState()
+        module.setNextInteraction(c, s)
         l.append(s.distance / Mpc)
 
     h = ROOT.TH1F('', '', 100, 0, max(l))
@@ -31,12 +31,13 @@ def compare(type, name):
     ppp = PhotoPionProduction(type)
     E_data, P_data, N_data = genfromtxt(getDataPath('/PhotoPionProduction/PPtable_' + name + '.txt'), unpack=True)
 
-    E = logspace(1.5,5,10) * EeV
+    E = E_data[1:-1:5]
     p = zeros(len(E))
     n = zeros(len(E))
     for i,energy in enumerate(E*EeV):
         p[i] = getSlope(ppp, energy, 1, name)
         n[i] = getSlope(ppp, energy, 0, name)
+    figure()
     plot(E_data, P_data, "r", label="Proton Data")
     plot(E, p, 'k+', label="Proton Simulated")
     plot(E_data, N_data, "b", label="Neutron Data")
@@ -46,10 +47,9 @@ def compare(type, name):
     legend(loc='center right')
     grid()
     loglog()
+    xlim(10, 1e5)
     ylim(1e-4, 1)
     savefig('PhotoPionProduction_' + name + '.png', bbox_inches='tight')
-    close()
 
 compare(CMB, "CMB")
 compare(IRB, "IRB")
-compare(CMB_IRB, "CMB_IRB")

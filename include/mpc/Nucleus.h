33c6dbcbaf24fa0b5d0ca592fa3e7a05d986a9c5
DavidWalz <walz@physik.rwth-aachen.de>
2012-05-08 15:54:12 +0200

    update and remove PhotoDisintegration files + other small changes

--- include/mpc/Nucleus.h
+++ include/mpc/Nucleus.h
@@ -1,6 +1,8 @@
 #ifndef NUCLEUS_H_
 #define NUCLEUS_H_
 
+#include "kiss/convert.h"
+
 #include <stdexcept>
 
 namespace mpc {
@@ -13,11 +15,17 @@ namespace mpc {
 // I is the isomer number, with I=0 corresponding to the ground state.
 inline int getNucleusId(int a, int z) {
 	if (z < 0)
-		throw std::runtime_error("mpc::Nucleus: no nucleus with Z < 0");
+		throw std::runtime_error(
+				"mpc::Nucleus: no nucleus with Z < 0, A=" + kiss::str(a) + " Z="
+						+ kiss::str(z));
 	if (a < 1)
-		throw std::runtime_error("mpc::Nucleus: no nucleus with A < 1");
+		throw std::runtime_error(
+				"mpc::Nucleus: no nucleus with A < 1, A=" + kiss::str(a) + " Z="
+						+ kiss::str(z));
 	if (a < z)
-		throw std::runtime_error("mpc::Nucleus: no nucleus with A < Z");
+		throw std::runtime_error(
+				"mpc::Nucleus: no nucleus with A < Z, A=" + kiss::str(a) + " Z="
+						+ kiss::str(z));
 	return 1000000000 + z * 10000 + a * 10;
 }
 

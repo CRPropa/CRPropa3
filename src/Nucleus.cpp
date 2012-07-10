49fc80b1cd08e44897883aa72a984f2108a25e74
DavidWalz <walz@physik.rwth-aachen.de>
2012-07-10 14:12:54 +0200

    make NuclearMassTable OpenMP safe

--- src/Nucleus.cpp
+++ src/Nucleus.cpp
@@ -12,51 +12,51 @@
 namespace mpc {
 
 int getNucleusId(int a, int z) {
-    if (z < 0)
-        throw std::runtime_error(
-                "mpc::Nucleus: no nucleus with Z < 0, A=" + kiss::str(a) + " Z="
-                        + kiss::str(z));
-    if (a < 1)
-        throw std::runtime_error(
-                "mpc::Nucleus: no nucleus with A < 1, A=" + kiss::str(a) + " Z="
-                        + kiss::str(z));
-    if (a < z)
-        throw std::runtime_error(
-                "mpc::Nucleus: no nucleus with A < Z, A=" + kiss::str(a) + " Z="
-                        + kiss::str(z));
-    return 1000000000 + z * 10000 + a * 10;
+	if (z < 0)
+		throw std::runtime_error(
+				"mpc::Nucleus: no nucleus with Z < 0, A=" + kiss::str(a) + " Z="
+						+ kiss::str(z));
+	if (a < 1)
+		throw std::runtime_error(
+				"mpc::Nucleus: no nucleus with A < 1, A=" + kiss::str(a) + " Z="
+						+ kiss::str(z));
+	if (a < z)
+		throw std::runtime_error(
+				"mpc::Nucleus: no nucleus with A < Z, A=" + kiss::str(a) + " Z="
+						+ kiss::str(z));
+	return 1000000000 + z * 10000 + a * 10;
 }
 
+struct NuclearMassTable {
+	std::vector<double> table;
 
-static std::vector<double> nuclearMassTable;
+	NuclearMassTable() {
+		std::string filename = getDataPath("/NuclearMass/nuclearMassTable.txt");
+		std::ifstream infile(filename.c_str());
 
-void initNuclearMassTable() {
-	std::string filename = getDataPath("/NuclearMass/nuclearMassTable.txt");
-	std::ifstream infile(filename.c_str());
+		if (!infile.good())
+			throw std::runtime_error("mpc: could not open file " + filename);
 
-	if (!infile.good())
-		throw std::runtime_error("mpc: could not open file " + filename);
-
-	int Z, N;
-	double mass;
-	while (infile.good()) {
-		if (infile.peek() != '#') {
-			infile >> Z >> N >> mass;
-			nuclearMassTable.push_back(mass);
+		int Z, N;
+		double mass;
+		while (infile.good()) {
+			if (infile.peek() != '#') {
+				infile >> Z >> N >> mass;
+				table.push_back(mass);
+			}
+			infile.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 		}
-		infile.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-	}
 
-	infile.close();
-}
+		infile.close();
+	}
+};
+
+static NuclearMassTable nuclearMassTable;
 
 double getNucleusMass(int id) {
-	if (nuclearMassTable.size() == 0)
-		initNuclearMassTable();
-
 	int Z = HepPID::Z(id);
 	int N = HepPID::A(id) - Z;
-	double mass = nuclearMassTable[Z * 31 + N];
+	double mass = nuclearMassTable.table[Z * 31 + N];
 	if (mass == 0)
 		throw std::runtime_error("mpc: nucleus not found " + kiss::str(id));
 	return mass;

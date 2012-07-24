ef73350be33640d0dc09a9e4a67622a6a47fbdb4
DavidWalz <walz@physik.rwth-aachen.de>
2012-07-24 11:12:48 +0200

    add output in CRPropa2 format

--- include/mpc/module/Output.h
+++ include/mpc/module/Output.h
@@ -7,38 +7,6 @@
 namespace mpc {
 
 /**
- @class TrajectoryOutput
- @brief Saves trajectories to CSV file.
- */
-class TrajectoryOutput: public Module {
-private:
-	mutable std::ofstream outfile;
-
-public:
-	TrajectoryOutput(std::string name);
-	~TrajectoryOutput();
-	void process(Candidate *candidate) const;
-	std::string getDescription() const;
-};
-
-/**
- @class ConditionalOutput
- @brief Saves particles with a given property to a CSV file.
- */
-class ConditionalOutput: public Module {
-private:
-	mutable std::ofstream outfile;
-	std::string propertyName;
-	bool removeProperty;
-public:
-	ConditionalOutput(std::string filename, std::string propName);
-	~ConditionalOutput();
-	void setRemoveProperty(bool removeProperty);
-	void process(Candidate *candidate) const;
-	std::string getDescription() const;
-};
-
-/**
  @class ShellOutput
  @brief Write properties of the candidate to the shell.
  */
@@ -47,6 +15,58 @@ public:
 	void process(Candidate *candidate) const;
 	std::string getDescription() const;
 };
+
+/**
+ @class TrajectoryOutput
+ @brief Saves trajectories to CSV file.
+ */
+class TrajectoryOutput: public Module {
+	mutable std::ofstream outfile;
+public:
+	TrajectoryOutput(std::string name);
+	~TrajectoryOutput();
+	void process(Candidate *candidate) const;
+};
+
+/**
+ @class ConditionalOutput
+ @brief Saves particles with a given property to a CSV file.
+ */
+class ConditionalOutput: public Module {
+	mutable std::ofstream outfile;
+	std::string propertyName;
+	bool removeProperty;
+public:
+	ConditionalOutput(std::string filename, std::string propName);
+	~ConditionalOutput();
+	void setRemoveProperty(bool removeProperty);
+	void process(Candidate *candidate) const;
+};
+
+/**
+ @class CRPropa2EventOutput
+ @brief Saves events in CRPropa2 format.
+ */
+class CRPropa2EventOutput: public Module {
+	mutable std::ofstream outfile;
+public:
+	CRPropa2EventOutput(std::string name);
+	~CRPropa2EventOutput();
+	void process(Candidate *candidate) const;
+};
+
+/**
+ @class CRPropa2TrajectoryOutput
+ @brief Saves trajectory in CRPropa2 format.
+ */
+class CRPropa2TrajectoryOutput: public Module {
+	mutable std::ofstream outfile;
+public:
+	CRPropa2TrajectoryOutput(std::string name);
+	~CRPropa2TrajectoryOutput();
+	void process(Candidate *candidate) const;
+};
+
 
 } // namespace mpc
 
--- src/module/Output.cpp
+++ src/module/Output.cpp
@@ -9,6 +9,7 @@
 namespace mpc {
 
 TrajectoryOutput::TrajectoryOutput(std::string name) {
+	setDescription("Trajectory output");
 	outfile.open(name.c_str());
 	outfile << "# Age, HepId, E, posX, posY, posZ, dirX, dirY, dirZ, event\n";
 }
@@ -35,12 +36,9 @@ void TrajectoryOutput::process(Candidate *candidate) const {
 	}
 }
 
-std::string TrajectoryOutput::getDescription() const {
-	return "Trajectory output";
-}
-
 ConditionalOutput::ConditionalOutput(std::string filename,
 		std::string propName) {
+	setDescription("ConditionalOutput, condition: " + propName);
 	removeProperty = false;
 	propertyName = propName;
 	outfile.open(filename.c_str());
@@ -62,26 +60,26 @@ void ConditionalOutput::process(Candidate *candidate) const {
 		char buffer[256];
 		size_t p = 0;
 
-		p += ::sprintf(buffer + p, "%d", candidate->current.getId());
+		p += sprintf(buffer + p, "%i", candidate->current.getId());
 
 		const Vector3d &pos = candidate->current.getPosition() / Mpc;
-		p += ::sprintf(buffer + p, ", %.4f, %.4f, %.4f", pos.x, pos.y, pos.z);
+		p += sprintf(buffer + p, ", %.4f, %.4f, %.4f", pos.x, pos.y, pos.z);
 
 		const Vector3d &dir = candidate->current.getDirection();
-		p += ::sprintf(buffer + p, ", %.4f, %.4f, %.4f",
+		p += sprintf(buffer + p, ", %.4f, %.4f, %.4f",
 				candidate->current.getEnergy() / EeV, dir.getPhi(),
 				dir.getTheta());
 
-		p += ::sprintf(buffer + p, ", %.4f",
+		p += sprintf(buffer + p, ", %.4f",
 				candidate->getTrajectoryLength() / Mpc);
 
-		p += ::sprintf(buffer + p, ", %d", candidate->initial.getId());
+		p += sprintf(buffer + p, ", %i", candidate->initial.getId());
 
 		const Vector3d &ipos = candidate->initial.getPosition() / Mpc;
-		p += ::sprintf(buffer + p, ", %.4f, %.4f, %.4f", ipos.x, ipos.y, ipos.z);
+		p += sprintf(buffer + p, ", %.4f, %.4f, %.4f", ipos.x, ipos.y, ipos.z);
 
 		const Vector3d &idir = candidate->initial.getDirection();
-		p += ::sprintf(buffer + p, ", %.4f, %.4f, %.4f\n",
+		p += sprintf(buffer + p, ", %.4f, %.4f, %.4f\n",
 				candidate->initial.getEnergy() / EeV, idir.getPhi(),
 				idir.getTheta());
 
@@ -96,8 +94,102 @@ void ConditionalOutput::process(Candidate *candidate) const {
 	}
 }
 
-std::string ConditionalOutput::getDescription() const {
-	return "ConditionalOutput, condition: " + propertyName;
+//  _fDataStream << "#CRPropa - Output data file" << endl ;
+//  if (lUnivType == "One Dimension" && lRecordType == "Events") {
+//    _fDataStream << "#Format - Particle_Type Initial_Particle_Type Initial_Position(Mpc) Initial_Energy(EeV) Time(Mpc) Energy(EeV)" << endl ;
+//  } else if (lUnivType == "One Dimension" && lRecordType == "Full Trajectories") {
+//    _fDataStream << "#Format - Particle_Type Initial_Particle_Type Time(Mpc) Position(Mpc) Energy(EeV)" << endl ;
+//  } else throw TCrpErr("Output format determination failed") ;
+
+CRPropa2EventOutput::CRPropa2EventOutput(std::string filename) {
+	setDescription("Event output in CRPropa2 format");
+	outfile.open(filename.c_str());
+	outfile << "#CRPropa - Output data file" << std::endl
+			<< "#Format - Particle_Type Initial_Particle_Type Initial_Position[X,Y,Z](Mpc) Initial_Momentum[E,theta,phi](EeV) Time(Mpc) Position[X,Y,Z](Mpc) Momentum[E,theta,phi](EeV)"
+			<< std::endl;
+}
+
+CRPropa2EventOutput::~CRPropa2EventOutput() {
+	outfile.close();
+}
+
+void CRPropa2EventOutput::process(Candidate *candidate) const {
+	if (candidate->isActive())
+		return;
+	if (candidate->hasProperty("Detected")) {
+		char buffer[256]; // max. 256 characters per line
+		size_t p = 0; // length of line
+
+		int cid = convertToCRPropaId(candidate->current.getId());
+		p += sprintf(buffer + p, "%i, ", cid);
+
+		int iid = convertToCRPropaId(candidate->initial.getId());
+		p += sprintf(buffer + p, "%i, ", iid);
+
+		const Vector3d &ipos = candidate->initial.getPosition() / Mpc;
+		p += sprintf(buffer + p, ", %.4f, %.4f, %.4f", ipos.x, ipos.y, ipos.z);
+
+		double iPhi = candidate->initial.getDirection().getPhi();
+		double iTheta = candidate->initial.getDirection().getTheta();
+		double iE = candidate->initial.getEnergy() / EeV;
+		p += sprintf(buffer + p, ", %.4f, %.4f, %.4f", iE, iPhi, iTheta);
+
+		double t = candidate->getTrajectoryLength() / Mpc;
+		p += sprintf(buffer + p, ", %.4f", t);
+
+		const Vector3d &pos = candidate->current.getPosition() / Mpc;
+		p += sprintf(buffer + p, ", %.4f, %.4f, %.4f", ipos.x, ipos.y, ipos.z);
+
+		double phi = candidate->current.getDirection().getPhi();
+		double theta = candidate->current.getDirection().getTheta();
+		double E = candidate->current.getEnergy() / EeV;
+		p += sprintf(buffer + p, ", %.4f, %.4f, %.4f\n", E, phi, theta);
+
+#pragma omp critical
+		{
+			outfile.write(buffer, p);
+		}
+	}
+}
+
+CRPropa2TrajectoryOutput::CRPropa2TrajectoryOutput(std::string filename) {
+	setDescription("Trajectory output in CRPropa2 format");
+	outfile.open(filename.c_str());
+	outfile << "#CRPropa - Output data file" << std::endl
+			<< "#Format - Particle_Type Initial_Particle_Type Time(Mpc) Position[X,Y,Z](Mpc) Momentum[X,Y,Z](EeV) Energy(EeV)"
+			<< std::endl;
+}
+
+CRPropa2TrajectoryOutput::~CRPropa2TrajectoryOutput() {
+	outfile.close();
+}
+
+void CRPropa2TrajectoryOutput::process(Candidate *candidate) const {
+	char buffer[256];
+	size_t p = 0;
+
+	int cid = convertToCRPropaId(candidate->current.getId());
+	p += sprintf(buffer + p, "%i", cid);
+
+	int iid = convertToCRPropaId(candidate->initial.getId());
+	p += sprintf(buffer + p, ", %i", iid);
+
+	double t = candidate->getTrajectoryLength() / Mpc;
+	p += sprintf(buffer + p, ", %.4f", t);
+
+	const Vector3d &pos = candidate->current.getPosition() / Mpc;
+	p += sprintf(buffer + p, ", %.4f, %.4f, %.4f", pos.x, pos.y, pos.z);
+
+	const Vector3d &mom = candidate->current.getMomentum() / EeV;
+	p += sprintf(buffer + p, ", %.4g, %.4g, %.4g", mom.x, mom.y, mom.z);
+
+	double E = candidate->current.getEnergy() / EeV;
+	p += sprintf(buffer + p, ", %.4f\n", E);
+
+#pragma omp critical
+	{
+		outfile.write(buffer, p);
+	}
 }
 
 void ShellOutput::process(Candidate *candidate) const {
@@ -116,7 +208,7 @@ void ShellOutput::process(Candidate *candidate) const {
 }
 
 std::string ShellOutput::getDescription() const {
-	return "ShellOutput";
+	return "Shell output";
 }
 
 } // namespace mpc

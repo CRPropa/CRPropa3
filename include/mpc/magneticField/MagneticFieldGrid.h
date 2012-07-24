b3ac912af4dab58bf33e9fb8cc84c5317ca11c87
DavidWalz <walz@physik.rwth-aachen.de>
2012-07-24 18:18:39 +0200

    implement template class for periodic grids refactore MagneticFieldGrid

--- include/mpc/magneticField/MagneticFieldGrid.h
+++ include/mpc/magneticField/MagneticFieldGrid.h
@@ -2,7 +2,7 @@
 #define MPC_MAGNETICFIELDGRID_H_
 
 #include "mpc/magneticField/MagneticField.h"
-#include <vector>
+#include "mpc/magneticField/PeriodicGrid.h"
 
 namespace mpc {
 
@@ -10,42 +10,93 @@ namespace mpc {
  @class MagneticFieldGrid
  @brief Magnetic field on a periodic, cartesian grid with trilinear interpolation.
 
- This class provides a three-dimensional magnetic field grid.
- The grid spacing is constant and equal along all three axes.
- Magnetic field values are calculated by trilinear interpolation of the surrounding 8 grid points. \n
- The grid is periodically extended.
- Thus the field at opposite borders is assumed identical and does not need to be sampled twice.
- The grid sample positions are hence at 0, size / samples, ... size * (samples - 1) / samples and the grid spacing is size / samples.
+ This class provides a container for a VectorFieldGrid (PeriodicGrid of type Vector3f) to serve as a MagneticField.
  */
 class MagneticFieldGrid: public MagneticField {
-	std::vector<Vector3f> grid; /**< Magnetic field vectors */
-	Vector3d origin; /**< Lower left front corner of the grid */
-	size_t Nx, Ny, Nz; /**< Number of grid points per edge */
-	double spacing; /**< Distance between two grid points (= size / samples) */
+	VectorFieldGrid *grid;
 
 public:
-	MagneticFieldGrid(Vector3d origin, size_t N, double spacing);
-	MagneticFieldGrid(Vector3d origin, size_t Nx, size_t Ny, size_t Nz,
-			double spacing);
-
-	void setOrigin(Vector3d origin);
-	void setGridSize(size_t Nx, size_t Ny, size_t Nz);
-	void setSpacing(double spacing);
-
-	Vector3d getOrigin() const;
-	size_t getNx() const;
-	size_t getNy() const;
-	size_t getNz() const;
-	double getSpacing() const;
-
-	Vector3f &get(size_t ix, size_t iy, size_t iz);
-	const Vector3f &get(size_t ix, size_t iy, size_t iz) const;
-
+	MagneticFieldGrid(VectorFieldGrid *grid);
+	void setGrid(VectorFieldGrid *grid);
+	VectorFieldGrid *getGrid();
 	Vector3d getField(const Vector3d &position) const;
 };
 
-/** Lower and upper neighbor in a periodically continued unit grid */
-void periodicClamp(double x, int n, int &lo, int &hi);
+/**
+ @class MagneticFieldGrid
+ @brief Modulated magnetic field on a periodic grid.
+
+ The class
+ */
+class ModulatedMagneticFieldGrid: public MagneticField {
+	VectorFieldGrid *grid;
+	ScalarFieldGrid *modGrid;
+
+public:
+	ModulatedMagneticFieldGrid(VectorFieldGrid *grid, ScalarFieldGrid *modGrid);
+	void setGrid(VectorFieldGrid *grid);
+	void setModulationGrid(ScalarFieldGrid *modGrid);
+	VectorFieldGrid *getGrid();
+	ScalarFieldGrid *getModulationGrid();
+	Vector3d getField(const Vector3d &position) const;
+};
+
+/** Calculate the mean field strength */
+Vector3f meanFieldStrength(VectorFieldGrid *m);
+
+/** Calculate the RMS field strength */
+double rmsFieldStrength(VectorFieldGrid *m);
+
+/** Multiply a magnetic field grid by a factor. */
+void scale(VectorFieldGrid *m, double a);
+
+#ifdef MPC_HAVE_FFTW3F
+/**
+ * Create a random initialization of a turbulent field.
+ * @param lMin		Minimum wavelength of the turbulence
+ * @param lMax		Maximum wavelength of the turbulence
+ * @param spectralIndex	Power spectral index of the turbulence (-11/3 corresponds to a Kolmogorov spectrum)
+ * @param Brms		RMS field strength
+ * @param seed		Random seed
+ */
+void initTurbulence(VectorFieldGrid *m, double Brms, double lMin,
+		double lMax, double spectralIndex = -11. / 3., int seed = 0);
+
+/** Analytically calculate the correlation length of a turbulent field */
+double turbulentCorrelationLength(double lMin, double lMax, double spectralIndex = -11. / 3.);
+#endif // MPC_HAVE_FFTW3F
+
+/**
+ * Load a magnetic field grid from a binary file.
+ * The field is stored single precision numbers with the field components in xyz order and the grid z-index changing the fastest.
+ * @param c	conversion of the values in the file to SI
+ */
+void load(VectorFieldGrid *m, std::string filename, double c = 1);
+
+/**
+ * Dump a magnetic field grid to a binary file.
+ * The field is stored single precision numbers with the field components in xyz order and the grid z-index changing the fastest.
+ * @param c	conversion of the values in the file to SI
+ */
+void dump(VectorFieldGrid *m, std::string filename, double c = 1);
+
+/**
+ * Load a magnetic field grid from a plain text file.
+ * The field is stored as one grid point per line from (0,0,0) to (nx, ny, nz) with the grid z-index changing the fastest.
+ * Within one line the magnetic field values are stored in xyz order separated by a blank or tab.
+ * Header lines must start with a #.
+ * @param c	conversion of the values in the file to SI
+ */
+void loadTxt(VectorFieldGrid *m, std::string filename, double c = 1);
+
+/**
+ * Dump a magnetic field grid to a plain text file.
+ * The field is stored as one grid point per line from (0,0,0) to (nx, ny, nz) with the grid z-index changing the fastest.
+ * Within one line the magnetic field values are stored in xyz order separated by a blank or tab.
+ * Header lines must start with a #.
+ * @param c	conversion of SI to the values in the file
+ */
+void dumpTxt(VectorFieldGrid *m, std::string filename, double c = 1);
 
 } // namespace mpc
 
--- src/magneticField/MagneticFieldGrid.cpp
+++ src/magneticField/MagneticFieldGrid.cpp
@@ -1,109 +1,294 @@
 #include "mpc/magneticField/MagneticFieldGrid.h"
-#include <stdexcept>
+#include "mpc/Random.h"
+#include <fstream>
 
 namespace mpc {
 
-MagneticFieldGrid::MagneticFieldGrid(Vector3d origin, size_t N,
-		double spacing) {
-	setOrigin(origin);
-	setGridSize(N, N, N);
-	setSpacing(spacing);
+MagneticFieldGrid::MagneticFieldGrid(VectorFieldGrid *grid) {
+	setGrid(grid);
 }
 
-MagneticFieldGrid::MagneticFieldGrid(Vector3d origin, size_t Nx, size_t Ny,
-		size_t Nz, double spacing) {
-	setOrigin(origin);
-	setGridSize(Nx, Ny, Nz);
-	setSpacing(spacing);
+void MagneticFieldGrid::setGrid(VectorFieldGrid *grid) {
+	this->grid = grid;
 }
 
-void MagneticFieldGrid::setOrigin(Vector3d origin) {
-	this->origin = origin;
+VectorFieldGrid *MagneticFieldGrid::getGrid() {
+	return grid;
 }
 
-void MagneticFieldGrid::setGridSize(size_t Nx, size_t Ny, size_t Nz) {
-	this->Nx = Nx;
-	this->Ny = Ny;
-	this->Nz = Nz;
-	grid.resize(Nx * Ny * Nz);
+Vector3d MagneticFieldGrid::getField(const Vector3d &pos) const {
+	return grid->interpolate(pos);
 }
 
-void MagneticFieldGrid::setSpacing(double spacing) {
-	this->spacing = spacing;
+void ModulatedMagneticFieldGrid::setGrid(VectorFieldGrid *g) {
+	grid = g;
 }
 
-Vector3d MagneticFieldGrid::getOrigin() const {
-	return origin;
+VectorFieldGrid *ModulatedMagneticFieldGrid::getGrid() {
+	return grid;
 }
 
-size_t MagneticFieldGrid::getNx() const {
-	return Nx;
+void ModulatedMagneticFieldGrid::setModulationGrid(ScalarFieldGrid *g) {
+	modGrid = g;
 }
 
-size_t MagneticFieldGrid::getNy() const {
-	return Ny;
+ScalarFieldGrid *ModulatedMagneticFieldGrid::getModulationGrid() {
+	return modGrid;
 }
 
-size_t MagneticFieldGrid::getNz() const {
-	return Nz;
+Vector3d ModulatedMagneticFieldGrid::getField(const Vector3d &pos) const {
+	float m = modGrid->interpolate(pos);
+	Vector3d b = grid->interpolate(pos);
+	return b * m;
 }
 
-double MagneticFieldGrid::getSpacing() const {
-	return spacing;
+Vector3f meanFieldStrength(VectorFieldGrid *m) {
+	size_t Nx = m->getNx();
+	size_t Ny = m->getNy();
+	size_t Nz = m->getNz();
+	Vector3f mean(0.);
+	for (int ix = 0; ix < Nx; ix++)
+		for (int iy = 0; iy < Ny; iy++)
+			for (int iz = 0; iz < Nz; iz++)
+				mean += m->get(ix, iy, iz);
+	return mean / Nx / Ny / Nz;
 }
 
-Vector3f &MagneticFieldGrid::get(size_t ix, size_t iy, size_t iz) {
-	return grid[ix * Ny * Nz + iy * Ny + iz];
+double rmsFieldStrength(VectorFieldGrid *m) {
+	size_t Nx = m->getNx();
+	size_t Ny = m->getNy();
+	size_t Nz = m->getNz();
+	double sumB2 = 0;
+	for (int ix = 0; ix < Nx; ix++)
+		for (int iy = 0; iy < Ny; iy++)
+			for (int iz = 0; iz < Nz; iz++)
+				sumB2 += m->get(ix, iy, iz).getMag2();
+	return sqrt(sumB2 / Nx / Ny / Nz);
 }
 
-const Vector3f &MagneticFieldGrid::get(size_t ix, size_t iy, size_t iz) const {
-	return grid[ix * Ny * Nz + iy * Ny + iz];
+void scale(VectorFieldGrid *m, double a) {
+	for (int ix = 0; ix < m->getNx(); ix++)
+		for (int iy = 0; iy < m->getNy(); iy++)
+			for (int iz = 0; iz < m->getNz(); iz++)
+				m->get(ix, iy, iz) *= a;
 }
 
-Vector3d MagneticFieldGrid::getField(const Vector3d &position) const {
-	// position on a unit grid
-	Vector3d r = (position - origin) / spacing;
+#ifdef MPC_HAVE_FFTW3F
+#include "fftw3.h"
 
-	// indices of lower and upper neighbors
-	int ix, iX, iy, iY, iz, iZ;
-	periodicClamp(r.x, Nx, ix, iX);
-	periodicClamp(r.y, Ny, iy, iY);
-	periodicClamp(r.z, Nz, iz, iZ);
+void initTurbulence(VectorFieldGrid *m, double Brms, double lMin, double lMax,
+		double spectralIndex, int seed) {
+	size_t Nx = m->getNx();
+	size_t Ny = m->getNy();
+	size_t Nz = m->getNz();
+	if ((Nx != Ny) or (Ny != Nz))
+	throw std::runtime_error("turbulentField: only cubic grid supported");
 
-	// linear fraction to lower and upper neighbors
-	double fx = r.x - floor(r.x);
-	double fX = 1 - fx;
-	double fy = r.y - floor(r.y);
-	double fY = 1 - fy;
-	double fz = r.z - floor(r.z);
-	double fZ = 1 - fz;
+	double spacing = m->getSpacing();
+	if (lMin < 2 * spacing)
+	throw std::runtime_error("turbulentField: lMin < 2 * spacing");
+	if (lMin >= lMax)
+	throw std::runtime_error("turbulentField: lMin >= lMax");
+	if (lMax > Nx * spacing / 2)
+	throw std::runtime_error("turbulentField: lMax > size / 2");
 
-	// trilinear interpolation
-	// check: http://paulbourke.net/miscellaneous/interpolation/
-	Vector3d b(0.);
-	//V000 (1 - x) (1 - y) (1 - z) +
-	b += get(ix, iy, iz) * fX * fY * fZ;
-	//V100 x (1 - y) (1 - z) +
-	b += get(iX, iy, iz) * fx * fY * fZ;
-	//V010 (1 - x) y (1 - z) +
-	b += get(ix, iY, iz) * fX * fy * fZ;
-	//V001 (1 - x) (1 - y) z +
-	b += get(ix, iy, iZ) * fX * fY * fz;
-	//V101 x (1 - y) z +
-	b += get(iX, iy, iZ) * fx * fY * fz;
-	//V011 (1 - x) y z +
-	b += get(ix, iY, iZ) * fX * fy * fz;
-	//V110 x y (1 - z) +
-	b += get(iX, iY, iz) * fx * fy * fZ;
-	//V111 x y z
-	b += get(iX, iY, iZ) * fx * fy * fz;
+	size_t n = Nx; // size of array
+	size_t n2 = (size_t) floor(n / 2) + 1;// size array in z-direction in configuration space
 
-	return b;
+	// arrays to hold the complex vector components of the B(k)-field
+	fftwf_complex *Bkx, *Bky, *Bkz;
+	Bkx = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex) * n * n * n2);
+	Bky = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex) * n * n * n2);
+	Bkz = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex) * n * n * n2);
+
+	Random random;
+	if (seed != 0)
+	random.seed(seed);// use given seed
+
+	// calculate the n possible discrete wave numbers
+	double K[n];
+	for (int i = 0; i < n; i++)
+	K[i] = (double) i / n - i / (n / 2);
+
+	// construct the field in configuration space
+	int i;
+	double k, theta, phase, cosPhase, sinPhase;
+	double kMin = spacing / lMax;
+	double kMax = spacing / lMin;
+	Vector3f b;// real b-field vector
+	Vector3f ek, e1, e2;// orthogonal base
+	Vector3f n0(1, 1, 1);// arbitrary vector to construct orthogonal base
+
+	for (size_t ix = 0; ix < n; ix++) {
+		for (size_t iy = 0; iy < n; iy++) {
+			for (size_t iz = 0; iz < n2; iz++) {
+
+				i = ix * n * n2 + iy * n2 + iz;
+				ek.setXYZ(K[ix], K[iy], K[iz]);
+				k = ek.getMag();
+
+				// wave outside of turbulent range -> B(k) = 0
+				if ((k < kMin) || (k > kMax)) {
+					Bkx[i][0] = 0;
+					Bkx[i][1] = 0;
+					Bky[i][0] = 0;
+					Bky[i][1] = 0;
+					Bkz[i][0] = 0;
+					Bkz[i][1] = 0;
+					continue;
+				}
+
+				// construct an orthogonal base ek, e1, e2
+				if (ek.getAngleTo(n0) < 1e-3) {
+					// ek parallel to (1,1,1)
+					e1.setXYZ(-1., 1., 0);
+					e2.setXYZ(1., 1., -2.);
+				} else {
+					// ek not parallel to (1,1,1)
+					e1 = n0.cross(ek);
+					e2 = ek.cross(e1);
+				}
+				e1 /= e1.getMag();
+				e2 /= e2.getMag();
+
+				// random orientation perpendicular to k
+				theta = 2 * M_PI * random.rand();
+				b = e1 * cos(theta) + e2 * sin(theta);
+
+				// standard normal distributed amplitude weighted with k^alpha/2
+				b *= random.randNorm() * pow(k, spectralIndex / 2.);
+
+				// uniform random phase
+				phase = 2 * M_PI * random.rand();
+				cosPhase = cos(phase);// real part
+				sinPhase = sin(phase);// imaginary part
+
+				Bkx[i][0] = b.x * cosPhase;
+				Bkx[i][1] = b.x * sinPhase;
+				Bky[i][0] = b.y * cosPhase;
+				Bky[i][1] = b.y * sinPhase;
+				Bkz[i][0] = b.z * cosPhase;
+				Bkz[i][1] = b.z * sinPhase;
+			}
+		}
+	}
+
+	// in-place, complex to real, inverse Fourier transformation on each component
+	// note that the last elements of B(x) are unused now
+	float *Bx = (float*) Bkx;
+	fftwf_plan plan_x = fftwf_plan_dft_c2r_3d(n, n, n, Bkx, Bx, FFTW_ESTIMATE);
+	fftwf_execute(plan_x);
+	fftwf_destroy_plan(plan_x);
+
+	float *By = (float*) Bky;
+	fftwf_plan plan_y = fftwf_plan_dft_c2r_3d(n, n, n, Bky, By, FFTW_ESTIMATE);
+	fftwf_execute(plan_y);
+	fftwf_destroy_plan(plan_y);
+
+	float *Bz = (float*) Bkz;
+	fftwf_plan plan_z = fftwf_plan_dft_c2r_3d(n, n, n, Bkz, Bz, FFTW_ESTIMATE);
+	fftwf_execute(plan_z);
+	fftwf_destroy_plan(plan_z);
+
+	// save to grid
+	for (size_t ix = 0; ix < n; ix++) {
+		for (size_t iy = 0; iy < n; iy++) {
+			for (size_t iz = 0; iz < n; iz++) {
+				i = ix * n * 2 * n2 + iy * 2 * n2 + iz;
+				Vector3f &b = m->get(ix, iy, iz);
+				b.x = Bx[i];
+				b.y = By[i];
+				b.z = Bz[i];
+			}
+		}
+	}
+
+	fftwf_free(Bkx);
+	fftwf_free(Bky);
+	fftwf_free(Bkz);
+
+	scale(m, Brms / rmsFieldStrength(m)); // normalize to Brms
 }
 
-void periodicClamp(double x, int n, int &lo, int &hi) {
-	lo = ((int(floor(x)) % n) + n) % n;
-	hi = (lo + 1) % n;
+double turbulentCorrelationLength(double lMin, double lMax, double spectralIndex) {
+	double r = lMin / lMax;
+	double a = -spectralIndex - 2;
+	return lMax / 2 * (a - 1) / a * (1 - pow(r, a)) / (1 - pow(r, a - 1));
+}
+#endif // MPC_HAVE_FFTW3F
+
+void load(VectorFieldGrid *m, std::string filename, double c) {
+	std::ifstream fin(filename.c_str(), std::ios::binary);
+	if (!fin)
+		throw std::runtime_error("MagneticFieldGrid: File not found");
+	for (int ix = 0; ix < m->getNx(); ix++) {
+		for (int iy = 0; iy < m->getNy(); iy++) {
+			for (int iz = 0; iz < m->getNz(); iz++) {
+				Vector3f &b = m->get(ix, iy, iz);
+				fin.read((char*) &(b.x), sizeof(float));
+				fin.read((char*) &(b.y), sizeof(float));
+				fin.read((char*) &(b.z), sizeof(float));
+				b *= c;
+			}
+		}
+	}
+	fin.close();
+}
+
+void dump(VectorFieldGrid *m, std::string filename, double c) {
+	std::ofstream fout(filename.c_str(), std::ios::binary);
+	if (!fout)
+		throw std::runtime_error("MagneticFieldGrid: Could not open file");
+	for (int ix = 0; ix < m->getNx(); ix++) {
+		for (int iy = 0; iy < m->getNy(); iy++) {
+			for (int iz = 0; iz < m->getNz(); iz++) {
+				Vector3f b = m->get(ix, iy, iz) * c;
+				fout.write((char*) &(b.x), sizeof(float));
+				fout.write((char*) &(b.y), sizeof(float));
+				fout.write((char*) &(b.z), sizeof(float));
+			}
+		}
+	}
+	fout.close();
+}
+
+void loadTxt(VectorFieldGrid *m, std::string filename, double unit) {
+	std::ifstream fin(filename.c_str());
+	if (!fin)
+		throw std::runtime_error("MagneticFieldGrid: file not found");
+
+	// skip header lines
+	while (fin.peek() == '#')
+		fin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+	for (int ix = 0; ix < m->getNx(); ix++) {
+		for (int iy = 0; iy < m->getNy(); iy++) {
+			for (int iz = 0; iz < m->getNz(); iz++) {
+				Vector3f &b = m->get(ix, iy, iz);
+				fin >> b.x >> b.y >> b.z;
+				b *= unit;
+				if (fin.eof())
+					throw std::runtime_error(
+							"MagneticFieldGrid: file too short");
+			}
+		}
+	}
+	fin.close();
+}
+
+void dumpTxt(VectorFieldGrid *m, std::string filename, double unit) {
+	std::ofstream fout(filename.c_str());
+	if (!fout)
+		throw std::runtime_error("MagneticFieldGrid: could not open file");
+	for (int ix = 0; ix < m->getNx(); ix++) {
+		for (int iy = 0; iy < m->getNy(); iy++) {
+			for (int iz = 0; iz < m->getNz(); iz++) {
+				Vector3f b = m->get(ix, iy, iz) * unit;
+				fout << b << "\n";
+			}
+		}
+	}
+	fout.close();
 }
 
 } // namespace mpc

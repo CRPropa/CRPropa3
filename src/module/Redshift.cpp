4dae0578841f232f39d9ac1577fa1e320205f0ee
DavidWalz <walz@physik.rwth-aachen.de>
2012-09-04 14:05:13 +0200

    improved simple redshift module, started implementation of detailed redshift

--- src/module/Redshift.cpp
+++ src/module/Redshift.cpp
@@ -1,50 +1,79 @@
 #include "mpc/module/Redshift.h"
 #include "mpc/Units.h"
 
+#include <sstream>
+
 namespace mpc {
 
-SimpleRedshift::SimpleRedshift(Vector3d center) {
-	this->center = center;
+SimpleRedshift::SimpleRedshift(Vector3d observer, double h) :
+		observer(observer), h(h) {
 }
 
-void SimpleRedshift::process(Candidate *candidate) const {
-	double d = candidate->current.getPosition().getDistanceTo(center);
-	double z = 0.024 * d / 100 / Mpc;
-	double dz = candidate->getRedshift() - z;
+void SimpleRedshift::process(Candidate *c) const {
+	double d = c->current.getPosition().getDistanceTo(observer);
+	double z = h * 1e5 / Mpc * d / c_light;
+	double dz = z - c->getRedshift();
 
-	// redshift can only decrease
-	if (dz < 0)
-		return;
+	if (dz > 0)
+		return; // do nothing if the new redshift is higher
 
-	// using dE/dz=E/(1+z)
-	candidate->current.setEnergy(
-			candidate->current.getEnergy() * (1 - dz) / (1 + z));
-	candidate->setRedshift(z);
+	c->setRedshift(z);
+
+	double E = c->current.getEnergy();
+	c->current.setEnergy(E * (1 + dz) / (1 + z)); // using dE/dz=E/(1+z)
 }
 
 std::string SimpleRedshift::getDescription() const {
-	return "Simple redshift";
+	std::stringstream ss;
+	ss << "Simple redshift: observer " << observer / Mpc << "Mpc, h = " << h;
+	return ss.str();
 }
 
-} // namespace mpc
-
-// from CRPropa
-//double omegaM = 0.3;
-//double omegaL = 0.7;
-//double H0 = 7e4 * meter / second;
-//size_t nBins = 1000;
-//double zMin = 0.0001;
-//double zMax = 100;
+//Redshift::Redshift(double h, double omegaM, double omegaL) {
+//	double H0 = h * 1e5 / Mpc;
 //
-//std::vector<double> z;
-//std::vector<double> R;
-//std::vector<double> D;
-//z.push_back(0);
-//R.push_back(1);
-//D.push_back(0);
+//	const int n = 1000;
+//	double zMin = 0.0001;
+//	double zMax = 100;
 //
-//for (size_t i=0; i<(nBins-1); i++) {
-//	z.push_back( zMin * pow( zMax/zMin, i / double(nBins-2) ) );
-//	R.push_back( 1. / ( (1.+z[i]) * sqrt( omegaL + omegaM * pow(1.+z[i],3) ) ) );
-//	D.push_back( D[i-1] + (z[i] - z[i-1]) / 2. * (R[i-1] + R[i]) * c_light/H0 );
+//	std::vector<double> H;
+//
+//	z.resize(n);
+//	H.resize(n);
+//	D.resize(n);
+//
+//	z[0] = 0;
+//	H[0] = H0;
+//	D[0] = 0;
+//
+//	// Relation between comoving distance and redshift (see J.A. Peacock, Cosmological physics, p. 89 eq. 3.76)
+//	// R0 dr = c / H(z) dz
+//	// H(z) = H0 sqrt(omegaL + omegaM (1 + z)^3)
+//	// Integration with midpoint rule.
+//	for (int i = 1; i < n; i++) {
+//		z[i] = pow(10, zMin + (zMax - zMin) * i / (n - 1));
+//		H[i] = H0 * sqrt(omegaL + omegaM * pow(1 + z[i], 3));
+//		D[i] = D[i - 1]
+//				+ c_light * (z[i] - z[i - 1]) * (1 / H[i - 1] + 1 / H[i]) / 2;
+//	}
 //}
+//
+//void Redshift::process(Candidate *c) const {
+//}
+//
+//std::string Redshift::getDescription() const {
+//	std::stringstream ss;
+//	ss << "Redshift: h = ";
+//	return ss.str();
+//}
+
+//void TParticlePropa::RedshiftLoss() {
+//	if (_fpUniverse->Type() == UNIVERSE_ENV1D) {
+//		// Using the relations dE/dz=E/(1+z) and dz/dt=H_0(1+z)E(z)
+//		double lLoss = _fpUniverse->H0() * _fTimeStep * sqrt(_fpUniverse->OmegaLambda() + _fpUniverse->OmegaM() * pow(1 + _fRedshift, 3));
+//		_fEnergy *= (1 - lLoss);
+//		_fMomentum.setMag(_fEnergy * inv_c_light);
+//	}
+//}
+
+} // namespace mpc

3b29751cd8c51dd1815a40f9565a525cebc05419
geromueller <post@geromueller.de>
2012-11-12 17:00:03 +0100

    fix memory leak in modulelist, add ctrl-c handler

--- include/mpc/ProgressBar.h
+++ include/mpc/ProgressBar.h
@@ -20,15 +20,12 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-
-
 #include <stdio.h>
 #include <string>
 #include <iostream>
 #include <ctime>
 
-class ProgressBar
-{
+class ProgressBar {
 private:
 	unsigned long _steps;
 	unsigned long _currentCount;
@@ -41,65 +38,67 @@ private:
 
 public:
 	/// Initialize a ProgressBar with [steps] number of steps, updated at [updateSteps] intervalls
-	ProgressBar(const std::string &title, unsigned long steps = 0, unsigned long updateSteps = 100) : 
-		_steps(steps), _currentCount(0), _maxbarLength(10), _updateSteps(updateSteps), _nextStep(1)
-	{
+	ProgressBar(unsigned long steps = 0, unsigned long updateSteps = 100) :
+			_steps(steps), _currentCount(0), _maxbarLength(10), _updateSteps(
+					updateSteps), _nextStep(1), _startTime(0) {
 		if (_updateSteps > _steps)
 			_updateSteps = _steps;
+		arrow.append(">");
+	}
+
+	void start(const std::string &title) {
 		_startTime = time(NULL);
 		std::string s = ctime(&_startTime);
-		s.erase(s.end()-1, s.end());
+		s.erase(s.end() - 1, s.end());
 		stringTmpl = "  Started ";
 		stringTmpl.append(s);
 		stringTmpl.append(" : [%-10s] %3i%%    %s: %02i:%02is %s\r");
-		arrow.append(">");
 		std::cout << title << std::endl;
-	}
 
+	}
 	/// update the progressbar
 	/// should be called steps times in a loop 
-	void update()
-	{
+	void update() {
 		_currentCount++;
-		if (_currentCount == _nextStep || _currentCount == _steps)
-		{
-			_nextStep+=long(_steps / float(_updateSteps));
+		if (_currentCount == _nextStep || _currentCount == _steps) {
+			_nextStep += long(_steps / float(_updateSteps));
 
-			int percentage = int(100*_currentCount/float(_steps));
+			int percentage = int(100 * _currentCount / float(_steps));
 			time_t currentTime = time(NULL);
-			if (_currentCount < _steps)
-			{
-				int j=0;
-				if (arrow.size() <= (_maxbarLength)*(_currentCount)/(_steps))
+			if (_currentCount < _steps) {
+				int j = 0;
+				if (arrow.size()
+						<= (_maxbarLength) * (_currentCount) / (_steps))
 					arrow.insert(0, "=");
 				float tElapsed = currentTime - _startTime;
-				float tToGo = (_steps - _currentCount) * tElapsed / _currentCount;
-				printf(stringTmpl.c_str(), arrow.c_str(), percentage , "Finish in",int(tToGo/60), int(tToGo)%60, "");
+				float tToGo = (_steps - _currentCount) * tElapsed
+						/ _currentCount;
+				printf(stringTmpl.c_str(), arrow.c_str(), percentage,
+						"Finish in", int(tToGo / 60), int(tToGo) % 60, "");
 				fflush(stdout);
-			}
-			else
-			{
+			} else {
 				float tElapsed = currentTime - _startTime;
 				std::string s = " - Finished at ";
 				s.append(ctime(&currentTime));
 				char fs[255];
-				sprintf(fs, "%c[%d;%dm Finished %c[%dm",27,1,32,27,0);
-				printf(stringTmpl.c_str(), fs, percentage, "Needed",int(tElapsed/60), int(tElapsed)%60, s.c_str());
+				sprintf(fs, "%c[%d;%dm Finished %c[%dm", 27, 1, 32, 27, 0);
+				printf(stringTmpl.c_str(), fs, percentage, "Needed",
+						int(tElapsed / 60), int(tElapsed) % 60, s.c_str());
 			}
 		}
 	}
 
 	/// Mark the progressbar with an error
-	void setError()
-	{
+	void setError() {
 		time_t currentTime = time(NULL);
 		_currentCount++;
 		float tElapsed = currentTime - _startTime;
 		std::string s = " - Finished at ";
 		s.append(ctime(&currentTime));
 		char fs[255];
-		sprintf(fs, "%c[%d;%dm  ERROR   %c[%dm",27,1,31,27,0);
-		printf(stringTmpl.c_str(), fs, _currentCount, "Needed",int(tElapsed/60), int(tElapsed)%60, s.c_str());
+		sprintf(fs, "%c[%d;%dm  ERROR   %c[%dm", 27, 1, 31, 27, 0);
+		printf(stringTmpl.c_str(), fs, _currentCount, "Needed",
+				int(tElapsed / 60), int(tElapsed) % 60, s.c_str());
 	}
 
 };

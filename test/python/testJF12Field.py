67bcac825c92c565aaff46f9c71af0ee8c704744
DavidWalz <walz@physik.rwth-aachen.de>
2013-01-09 19:15:47 +0100

    refactor JF12

--- test/python/testJF12Field.py
+++ test/python/testJF12Field.py
@@ -8,24 +8,24 @@ bField = JF2012Field()
 #bField.randomTurbulent()
 
 N = 241
-x = (linspace(-20, 20, N, endpoint=True)) * kpc
-y = (linspace(-20, 20, N, endpoint=True)) * kpc
-z = 0 * kpc
+lx = (linspace(-20, 20, N, endpoint=True))
+z = 0
+B, X, Y = zeros((3, N,N))
 
-B = zeros((N,N))
 for ix in range(N):
   for iy in range(N):
-    b = bField.getField(Vector3d(x[ix], y[iy], z))
-    B[iy, ix] = b.getMag() / gauss # B in [G], rows = y, columns = x
-
-maB = ma.masked_array(B, B==0)
-X, Y = meshgrid(x / kpc, y / kpc)
+    b = bField.getField(Vector3d(lx[ix], lx[iy], z) * kpc)
+    B[ix, iy] = b.getMag() / gauss # B in [G]
+    X[ix, iy] = lx[ix]
+    Y[ix, iy] = lx[iy]
 
 figure()
+maB = ma.masked_array(B, B==0)
 pc = pcolor(X, Y, maB, norm=LogNorm(), vmin=1e-9, vmax=1e-4)
 gca().set_aspect(1)
 cbar = colorbar(pc, shrink=0.8)
 cbar.set_label(r'$\vert \vec{B} \vert$ [G]')
+plot(-8.5, 0, 'wo')
 xlabel('x [kpx]')
 ylabel('y [kpx]')
 show()

db882d4cf9948f98ad8ea77e68140a7f860cd762
DavidWalz <walz@physik.rwth-aachen.de>
2013-01-17 17:10:53 +0100

    add missing files

---
+++ src/PhotonBackground.cpp
@@ -0,0 +1,21 @@
+#include "mpc/PhotonBackground.h"
+#include "mpc/Common.h"
+
+#include <vector>
+#include <math.h>
+
+namespace mpc {
+
+// Overall redshift scaling of the Kneiske IRB (see data/PhotonField/KneiskeIRB.py and Kneiske et al. 2004, astro-ph/0309141)
+double a[9] = { 0, 0.2, 0.4, 0.6, 1, 2, 3, 4, 5 };
+static std::vector<double> zKneiske(a, a + sizeof(a) / sizeof(double));
+double b[9] = { 1, 1.6937, 2.5885, 3.6178, 5.1980, 7.3871, 8.5471, 7.8605, 0 };
+static std::vector<double> sKneiske(b, b + sizeof(b) / sizeof(double));
+
+double photonFieldScaling(int photonField, double z) {
+	if (photonField == IRB)
+		return interpolate(z, zKneiske, sKneiske);
+	return pow(z + 1, 3); // CMB-like scaling
+}
+
+} // namespace mpc

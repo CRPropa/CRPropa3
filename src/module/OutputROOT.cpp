5d64be4c8cc0c5d68dfabd43400a9c485e6f605f
DavidWalz <walz@physik.rwth-aachen.de>
2013-01-30 16:00:44 +0100

    fix output bug: CRPropa2 - like and ROOT output modules did not remove the "Detected" flag leading to output in each step changed XML steering behaviour back: Spheres around source do not inactivate candidates refactored observer code

--- src/module/OutputROOT.cpp
+++ src/module/OutputROOT.cpp
@@ -6,7 +6,7 @@ namespace mpc {
 
 /////////////////////// ROOT EVENT OUTPUT 1D //////////////////////////////////
 ROOTEventOutput1D::ROOTEventOutput1D(std::string filename) {
-        TThread::Lock();
+	TThread::Lock();
 	ROOTFile = new TFile(filename.c_str(), "RECREATE",
 			"CRPropa output data file");
 	Ntuple =
@@ -16,50 +16,50 @@ ROOTEventOutput1D::ROOTEventOutput1D(std::string filename) {
 }
 
 ROOTEventOutput1D::~ROOTEventOutput1D() {
-        TThread::Lock();
+	TThread::Lock();
 	ROOTFile->Write();
 	ROOTFile->Close();
-        TThread::UnLock();
+	TThread::UnLock();
 }
 
 void ROOTEventOutput1D::process(Candidate *c) const {
-	if (c->isActive())
+	if (not (c->hasProperty("Detected")))
 		return;
-	if (c->hasProperty("Detected")) {
-	        TThread::Lock();
+
+	c->removeProperty("Detected");
+
+	TThread::Lock();
 #pragma omp critical
-		{
-			Ntuple->Fill(c->current.getId(), c->initial.getId(),
-					c->initial.getPosition().getX() / Mpc, c->getRedshift(),
-					c->initial.getEnergy() / EeV,
-					c->getTrajectoryLength() / Mpc,
-					c->current.getEnergy() / EeV);
-		}
-		TThread::UnLock();
+	{
+		Ntuple->Fill(c->current.getId(), c->initial.getId(),
+				c->initial.getPosition().getX() / Mpc, c->getRedshift(),
+				c->initial.getEnergy() / EeV, c->getTrajectoryLength() / Mpc,
+				c->current.getEnergy() / EeV);
 	}
+	TThread::UnLock();
 }
 ////////////////////////////////////////////////////////////////////////////////
 
 /////////////////////// ROOT TRAJECTORY OUTPUT 1D //////////////////////////////
 ROOTTrajectoryOutput1D::ROOTTrajectoryOutput1D(std::string filename) {
-        TThread::Lock();
+	TThread::Lock();
 	ROOTFile = new TFile(filename.c_str(), "RECREATE",
 			"CRPropa output data file");
 	//  Ntuple = new TNtuple("traj","CRPropa 1D trajectories","Particle_Type:Initial_Type:Time_Mpc:Position_Mpc:Energy_EeV");
 	Ntuple = new TNtuple("traj", "CRPropa 1D trajectories",
 			"Particle_Type:Initial_Type:Time_Mpc:Position_Mpc:Energy_EeV");
-        TThread::UnLock();
+	TThread::UnLock();
 }
 
 ROOTTrajectoryOutput1D::~ROOTTrajectoryOutput1D() {
-        TThread::Lock();
+	TThread::Lock();
 	ROOTFile->Write();
 	ROOTFile->Close();
-        TThread::UnLock();
+	TThread::UnLock();
 }
 
 void ROOTTrajectoryOutput1D::process(Candidate *c) const {
-        TThread::Lock();
+	TThread::Lock();
 #pragma omp critical
 	{
 		Ntuple->Fill(c->current.getId(), c->initial.getId(),
@@ -67,73 +67,76 @@ void ROOTTrajectoryOutput1D::process(Candidate *c) const {
 				c->current.getPosition().getX() / Mpc,
 				c->current.getEnergy() / EeV);
 	}
-        TThread::UnLock();
+	TThread::UnLock();
 }
 ////////////////////////////////////////////////////////////////////////////////
 
 /////////////////////// ROOT EVENT OUTPUT 3D //////////////////////////////////
 ROOTEventOutput3D::ROOTEventOutput3D(std::string filename) {
-        TThread::Lock();
+	TThread::Lock();
 	ROOTFile = new TFile(filename.c_str(), "RECREATE",
 			"CRPropa output data file");
 	Ntuple =
 			new TNtuple("events", "CRPropa 3D events",
 					"Particle_Type:Initial_Type:Initial_Position_X_Mpc:Initial_Position_Y_Mpc:Initial_Position_Z_Mpc:Initial_Momentum_E_EeV:Initial_Momentum_theta:Initial_Momentum_phi:Time_Mpc:Position_X_Mpc:Position_Y_Mpc:Position_Z_Mpc:Momentum_E_EeV:Momentum_theta:Momentum_phi");
-        TThread::UnLock();
+	TThread::UnLock();
 }
 
 ROOTEventOutput3D::~ROOTEventOutput3D() {
-        TThread::Lock();
+	TThread::Lock();
 	ROOTFile->Write();
 	ROOTFile->Close();
-        TThread::UnLock();
+	TThread::UnLock();
 }
 
 void ROOTEventOutput3D::process(Candidate *c) const {
-	if (c->hasProperty("Detected")) {
-        TThread::Lock();
+	if (not (c->hasProperty("Detected")))
+		return;
+
+	c->removeProperty("Detected");
+
+	TThread::Lock();
 #pragma omp critical
-		{
-			Ntuple->Fill(c->current.getId(), c->initial.getId(),
-					c->initial.getPosition().getX() / Mpc,
-					c->initial.getPosition().getY() / Mpc,
-					c->initial.getPosition().getZ() / Mpc,
-					c->initial.getEnergy() / EeV,
-					c->initial.getDirection().getTheta(),
-					c->initial.getDirection().getPhi(),
-					c->getTrajectoryLength() / Mpc,
-					c->current.getPosition().getX() / Mpc,
-					c->current.getPosition().getY() / Mpc,
-					c->current.getPosition().getZ() / Mpc,
-					c->current.getEnergy() / EeV,
-					c->current.getDirection().getTheta(),
-					c->current.getDirection().getPhi());
-		}
-        TThread::UnLock();
+	{
+		Ntuple->Fill(c->current.getId(), c->initial.getId(),
+				c->initial.getPosition().getX() / Mpc,
+				c->initial.getPosition().getY() / Mpc,
+				c->initial.getPosition().getZ() / Mpc,
+				c->initial.getEnergy() / EeV,
+				c->initial.getDirection().getTheta(),
+				c->initial.getDirection().getPhi(),
+				c->getTrajectoryLength() / Mpc,
+				c->current.getPosition().getX() / Mpc,
+				c->current.getPosition().getY() / Mpc,
+				c->current.getPosition().getZ() / Mpc,
+				c->current.getEnergy() / EeV,
+				c->current.getDirection().getTheta(),
+				c->current.getDirection().getPhi());
 	}
+	TThread::UnLock();
 }
 ////////////////////////////////////////////////////////////////////////////////
 
 /////////////////////// ROOT TRAJECTORY OUTPUT 3D //////////////////////////////
 ROOTTrajectoryOutput3D::ROOTTrajectoryOutput3D(std::string filename) {
-        TThread::Lock();
+	TThread::Lock();
 	ROOTFile = new TFile(filename.c_str(), "RECREATE",
 			"CRPropa output data file");
 	Ntuple =
 			new TNtuple("traj", "CRPropa 3D trajectories",
 					"Particle_Type:Initial_Type:Time_Mpc:Position_X_Mpc:Position_Y_Mpc:Position_Z_Mpc:Direction_X:Direction_Y:Direction_Z:Energy_EeV");
-        TThread::UnLock();
+	TThread::UnLock();
 }
 
 ROOTTrajectoryOutput3D::~ROOTTrajectoryOutput3D() {
-        TThread::Lock();
+	TThread::Lock();
 	ROOTFile->Write();
 	ROOTFile->Close();
-        TThread::UnLock();
+	TThread::UnLock();
 }
 
 void ROOTTrajectoryOutput3D::process(Candidate *c) const {
-        TThread::Lock();
+	TThread::Lock();
 #pragma omp critical
 	{
 		Ntuple->Fill(c->current.getId(), c->initial.getId(),
@@ -145,9 +148,10 @@ void ROOTTrajectoryOutput3D::process(Candidate *c) const {
 				c->current.getDirection().getY(),
 				c->current.getDirection().getZ(), c->current.getEnergy() / EeV);
 	}
-        TThread::UnLock();
+	TThread::UnLock();
 }
 ////////////////////////////////////////////////////////////////////////////////
 
 }// namespace mpc
+
 #endif // MPC_HAVE_ROOT

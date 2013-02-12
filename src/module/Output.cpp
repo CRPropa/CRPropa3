e547d19957f5bba6ada50dfcfc799d4d9b83c8b9
DavidWalz <walz@physik.rwth-aachen.de>
2013-02-12 15:01:46 +0100

    ASCII Output: output of direction as cartesian coordinates instead of angles

--- src/module/Output.cpp
+++ src/module/Output.cpp
@@ -28,13 +28,14 @@ std::string ShellOutput::getDescription() const {
 TrajectoryOutput::TrajectoryOutput(std::string name) {
 	setDescription("Trajectory output");
 	fout.open(name.c_str());
-	fout << "# D\tID\E\tX\tY\tZ\tPhi\tTheta\n";
+	fout << "# D\tID\tE\tX\tY\tZ\tPx\tPy\tPz\n";
 	fout << "#\n";
-	fout << "# D          Comoving trajectory length\n";
-	fout << "# ID         Particle type\n";
-	fout << "# E          Energy [EeV]\n";
-	fout << "# X, Y, Z    Position [Mpc]\n";
-	fout << "# Phi, Theta Direction\n";
+	fout << "# D           Trajectory length\n";
+	fout << "# ID          Particle type (PDG MC numbering scheme)\n";
+	fout << "# E           Energy [EeV]\n";
+	fout << "# X, Y, Z     Position [Mpc]\n";
+	fout << "# Px, Py, Pz  Heading (unit vector of momentum)\n";
+	fout << "#\n";
 }
 
 TrajectoryOutput::~TrajectoryOutput() {
@@ -45,13 +46,13 @@ void TrajectoryOutput::process(Candidate *c) const {
 	char buffer[1024];
 	size_t p = 0;
 
-	p += sprintf(buffer + p, "%9.4f\t", c->getTrajectoryLength() / Mpc);
+	p += sprintf(buffer + p, "%8.3f\t", c->getTrajectoryLength() / Mpc);
 	p += sprintf(buffer + p, "%10i\t", c->current.getId());
 	p += sprintf(buffer + p, "%8.4f\t", c->current.getEnergy() / EeV);
 	Vector3d pos = c->current.getPosition() / Mpc;
 	p += sprintf(buffer + p, "%8.4f\t%8.4f\t%8.4f\t", pos.x, pos.y, pos.z);
 	const Vector3d &dir = c->current.getDirection();
-	p += sprintf(buffer + p, "%7.4f\t%7.4f\n", dir.getPhi(), dir.getTheta());
+	p += sprintf(buffer + p, "%8.5f\t%8.5f\t%8.5f\n", dir.x, dir.y, dir.z);
 
 #pragma omp critical
 	{
@@ -65,20 +66,15 @@ ConditionalOutput::ConditionalOutput(std::string fname, std::string cond) :
 	setDescription(
 			"Conditional output, condition: " + cond + ", filename: " + fname);
 	fout.open(fname.c_str());
-	fout << "# ID\tE\tY\tY\tZ\tPhi\tTheta\tD\tID0\tE0\tX0\tY0\tZ0\tPhi0\tTheta0\n";
+	fout << "# D\tID\tID0\tE\tE0\tX\tY\tZ\tX0\tY0\tZ0\tPx\tPy\tPz\tP0x\tP0y\tP0z\n";
 	fout << "#\n";
-	fout << "# Final state:\n";
-	fout << "#    ID           Particle type\n";
-	fout << "#    E            Energy [EeV]\n";
-	fout << "#    X, Y, Z      Position [Mpc]\n";
-	fout << "#    Phi, Theta   Direction\n";
-	fout << "#    D            Comoving trajectory length [Mpc]\n";
+	fout << "# D           Trajectory length [Mpc]\n";
+	fout << "# ID          Particle type (PDG MC numbering scheme)\n";
+	fout << "# E           Energy [EeV]\n";
+	fout << "# X, Y, Z     Position [Mpc]\n";
+	fout << "# Px, Py, Pz  Heading (unit vector of momentum)\n";
+	fout << "# Initial state: ID0, E0, ...\n";
 	fout << "#\n";
-	fout << "# Initial state:\n";
-	fout << "#    I0           Particle type\n";
-	fout << "#    E0           Energy [EeV]\n";
-	fout << "#    X0, Y0, Z0   Position [Mpc]\n";
-	fout << "#    Phi0, Theta0 Direction\n";
 }
 
 ConditionalOutput::~ConditionalOutput() {
@@ -94,19 +90,19 @@ void ConditionalOutput::process(Candidate *c) const {
 	char buffer[256];
 	size_t p = 0;
 
+	p += sprintf(buffer + p, "%8.3f\t", c->getTrajectoryLength() / Mpc);
 	p += sprintf(buffer + p, "%10i\t", c->current.getId());
-	p += sprintf(buffer + p, "%8.4f\t", c->current.getEnergy() / EeV);
-	const Vector3d &pos = c->current.getPosition() / Mpc;
-	p += sprintf(buffer + p, "%9.4f\t%9.4f\t%9.4f\t", pos.x, pos.y, pos.z);
-	const Vector3d &dir = c->current.getDirection();
-	p += sprintf(buffer + p, "%7.4f\t%7.4f\t", dir.getPhi(), dir.getTheta());
-	p += sprintf(buffer + p, "%9.4f\t", c->getTrajectoryLength() / Mpc);
 	p += sprintf(buffer + p, "%10i\t", c->initial.getId());
+	p += sprintf(buffer + p, "%8.4f\t", c->current.getEnergy() / EeV);
 	p += sprintf(buffer + p, "%8.4f\t", c->initial.getEnergy() / EeV);
-	const Vector3d &ipos = c->initial.getPosition() / Mpc;
+	Vector3d pos = c->current.getPosition() / Mpc;
+	p += sprintf(buffer + p, "%9.4f\t%9.4f\t%9.4f\t", pos.x, pos.y, pos.z);
+	Vector3d ipos = c->initial.getPosition() / Mpc;
 	p += sprintf(buffer + p, "%9.4f\t%9.4f\t%9.4f\t", ipos.x, ipos.y, ipos.z);
-	const Vector3d &idir = c->initial.getDirection();
-	p += sprintf(buffer + p, "%7.4f\t%7.4f\n", idir.getPhi(), idir.getTheta());
+	Vector3d dir = c->current.getDirection();
+	p += sprintf(buffer + p, "%8.5f\t%8.5f\t%8.5f\t", dir.x, dir.y, dir.z);
+	Vector3d idir = c->initial.getDirection();
+	p += sprintf(buffer + p, "%8.5f\t%8.5f\t%8.5f\n", idir.x, idir.y, idir.z);
 
 #pragma omp critical
 	{

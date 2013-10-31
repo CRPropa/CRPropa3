e00f25f8d823fa82a7fcda2214f6fb529c2ac75b
DavidWalz <walz@physik.rwth-aachen.de>
2013-10-31 11:49:20 +0100

    use of randBin to simplify several SourceProperties

--- include/crpropa/Source.h
+++ include/crpropa/Source.h
@@ -24,7 +24,8 @@ public:
  @brief General cosmic ray source
 
  This class is a container for source properties.
- The source prepares a new candidate by passing it to all its source properties to be modified accordingly.
+ The source prepares a new candidate by passing it to all its source properties
+ to be modified accordingly.
  */
 class Source: public Referenced {
 	std::vector<ref_ptr<SourceProperty> > properties;
@@ -37,13 +38,14 @@ public:
  @class SourceList
  @brief List of cosmic ray sources of individual total lumosities.
 
- The SourceList is a source itself. It can be used if several UHECR sources are needed in one simulation.
+ The SourceList is a source itself. It can be used if several sources are
+ needed in one simulation.
  */
 class SourceList: public Source {
 	std::vector<ref_ptr<Source> > sources;
-	std::vector<double> luminosities;
+	std::vector<double> cdf;
 public:
-	void addSource(Source* source, double luminosity = 1);
+	void addSource(Source* source, double weight = 1);
 	ref_ptr<Candidate> getCandidate() const;
 };
 
@@ -60,13 +62,13 @@ public:
 
 /**
  @class SourceMultipleParticleTypes
- @brief Multiple particle types with individual (relative) total abundances
+ @brief Multiple particle types with individual relative abundances
  */
 class SourceMultipleParticleTypes: public SourceProperty {
-	std::vector<int> ids; /**< particle ids */
-	std::vector<double> abundances; /**< (relative) total abundances */
+	std::vector<int> particleTypes;
+	std::vector<double> cdf;
 public:
-	void add(int id, double abundance = 1);
+	void add(int id, double weight = 1);
 	void prepare(ParticleState &particle) const;
 };
 
@@ -90,49 +92,26 @@ class SourcePowerLawSpectrum: public SourceProperty {
 	double Emax;
 	double index;
 public:
-	/** Constructor
-	 @param Emin	minimum energy
-	 @param Emax	maximum energy
-	 @param index	differential spectral index
-	 */
-	SourcePowerLawSpectrum(double Emin, double Emax, double differentialIndex);
-	/** Set particle with a random energy from a power law distribution */
+	SourcePowerLawSpectrum(double Emin, double Emax, double index);
 	void prepare(ParticleState &particle) const;
 };
 
 /**
  @class SourceComposition
- @brief Nuclei with given abundances and a uniform power law spectrum between Emin and Z * Rmax
+ @brief Multiple nuclei with power law spectrum between Emin and Z * Rmax
+
+ See Allard et al. 2006, DOI 10.1088/1475-7516/2006/09/005
  */
 class SourceComposition: public SourceProperty {
 	double Emin;
 	double Rmax;
 	double index;
-	std::vector<int> isotope; /**< isotope id */
-	std::vector<double> abundance; /**< relative abundance of source isotopes at equal energies */
-	std::vector<double> probability; /**< cumulative probability of source isotopes */
-	void normalize();
-	double getSpectrumIntegral(int Z) const;
-
+	std::vector<int> nuclei;
+	std::vector<double> cdf;
 public:
-	/** Constructor
-	 @param Emin	minimum energy for cosmic rays
-	 @param Rmax	maximum rigidity for cosmic rays
-	 @param index	differential spectral index
-	 */
 	SourceComposition(double Emin, double Rmax, double index);
-	/** Add a species to the composition
-	 @param id 			particle id
-	 @param abundance	absolute or relative abundance at a fixed value of energy/nucleons
-	 */
 	void add(int id, double abundance);
-	/** Add a species to the composition
-	 @param A 			mass number
-	 @param Z			charge number
-	 @param abundance	absolute or relative abundance at a fixed value of energy/nucleons
-	 */
 	void add(int A, int Z, double abundance);
-	/** Randomly select a species and energy */
 	void prepare(ParticleState &particle) const;
 };
 
@@ -153,9 +132,9 @@ public:
  */
 class SourceMultiplePositions: public SourceProperty {
 	std::vector<Vector3d> positions;
-	std::vector<double> luminosities;
+	std::vector<double> cdf;
 public:
-	void add(Vector3d position, double luminosity = 1);
+	void add(Vector3d position, double weight = 1);
 	void prepare(ParticleState &particle) const;
 };
 
@@ -225,14 +204,9 @@ public:
 /**
  @class SourceDensityGrid
  @brief Random source positions from a density grid
-
- This source property takes a density grid to compute random initial positions.
- To dial a source position, first a bin is drawn following the density distribution.
- Then a random position is drawn from a uniform distribution in the bin.
  */
 class SourceDensityGrid: public SourceProperty {
 	ref_ptr<ScalarGrid> grid;
-	float sumDensity;
 public:
 	SourceDensityGrid(ref_ptr<ScalarGrid> densityGrid);
 	void prepare(ParticleState &particle) const;
@@ -241,14 +215,9 @@ public:
 /**
  @class SourceDensityGrid1D
  @brief Random source positions from a 1D density grid
-
- This source property takes a N*1*1 grid to compute random initial positions.
- To dial a source position, first a bin is drawn following the density distribution.
- Then a random position is drawn from a uniform distribution in the bin.
  */
 class SourceDensityGrid1D: public SourceProperty {
 	ref_ptr<ScalarGrid> grid;
-	float sumDensity;
 public:
 	SourceDensityGrid1D(ref_ptr<ScalarGrid> densityGrid);
 	void prepare(ParticleState &particle) const;

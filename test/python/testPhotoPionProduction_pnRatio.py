6d2e00b34eeddb1a2f4f324ebcee7159110410ca
DavidWalz <walz@physik.rwth-aachen.de>
2014-01-13 18:31:06 +0100

    fix script for testing photo-pion pn-ratio remove testPhotoPionProduction.py and testPhotoDisintegration.py which were using a circular argument for the test and are not easily adapted to new stochastic interaction interface.

--- test/python/testPhotoPionProduction_pnRatio.py
+++ test/python/testPhotoPionProduction_pnRatio.py
@@ -5,27 +5,25 @@ from crpropa import *
 from pylab import *
 
 
-spp = SophiaPhotoPionProduction()
+ppp = PhotoPionProduction()
 c = Candidate()
-s = InteractionState()
 
 def getPNRatio(E):
-	nP, nN = 0, 0
-	for i in range(2000):
-		c.current.setId(nucleusId(1, 1))
-		c.current.setEnergy(E * EeV)
-		spp.setNextInteraction(c, s)
-		spp.performInteraction(c)
-		if c.current.getId() == nucleusId(1,1):
-			nP += 1.
-		if c.current.getId() == nucleusId(1,0):
-			nN += 1.
-	return nP / nN
+    nP, nN = 0., 0.
+    for i in range(2000):
+        c.current.setId(nucleusId(1, 1))
+        c.current.setEnergy(E * EeV)
+        ppp.performInteraction(c, 1)
+        if c.current.getId() == nucleusId(1,1):
+            nP += 1
+        if c.current.getId() == nucleusId(1,0):
+            nN += 1
+    return nP / nN
 
 E = logspace(log10(50), 4, 20)
 Rpn = zeros(20)
 for i in range(20):
-	Rpn[i] = getPNRatio(E[i])
+    Rpn[i] = getPNRatio(E[i])
 
 figure()
 plot(E, Rpn)

dd03bc844bb438655202120ba25eaed389944a84
Gero Müller <post@geromueller.de>
2014-04-30 15:23:22 +0200

    add PhotonOutput1D module

---
+++ include/crpropa/module/PhotonOutput1D.h
@@ -0,0 +1,24 @@
+#ifndef CRPROPA_PHOTON_OUTPUT_H
+#define CRPROPA_PHOTON_OUTPUT_H
+
+#include "crpropa/Module.h"
+
+#include <memory>
+#include <fstream>
+
+namespace crpropa {
+
+class PhotonOutput1D: public Module {
+private:
+	std::string filename;
+	mutable std::ofstream output;
+public:
+	PhotonOutput1D(const std::string &filename);
+	~PhotonOutput1D();
+	void process(Candidate *candidate) const;
+	std::string getDescription() const;
+};
+
+} // namespace crpropa
+
+#endif // CRPROPA_PHOTON_OUTPUT_H
---
+++ src/module/PhotonOutput1D.cpp
@@ -0,0 +1,57 @@
+#include "crpropa/module/PhotonOutput1D.h"
+
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+
+using namespace std;
+
+namespace crpropa {
+
+PhotonOutput1D::PhotonOutput1D(const string &filename) :
+		filename(filename), output(filename.c_str()) {
+	output << "# iE\tiID\tE\tID\tR\tz\n";
+	output << "#\n";
+	output << "# iE          Energy [EeV] of parent particle\n";
+	output << "# iID         Id of parent particle\n";
+	output << "# E           Energy [EeV]\n";
+	output << "# R           Distance [Mpc]\n";
+	output << "# z           Redshift\n";
+	output << "#\n";
+}
+
+PhotonOutput1D::~PhotonOutput1D() {
+}
+
+void PhotonOutput1D::process(Candidate *candidate) const {
+	if (candidate->current.getId() != 22)
+		return;
+
+	char buffer[1024];
+	size_t p = 0;
+
+	p += sprintf(buffer + p, "%8.4f\t", candidate->created.getEnergy() / EeV);
+	p += sprintf(buffer + p, "%10i\t", candidate->created.getId());
+
+	p += sprintf(buffer + p, "%8.4f\t", candidate->current.getEnergy() / EeV);
+	p += sprintf(buffer + p, "%10i\t", candidate->current.getId());
+	p += sprintf(buffer + p, "%8.4f\t",
+			candidate->current.getPosition().getR() / Mpc);
+	p += sprintf(buffer + p, "%8.4f\n", candidate->getRedshift());
+
+#pragma omp critical
+	{
+		output.write(buffer, p);
+		//output.flush();
+	}
+
+	candidate->setActive(false);
+}
+
+string PhotonOutput1D::getDescription() const {
+	std::stringstream s;
+	s << "PhotonOutput1D: Output file = " << filename;
+	return s.str();
+}
+
+} // namespace crpropa

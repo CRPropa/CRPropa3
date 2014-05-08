5720c6dad00e1704d3f54f009925b5ec6327062b
DavidWalz <walz@physik.rwth-aachen.de>
2014-05-08 15:27:35 +0200

    PhotonOutput1D: output electrons and positrons as well

--- src/module/PhotonOutput1D.cpp
+++ src/module/PhotonOutput1D.cpp
@@ -10,15 +10,15 @@ namespace crpropa {
 
 PhotonOutput1D::PhotonOutput1D(const string &filename) :
 		filename(filename), output(filename.c_str()) {
-	output << "# sE\tsID\tpE\tpID\tE\tR\tz\n";
+	output << "#ID\tE\tz\tiE\tiID\tpE\tpID\n";
 	output << "#\n";
+	output << "# ID          Id of particle (photon, electron, positron)\n";
+	output << "# E           Energy [EeV]\n";
+	output << "# z           Redshift\n";
 	output << "# iE          Energy [EeV] of source particle\n";
 	output << "# iID         Id of source particle\n";
 	output << "# pE          Energy [EeV] of parent particle\n";
 	output << "# pID         Id of parent particle\n";
-	output << "# E           Energy [EeV]\n";
-	output << "# R           Distance point of creation [Mpc]\n";
-	output << "# z           Redshift\n";
 	output << "#\n";
 }
 
@@ -26,22 +26,22 @@ PhotonOutput1D::~PhotonOutput1D() {
 }
 
 void PhotonOutput1D::process(Candidate *candidate) const {
-	if (candidate->current.getId() != 22)
+	int pid = candidate->current.getId();
+	if ((pid != 22) and (abs(pid) != 11))
 		return;
 
 	char buffer[1024];
 	size_t p = 0;
+
+	p += sprintf(buffer + p, "%4i\t", pid);
+	p += sprintf(buffer + p, "%8.4f\t", candidate->current.getEnergy() / EeV);
+	p += sprintf(buffer + p, "%8.4f\n", candidate->getRedshift());
 
 	p += sprintf(buffer + p, "%8.4f\t", candidate->source.getEnergy() / EeV);
 	p += sprintf(buffer + p, "%10i\t", candidate->source.getId());
 
 	p += sprintf(buffer + p, "%8.4f\t", candidate->created.getEnergy() / EeV);
 	p += sprintf(buffer + p, "%10i\t", candidate->created.getId());
-
-	p += sprintf(buffer + p, "%8.4f\t", candidate->current.getEnergy() / EeV);
-	p += sprintf(buffer + p, "%8.4f\t",
-			candidate->current.getPosition().getR() / Mpc);
-	p += sprintf(buffer + p, "%8.4f\n", candidate->getRedshift());
 
 #pragma omp critical
 	{

d840526b20ee87c8fab83bbf14286df4931af9eb
DavidWalz <walz@physik.rwth-aachen.de>
2014-07-27 21:37:08 +0200

    add more photon fields

--- include/crpropa/PhotonBackground.h
+++ include/crpropa/PhotonBackground.h
@@ -4,8 +4,9 @@
 namespace crpropa {
 
 // Photon fields
+// The default IRB model is that of Kneiske et al. 2004
 enum PhotonField {
-	CMB, IRB
+	CMB, IRB, IRB_Kneiske04, IRB_Kneiske10, IRB_Franceschini08, IRB_Stecker05
 };
 
 // Returns overall comoving scaling factor
--- src/PhotonBackground.cpp
+++ src/PhotonBackground.cpp
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <math.h>
+#include <stdexcept>
 
 namespace crpropa {
 
@@ -10,13 +11,23 @@ namespace crpropa {
 // The scaling is calculated in data-tools/PhotonField/Kneiske2004_IRB_scaling.py
 double a[9] = { 0, 0.2, 0.4, 0.6, 1, 2, 3, 4, 5 };
 static std::vector<double> zKneiske(a, a + sizeof(a) / sizeof(double));
-double b[9] = {1., 0.9749867, 0.93999977, 0.88430409, 0.64952017, 0.27170436, 0.130244, 0.05971749, 0.};
+double b[9] = { 1., 0.9749867, 0.93999977, 0.88430409, 0.64952017, 0.27170436,
+		0.130244, 0.05971749, 0. };
 static std::vector<double> sKneiske(b, b + sizeof(b) / sizeof(double));
 
 double photonFieldScaling(PhotonField photonField, double z) {
-	if (photonField == IRB)
+	switch (photonField) {
+	case CMB:
+		return 1.; // CMB-like scaling
+	case IRB:
+	case IRB_Kneiske04:
+	case IRB_Kneiske10:
+	case IRB_Stecker05:
+	case IRB_Franceschini08:
 		return interpolate(z, zKneiske, sKneiske);
-	return 1.; // CMB-like scaling
+	default:
+		throw std::runtime_error("PhotonField: unknown photon background");
+	}
 }
 
 } // namespace crpropa

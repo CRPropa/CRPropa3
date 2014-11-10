5d8a0a3ab37a7ddca9f99dc84c3e75a70a419fc6
Tobias Winchen <tobias.winchen@rwth-aachen.de>
2014-11-10 14:17:54 +0100

    Check for numpy version > 1.6.0

--- checkNumpy.py
+++ checkNumpy.py
@@ -1,8 +1,14 @@
-#Returns the nupy include path if nupy is available
+#Returns the nupy include path if numpy version > 1.6 is available
 #Silently exits with -1 otherwise
 import sys
 try:
   import numpy
+  
+  from pkg_resources import parse_version
+  if parse_version(numpy.__version__) < parse_version('1.6.0'):
+    sys.exit(-1)
+  
   sys.stdout.write(numpy.get_include())
+
 except ImportError:
   sys.exit(-1)

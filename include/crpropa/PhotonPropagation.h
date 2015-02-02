134cc104c364b09dfd8b306838ae5c81628a6c00
Tobias Winchen <tobias.winchen@rwth-aachen.de>
2015-02-02 14:55:17 +0100

    Added comments

--- include/crpropa/PhotonPropagation.h
+++ include/crpropa/PhotonPropagation.h
@@ -7,13 +7,19 @@
 namespace crpropa {
 
 
+/** Use EleCa to propagate the photons recorded in inputfile.
+ *  Propagation is stopped if the particles reach the observer or their energy
+ *  dropps below lowerEnergyThreshold [J].
+ * **/
 void EleCaPropagation(const std::string &inputfile,
 		const std::string &outputfile,
 		bool showProgress=true,
-		double lowerEnergyThreshold = 1E16,
+		double lowerEnergyThreshold = 0.001602176487,
     const std::string &background = "ALL");
 
 
+/** Use Dint to calculate the observed sprectrum fom the photons recorded in inputfile.
+ * **/
 void DintPropagation(const std::string &inputfile,
 		const std::string &outputfile, int IRFlag = 2, int RadioFlag = 2,
 		double Zmax = 5);

add92925b4fef9b2f15a97efba903e791479f1d3
DavidWalz <walz@physik.rwth-aachen.de>
2015-06-03 00:19:48 +0200

    Update EPP test script, close #26

--- test/python/testElectronPairProduction.py
+++ test/python/testElectronPairProduction.py
@@ -6,37 +6,34 @@ from pylab import *
 
 
 def compare(dataFileName, photonField, plotFileName):
-    A = genfromtxt(dataFileName, comments="#", unpack=True)
-    figure()
-    plot(A[0], A[1], label='Datatable')
-
     epp = ElectronPairProduction(photonField)
-
     c = Candidate()
     c.current.setId(nucleusId(1,1))
     c.setCurrentStep(1 * Mpc)
 
-    N = 50
-    E = logspace(16,23,N)
-    dE = zeros(N)
+    d = genfromtxt(dataFileName, unpack=True)
+    gamma = 10**d[0]  # Lorentz factor
+    loss1 = d[1]  # loss length
+    loss2 = zeros_like(loss1)
 
-    for i in range(N):
-        c.current.setEnergy(E[i] * eV)
+    for i, g in enumerate(gamma):
+        c.current.setLorentzFactor(g)
         epp.process(c)
-        dE[i] = (E[i] - c.current.getEnergy() / eV)
+        loss2[i] = (g - c.current.getLorentzFactor()) / g
 
-    plot(E, dE,'k+', label='Simulated using datatable', linewidth=2, markeredgewidth=2)
-
-    xlabel('Energy [eV]')
-    ylabel('Energy Loss Rate [eV / Mpc]')
+    figure()
+    plot(gamma, loss1, label='Datatable')
+    plot(gamma, loss2,'k+', label='Simulated', linewidth=2, markeredgewidth=2)
+    xlabel(r'$\log_{10}(\gamma)$')
+    ylabel(r'Loss length $\frac{1}{\gamma} d\gamma/dx$ [1/Mpc]')
     legend(loc='lower right')
     grid()
     loglog()
-
     savefig(plotFileName, bbox_inches='tight')
 
 
-compare(getDataPath('pair_rate_CMB.txt'), CMB, 'ElectronPairProduction_CMB.png')
-compare(getDataPath('pair_rate_IRB.txt'), IRB, 'ElectronPairProduction_IRB.png')
-
+# CMB
+compare(getDataPath('epp_CMB.txt'), CMB, 'ElectronPairProduction_CMB.png')
+# Dominguez '11 EBL
+compare(getDataPath('epp_IRB_Dominguez11.txt'), IRB_Dominguez11, 'ElectronPairProduction_IRB_Dominguez11.png')
 show()

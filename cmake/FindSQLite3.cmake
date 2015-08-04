d18b4a033714580e444c2014b2a977a14383390e
Rafael Alves Batista <8rafael@gmail.com>
2015-08-04 15:24:07 -0300

    minor fixes to find header file

--- cmake/FindSQLite3.cmake
+++ cmake/FindSQLite3.cmake
@@ -1,14 +1,15 @@
-# SQLITE3_INCLUDE_DIR = path to Quimby directory
-# SQLITE3_LIBRARY = libQuimby.a
-# SQLITE3_FOUND = true if Quimby is found
+# SQLITE3_INCLUDE_DIR = path to SAGA directory
+# SQLITE3_LIBRARY = libsaga.so
+# SQLITE3_FOUND = true if SAGA is found
 
 find_path(SQLITE3_INCLUDE_DIR sqlite3.h)
 find_library(SQLITE3_LIBRARY libsqlite3)
 
 set(SQLITE3_FOUND FALSE)
 if(SQLITE3_INCLUDE_DIR AND SQLITE3_LIBRARY)
-    set(QUIMBY_FOUND TRUE)
+    set(SAGA_FOUND TRUE)
     MESSAGE(STATUS "SQLite3: Found!")
+    include_directories(${SQLITE3_INCLUDE_DIR})
 else()
     MESSAGE(STATUS "SQLite3: NOT Found!")    
 endif()
@@ -16,4 +17,4 @@ endif()
 MESSAGE(STATUS "  Include:     ${SQLITE3_INCLUDE_DIR}")
 MESSAGE(STATUS "  Library:     ${SQLITE3_LIBRARY}")
 
-mark_as_advanced(SQLITE3_INCLUDE_DIR SQLITE3_LIBRARY QUIMBY_FOUND)
+mark_as_advanced(SQLITE3_INCLUDE_DIR SQLITE3_LIBRARY SAGA_FOUND)

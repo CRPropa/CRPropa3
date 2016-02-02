355f42fc25a678b49406d9ab2873b01d58cc126d
Gero Müller <post@geromueller.de>
2016-02-02 17:56:47 +0100

    refactor Output.

--- include/crpropa/module/TextOutput.h
+++ include/crpropa/module/TextOutput.h
@@ -1,11 +1,9 @@
-#ifndef CRPROPA_OUTPUTTXT_H
-#define CRPROPA_OUTPUTTXT_H
+#ifndef CRPROPA_TEXTOUTPUT_H
+#define CRPROPA_TEXTOUTPUT_H
 
-#include "crpropa/Module.h"
-#include "crpropa/AssocVector.h"
+#include "crpropa/module/Output.h"
 
 #include <fstream>
-#include <bitset>
 
 namespace crpropa {
 
@@ -13,109 +11,31 @@ namespace crpropa {
  @class TextOutput
  @brief Configurable plain text output for cosmic ray information.
  */
-class TextOutput: public Module {
+class TextOutput: public Output {
 protected:
-	double lengthScale, energyScale;
 	std::ostream *out;
 	std::ofstream outfile;
-	std::bitset<64> fields;
 	std::string filename;
-	bool oneDimensional;
-public:
-	enum OutputColumn {
-		TrajectoryLengthColumn,
-		RedshiftColumn,
-		CurrentIdColumn,
-		CurrentEnergyColumn,
-		CurrentPositionColumn,
-		CurrentDirectionColumn,
-		SourceIdColumn,
-		SourceEnergyColumn,
-		SourcePositionColumn,
-		SourceDirectionColumn,
-		CreatedIdColumn,
-		CreatedEnergyColumn,
-		CreatedPositionColumn,
-		CreatedDirectionColumn
-	};
 
-	TextOutput();
-	~TextOutput();
-	TextOutput(std::ostream &out);
-	TextOutput(const std::string &filename);
-	TextOutput(const std::string &filename, const std::string &outputtype);
-	void setEnergyScale(double scale);
-	void setLengthScale(double scale);
-
-	void set(OutputColumn field, bool value);
-	void enable(OutputColumn field);
-	void disable(OutputColumn field);
-	void enableAll();
-	void disableAll();
-	void resetFile();
 	void printHeader();
-	void set1D(bool value);
-	void process(Candidate *candidate) const;
-	void endRun();
+
+public:
+	TextOutput();
+	TextOutput(OutputType outputtype);
+	TextOutput(std::ostream &out);
+	TextOutput(std::ostream &out, OutputType outputtype);
+	TextOutput(const std::string &filename);
+	TextOutput(const std::string &filename, OutputType outputtype);
+	~TextOutput();
 
 	void gzip();
 
+	void process(Candidate *candidate) const;
+	void beginRun();
+	void endRun();
 	std::string getDescription() const;
-};
-
-/**
- @class TrajectoryOutput
- @brief Deprecated! Saves trajectories to plain text file.
- */
-class TrajectoryOutput: public Module {
-	mutable std::ofstream fout;
-public:
-	TrajectoryOutput(std::string filename);
-	~TrajectoryOutput();
-	void process(Candidate *candidate) const;
-	void endRun();
-};
-
-/**
- @class ConditionalOutput
- @brief Deprecated! Saves particles with a given property to a plain text file.
- */
-class ConditionalOutput: public Module {
-	mutable std::ofstream fout;
-	std::string condition;
-public:
-	ConditionalOutput(std::string filename, std::string condition = "Detected");
-	~ConditionalOutput();
-	void process(Candidate *candidate) const;
-	void endRun();
-};
-
-/**
- @class TrajectoryOutput1D
- @brief Deprecated! Saves 1D trajectories to plain text file.
- */
-class TrajectoryOutput1D: public Module {
-	mutable std::ofstream fout;
-public:
-	TrajectoryOutput1D(std::string filename);
-	~TrajectoryOutput1D();
-	void process(Candidate *candidate) const;
-	void endRun();
-};
-
-/**
- @class EventOutput1D
- @brief Deprecated! Records particles that are inactive and have the property 'Detected' to a plain text file.
- */
-class EventOutput1D: public Module {
-	mutable std::ofstream fout;
-public:
-	EventOutput1D(std::string filename);
-	~EventOutput1D();
-	void process(Candidate *candidate) const;
-	void endRun();
 };
 
 } // namespace crpropa
 
-#endif // CRPROPA_OUTPUTTXT_H
+#endif // CRPROPA_TEXTOUTPUT_H

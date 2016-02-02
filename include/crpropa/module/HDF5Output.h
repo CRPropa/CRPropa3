8337442dbe4b8c9e74986ce4c1635e1f4d346c6d
Gero Müller <post@geromueller.de>
2016-02-02 17:57:34 +0100

    add simple HDF5 Output.

---
+++ include/crpropa/module/HDF5Output.h
@@ -0,0 +1,64 @@
+#ifndef CRPROPA_HDF5OUTPUT_H
+#define CRPROPA_HDF5OUTPUT_H
+
+#include "crpropa/module/Output.h"
+#include "stdint.h"
+
+typedef int hid_t;
+
+namespace crpropa {
+
+typedef struct OuputRow {
+	double D;
+	double z;
+	uint64_t ID;
+	double E;
+	double X;
+	double Y;
+	double Z;
+	double Px;
+	double Py;
+	double Pz;
+	uint64_t ID0;
+	double E0;
+	double X0;
+	double Y0;
+	double Z0;
+	double P0x;
+	double P0y;
+	double P0z;
+	uint64_t ID1;
+	double E1;
+	double X1;
+	double Y1;
+	double Z1;
+	double P1x;
+	double P1y;
+	double P1z;
+} OutputRow;
+
+class HDF5Output: public Output {
+
+	hid_t file, sid;
+	hid_t dset, dataspace;
+	mutable size_t count;
+	mutable std::vector<OutputRow> buffer;
+	
+	void open(const std::string &filename);
+	void close();
+	void flush() const;
+public:
+
+	HDF5Output(const std::string &filename);
+	HDF5Output(const std::string &filename, OutputType outputtype);
+	~HDF5Output();
+	
+	void process(Candidate *candidate) const;
+	void beginRun();
+	void endRun();
+	std::string getDescription() const;
+};
+
+} // namespace crpropa
+
+#endif // CRPROPA_HDF5OUTPUT_H

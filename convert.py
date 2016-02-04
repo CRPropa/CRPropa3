b0dd81fe6bf55c8a1aafe17de687d6eba1bdd13e
DavidWalz <walz@physik.rwth-aachen.de>
2016-02-04 19:36:14 +0100

    conversion tool ipynb v4 -> v3

---
+++ convert.py
@@ -0,0 +1,23 @@
+from subprocess import call
+
+
+def convert(fname):
+    # jupyter-nbconvert --to=notebook --nbformat=3 xxx --output=yyy
+    ofname = fname.replace('.v4','.v3')
+    call(['jupyter-nbconvert', '--to=notebook', '--nbformat=3', fname, '--output=%s'%ofname])
+
+files = (
+    'basics/basics.v4.ipynb',
+    'galactic_backtracking/galactic_backtracking.v4.ipynb',
+    'galactic_lensing/lensing_cr.v4.ipynb',
+    'galactic_lensing/lensing_maps.v4.ipynb',
+    'galactic_trajectories/galactic_trajectories.v4.ipynb',
+    'secondaries/neutrinos.v4.ipynb',
+    'secondaries/photons.v4.ipynb',
+    'sim1D/sim1D.v4.ipynb',
+    'trajectories/trajectories.v4.ipynb'
+    )
+
+# convert all notebooks from v4 to v3
+for f in files:
+    convert(f)

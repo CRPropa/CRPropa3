f83ebae48312d145e2084ea4d30623df31b89094
Christopher Heiter <heiter@lx3a25.physik.rwth-aachen.de>
2016-03-18 11:20:52 +0100

    Added module to write EM particles to output file if energy is below certain threshold.

---
+++ include/crpropa/module/PhotonOutputThreshold1D.h
@@ -0,0 +1,28 @@
+#ifndef CRPROPA_PHOTON_OUTPUT_THRESHOLD_1D_H
+#define CRPROPA_PHOTON_OUTPUT_THRESHOLD_1D_H
+
+#include "crpropa/Module.h"
+#include "crpropa/Units.h"
+
+#include <memory>
+#include <fstream>
+
+namespace crpropa {
+
+class PhotonOutputThreshold1D: public Module {
+private:
+	std::string filename;
+	mutable std::ofstream output;
+  mutable double Ethreshold;
+public:
+	PhotonOutputThreshold1D(const std::string &filename);
+	PhotonOutputThreshold1D(const std::string &filename, const double Ethr);
+	~PhotonOutputThreshold1D();
+	void process(Candidate *candidate) const;
+	std::string getDescription() const;
+	void endRun();
+};
+
+} // namespace crpropa
+
+#endif // CRPROPA_PHOTON_OUTPUT_THRESHOLD_1D_H

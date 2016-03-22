ed23efc4bd660053794c3e29488dafaa6e9c6763
Andrej Dundović <andrej@dundovic.com.hr>
2016-03-22 09:49:03 +0100

    Fixes #52 by adding a ptr_ref version of process()

--- include/crpropa/ModuleList.h
+++ include/crpropa/ModuleList.h
@@ -28,6 +28,7 @@ public:
 	const module_list_t &getModules() const;
 
 	void process(Candidate *candidate) const; ///< call process in all modules
+	void process(ref_ptr<Candidate> candidate) const; ///< call process in all modules
 
 	void run(Candidate *candidate, bool recursive = true); ///< run simulation for a single candidate
 	void run(candidate_vector_t &candidates, bool recursive = true); ///< run simulation for a candidate vector

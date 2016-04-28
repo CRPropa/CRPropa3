5654ee7c5764a95e8827fb7d1fdef30674bfe956
DavidWalz <walz@physik.rwth-aachen.de>
2016-04-28 11:24:58 +0200

    PhotonPropagation: fix typos & improve documentation

--- include/crpropa/PhotonPropagation.h
+++ include/crpropa/PhotonPropagation.h
@@ -4,41 +4,50 @@
 #include <string>
 #include <vector>
 
+/**
+ @file
+ @brief Calculation of the electromagnetic cascade outside of CRPropa's module list
+ */
+
 namespace crpropa {
 
+/**
+ Propagate photons, electrons and positrons using the EleCa code.
+ The propagation is stopped when the particles reach the observer or their energy drops below the threshold energy.
+ */
+void ElecaPropagation(
+	const std::string &inputfile,               //!< input in PhotonOutput1D format
+	const std::string &outputfile,              //!< ???
+	bool showProgress = true,                   //!< show a progress bar
+	double lowerEnergyThreshold = 0.8010882435, //!< threshold energy [J], default = 5 EeV
+	double magneticFieldStrength = 1E-13,       //!< magnetic field strength [T], default = 1 nG
+	const std::string &background = "ALL"       //!< photon background string
+	);
 
-/** Use EleCa to propagate the photons recorded in inputfile.
- *  Propagation is stopped if the particles reach the observer or their energy
- *  dropps below lowerEnergyThreshold [J].
- * **/
-void EleCaPropagation(const std::string &inputfile,
-		const std::string &outputfile,
-		bool showProgress=true,
-		double lowerEnergyThreshold = 0.8010882435, // 5 EeV
-		double magneticFieldStrength = 1E-13,				// 1 nG
-    const std::string &background = "ALL");
+/**
+ Calculate the electromagnetic cascade with DINT
+ */
+void DintPropagation(
+	const std::string &inputfile,         //!< input in PhotonOutput1D format
+	const std::string &outputfile,        //!< output photon spectrum
+	double magneticFieldStrength = 1E-13, //!< magnetic field strength [T], default = 1 nG
+	int IRFlag = 4,                       //!< EBL background 0: high, 1: low, 2: Primack, 4: Stecker'06
+	int RadioFlag = 4,                    //!< radio background 0: high, 1: medium, 2: obs, 3: none, 4: Protheroe'96
+	double Zmax = 5,                      //!< ???
+	double aCutcascade_Magfield = 0       //!< ???
+	);
 
-
-/** Use Dint to calculate the observed sprectrum fom the photons recorded in inputfile.
- *  Magneticfield [T], default 1E-13 T = 1 nG
- *  IRFlag 0: High IR Background , 1: Low IR BAckground , 2: Primack Background, 4: Eleca Background (Stecker, Malkan, Scully (2006) arXiv:astro-ph/0510449v4)
- *  RadioFlag 0: High Radio Background , 1: MEd Radio BAckground , 2: Obs. Radio background , 3: No Radio, 4: Eleca Background (Protheroe, Bierman 1996.)
- * **/
-void DintPropagation(const std::string &inputfile,
-		const std::string &outputfile, 
-		double magneticFieldStrength = 1E-13, // 1 nG
-		int IRFlag = 4, int RadioFlag = 4,		
-		double Zmax = 5, double aCutcascade_Magfield = 0);
-
-
-/** Propagate high energy photons using eleca until the crossoverenergy is
- * reached. Then the cascade is continued using dint.
- **/
-void DintElcaPropagation(const std::string &inputfile,
-	const std::string &outputfile, 
-	bool showProgress = true,
-	double crossOverEnergy = 0.08010882435,  // in Joule! 5E17 eV = 0.080 J
-	double magneticFieldStrength = 1E-13, double aCutcascade_Magfield = 0);	 // 1 nG
+/**
+ Propagate photons using EleCa for energies above the crossover energy and DINT below
+ */
+void DintElecaPropagation(
+	const std::string &inputfile,           //!< input in PhotonOutput1D format
+	const std::string &outputfile,          //!< ???
+	bool showProgress = true,               //!< show a progress bar
+	double crossOverEnergy = 0.08010882435, //!< crossover energy [J] between EleCa and DINT, default = 0.5 EeV
+	double magneticFieldStrength = 1E-13,   //!< magnetic field strength [T], default = 1 nG
+	double aCutcascade_Magfield = 0         //!< ???
+	);
 
 } // namespace crpropa
 

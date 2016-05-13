85b502b5b7a016c22821c2c2bf6ba2388de7e860
DavidWalz <walz@physik.rwth-aachen.de>
2016-05-13 15:00:30 +0200

    Implement EMCascade module and remove PhotonDINT + PhotonDINT1D

---
+++ include/crpropa/module/EMCascade.h
@@ -0,0 +1,66 @@
+#ifndef CRPROPA_EMCASCADE_H
+#define CRPROPA_EMCASCADE_H
+
+#include "crpropa/Module.h"
+#include "crpropa/Units.h"
+
+#include <fstream>
+
+namespace crpropa {
+
+/**
+ @class EMCascade
+ @brief Collects and deactivates photons, electrons and positrons. Uses DINT to calculate the EM cascade.
+ */
+class EMCascade: public Module {
+private:
+	// fixed energy binning of DINT
+	const int nE = 170;
+	const double logEmin = 7;
+	const double logEmax = 24;
+	const double dlogE = 0.1;
+
+	// distance binning
+	int nD;
+	double Dmax;
+	double dD;
+
+	// histograms (distance,energy) of photons, electrons and positrons
+	mutable std::vector<uint> photonHist;
+	mutable std::vector<uint> electronHist;
+	mutable std::vector<uint> positronHist;
+	void init();
+
+public:
+	EMCascade();
+
+	/** Change the distance binning */
+	void setDistanceBinning(
+		double Dmax,  //!< maximum distance [m]
+		int nD        //!< number of distance bins
+		);
+
+	/** Collect and deactivate photons, electrons and positrons */
+	void process(Candidate *candidate) const;
+
+	/** Save the unpropagated histogram of EM particles */
+	void save(const std::string &filename);
+
+	/** Load the unpropagated histogram of EM particles */
+	void load(const std::string &filename);
+
+	/** Calculates the EM cascade with DINT */
+	void runCascade(
+		const std::string &filename,  //!< output filename
+		int IRBFlag = 4,        //!< EBL background 0: high, 1: low, 2: Primack, 4: Stecker'06
+		int RadioFlag = 4,      //!< radio background 0: high, 1: medium, 2: obs, 3: none, 4: Protheroe'96
+		double Bfield = 1E-13,  //!< magnetic field strength [T], default = 1 nG
+		double cutCascade = 0   //!< a-parameter, see CRPropa 2 paper
+		);
+
+	std::string getDescription() const;
+};
+
+} // namespace crpropa
+
+#endif // CRPROPA_EMCASCADE_H

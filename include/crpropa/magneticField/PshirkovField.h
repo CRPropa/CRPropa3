921fc48f6c1241bfa2bd05b05d43feaa13da4fdf
DavidWalz <walz@physik.rwth-aachen.de>
2016-05-19 15:46:04 +0200

    PshirkovField: refactor class + fix ASS field

--- include/crpropa/magneticField/PshirkovField.h
+++ include/crpropa/magneticField/PshirkovField.h
@@ -2,81 +2,63 @@
 #define CRPROPA_PSHIRKOVFIELD_H
 
 #include "crpropa/magneticField/MagneticField.h"
-#include "crpropa/Grid.h"
 
 namespace crpropa {
 
 /**
  @class PshirkovField
- @brief PshirkovField galactic magnetic field model
+ @brief Pshirkov2011 galactic magnetic field model
 
- Implements the Pshirkov magnetic field model, consisting of a large-scale regular
- (disk and halo) field.
- See:
- Pshirkov 2011, arXiv:1103.0814, Deriving global structure of the Galactic Magnetic Field from Faraday Rotation Measures of extragalactic sources
+ Implements the Pshirkov2011 galactic magnetic field model, consisting of large-scale regular disk and halo fields.
+ For the disk field an axisymmetric (ASS) and the bisymmetric (BSS, default) model can be chosen.
+ For the halo field the BHM Halo field model (Sun et al. 2008) is considered.
 
- Two models, the Axisymmetric (ASS) and the Bisymmetric (BSS) disk model can be chosen.
- Currently only best fit values of the field paramaters are implemented and
- cannot be changed.
+ Currently only best fit values of the field parameters are implemented.
+ The field is defined in the usual galactocentric coordinate system with the Galactic center at the origin, the x-axis pointing in the opposite direction of  the Sun, and the z-axis pointing towards Galactic north.
 
- The field is defined in the usual galactocentric coordinate system with the
- Galactic center at the origin, the x-axis pointing in the opposite direction of
- the Sun, and the z-axis pointing towards Galactic north.
+ See: Pshirkov, Tinyakov, Kronberg Newton-McGee 2011 - Deriving global structure of the Galactic Magnetic Field from Faraday Rotation Measures of extragalactic sources, DOI: 10.1088/0004-637X/738/2/192, arXiv:1103.0814
  */
 
 class PshirkovField: public MagneticField {
 private:
-	bool useASS;
-	bool useBSS;
-	bool useBHMHalo;
+	bool useASS;     // switch for axisymmetric spiral field (ASS)
+	bool useBSS;     // switch for bisymmetric spiral field (BSS)
+	bool useHalo; // switch for halo field
 
 	// disk parameters
-	double pitch_ASS;	// pitch angle
-	double pitch_BSS;
-	double b_ASS, b_BSS, d;	// b = 1/tan(p) // d means the distance to the first field reversal
-	double R_sun;		// distance of the Sun to the galactic center
-	double R_c;		// radius of central region
-	double theta_ASS, theta_BSS;		// theta = b*ln(1+d/R_sun) - pi/2
-	double z0;		// vertical thickness of the MF in the galactic disk
-	double B0;		// scale of the magnetic field strength
-	
-	// halo (north)
-	double z0_n;		// vertical position of the halo field
-	double R0_n;		// radial position of the halo field
-	double B0_n;		// scale of the magnetic field strength in the halo
-	double z11_n;		// vertical thickness towards the galactic disc
-	double z12_n;		// vertical thickness off the galactic disk
+	double pitch, cos_pitch, sin_pitch, theta, cos_theta;  // pitch angle parameters
+	double d;     // distance to first field reversal
+	double R_sun; // distance between sun and galactic center
+	double R_c;   // radius of central region
+	double z0;    // vertical thickness in the galactic disk
+	double B0;    // magnetic field scale
 
-	// halo (south)
-	double z0_s;		// SAME with south halo
-	double R0_s;
-	double B0_s_ASS;
-	double B0_s_BSS;
-	double z11_s;
-	double z12_s;
-	
+	// halo parameters
+	double z0_H;  // halo vertical position
+	double R0_H;  // halo radial position
+	double B0_H;  // halo magnetic field scale (north)
+	double B0_H_ASS; // halo magnetic field scale (south, ASS)
+	double B0_H_BSS; // halo magnetic field scale (south, BSS)
+	double z11_H; // halo vertical thickness towards disc
+	double z12_H; // halo vertical thickness off the disk
+
 public:
 	PshirkovField();
 
 	void setUseASS(bool use);
 	void setUseBSS(bool use);
-	void setUseBHMHalo(bool use);
+	void setUseHalo(bool use);
 
 	bool isUsingASS();
 	bool isUsingBSS();
-	bool isUsingBHMHalo();
-	
-	double getSouthernFieldstrength() const;
+	bool isUsingHalo();
 
-	// Regular ASS field
-	Vector3d getASSField(const Vector3d& pos) const;
+	// Regular disk field component
+	Vector3d getDiskField(const Vector3d& pos) const;
 
-	// Regular BSS field
-	Vector3d getBSSField(const Vector3d& pos) const;
-	
-	// Regular BHM Halo field (Sun et al. 2008)
-	Vector3d getBHMHaloField(const Vector3d& pos) const;
-	
+	// Regular BHM Halo field (Sun et al. 2008) component
+	Vector3d getHaloField(const Vector3d& pos) const;
+
 	// All set field components
 	Vector3d getField(const Vector3d& pos) const;
 };

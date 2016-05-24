08204442e955ed42b68f68f136ec0a89b345ab10
Andrej Dundović <andrej@dundovic.com.hr>
2016-05-24 10:30:18 +0200

    Indentation fixed

--- src/module/NuclearDecay.cpp
+++ src/module/NuclearDecay.cpp
@@ -244,25 +244,25 @@ void NuclearDecay::nucleonEmission(Candidate *candidate, int dA, int dZ) const {
 }
 
 double NuclearDecay::lossLength(int id, double gamma, double z) {
-        if (not (isNucleus(id)))
-            return std::numeric_limits<double>::max();
-            
-        int A = massNumber(id);
-        int Z = chargeNumber(id);
-        int N = A - Z;
-        
-        // check if particle can decay
-        std::vector<DecayMode> decays = decayTable[Z * 31 + N];
-        if (decays.size() == 0)
-            return std::numeric_limits<double>::max();
-            
-        double totalRate = 0;
-        
-        for (size_t i = 0; i < decays.size(); i++) {
-            double rate = decays[i].rate;
-            rate /= gamma;
-            totalRate += rate;
-        }
+	if (not (isNucleus(id)))
+		return std::numeric_limits<double>::max();
+
+	int A = massNumber(id);
+	int Z = chargeNumber(id);
+	int N = A - Z;
+
+	// check if particle can decay
+	std::vector<DecayMode> decays = decayTable[Z * 31 + N];
+	if (decays.size() == 0)
+		return std::numeric_limits<double>::max();
+
+	double totalRate = 0;
+
+	for (size_t i = 0; i < decays.size(); i++) {
+		double rate = decays[i].rate;
+		rate /= gamma;
+		totalRate += rate
+	}
 
 		return 1. / totalRate;
 }

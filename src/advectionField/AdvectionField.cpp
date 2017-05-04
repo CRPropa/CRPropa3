f9a0757276435e90a775b085dbc3d74d4fdf040d
Lukas Merten <lukas.merten@rub.de>
2017-05-04 23:56:56 +0200

    Include a spherical symmetric wind model with asymptotically constant wind speed. Velocity profil from Bustard et al. (2016). Added units for speed description.

--- src/advectionField/AdvectionField.cpp
+++ src/advectionField/AdvectionField.cpp
@@ -1,5 +1,6 @@
 #include "crpropa/advectionField/AdvectionField.h"
 
+
 namespace crpropa {
 
 
@@ -25,5 +26,84 @@ Vector3d UniformAdvectionField::getField(const Vector3d &position) const {
 		return value;
 	}
 
+//----------------------------------------------------------------
+
+SphericalAdvectionField::SphericalAdvectionField(Vector3d origin, double radius, double vMax, double tau, double alpha) {
+	setOrigin(origin);
+	setRadius(radius);
+	setVMax(vMax);
+	setTau(tau);
+	setAlpha(alpha);
+}
+
+Vector3d SphericalAdvectionField::getField(const Vector3d &position) const {
+	Vector3d Pos = position-origin;
+	double R = Pos.getR();
+	if (R>radius) {
+		return Vector3d(0.);
+	}
+	double v_R = getV(R);
+	return v_R * Pos.getUnitVector();
+}
+
+double SphericalAdvectionField::getV(const double &r) const {
+	double f = vMax * (1-exp(-(pow(r, alpha)/tau)));
+	return f;
+}
+
+void SphericalAdvectionField::setOrigin(Vector3d o) {
+	origin = o;
+	return;
+}
+
+void SphericalAdvectionField::setRadius(double r) {
+	radius = r;
+	return;
+}
+
+void SphericalAdvectionField::setVMax(double v) {
+	vMax = v;
+	return;
+}
+
+void SphericalAdvectionField::setTau(double t) {
+	tau = t;
+	return;
+}
+
+void SphericalAdvectionField::setAlpha(double a) {
+	alpha = a;
+	return;
+}
+
+Vector3d SphericalAdvectionField::getOrigin() const {
+	return origin;
+}
+
+double SphericalAdvectionField::getRadius() const {
+	return radius;
+}
+
+double SphericalAdvectionField::getVMax() const {
+	return vMax;
+}
+
+double SphericalAdvectionField::getTau() const {
+	return tau;
+}
+
+double SphericalAdvectionField::getAlpha() const {
+	return alpha;
+}
+
+std::string SphericalAdvectionField::getDescription() const {
+	std::stringstream s;
+	s << "Origin: " << origin / kpc  << " kpc, ";
+	s << "Radius: " << radius / kpc  << " kpc, ";
+	s << "vMax: " << vMax / km * sec << " km/s, ";
+	s << "tau: " << tau << ", ";
+	s << "alpha: " << alpha << "\n";
+	return s.str();
+}
 
 } // namespace crpropa

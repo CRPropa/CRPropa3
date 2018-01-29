7ae2634cc48d83db308a33d9275b16a68e498501
Rafa <8rafael@gmail.com>
2018-01-29 20:45:49 -0200

    fix indentation

--- plugin-template/myPlugin.h
+++ plugin-template/myPlugin.h
@@ -12,18 +12,18 @@
 /// A custom C++ module
 class MyModule : public crpropa::Module
 {
-  public:
-    /// The parent's constructor need to be called on initialization!
-  	MyModule();
-  	void process(crpropa::Candidate *candidate) const;
+public:
+	/// The parent's constructor need to be called on initialization!
+	MyModule();
+	void process(crpropa::Candidate *candidate) const;
 };
 
 
 /// A custom source feature
 class AddMyProperty: public crpropa::SourceFeature
 {
-  public:
-    /// The parent's constructor need to be called on initialization!
-  	AddMyProperty();
-  	void prepareCandidate(crpropa::Candidate &candidate) const;
+public:
+	/// The parent's constructor need to be called on initialization!
+	AddMyProperty();
+	void prepareCandidate(crpropa::Candidate &candidate) const;
 };

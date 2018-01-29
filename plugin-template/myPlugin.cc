663525dace4e2e2b2fa886d60f19832ec0240268
Rafa <8rafael@gmail.com>
2018-01-29 20:45:49 -0200

    fix indentation

--- plugin-template/myPlugin.cc
+++ plugin-template/myPlugin.cc
@@ -2,25 +2,24 @@
 
 #include <string>
 
-const std::string myPropertyName="counter";
+const std::string myPropertyName = "counter";
 
 // The parent's constructor need to be called on initialization!
 MyModule::MyModule() : crpropa::Module()
 {
-  setDescription("MyPlugin::MyModule");
+	setDescription("MyPlugin::MyModule");
 }
 
 void MyModule::process(crpropa::Candidate *candidate) const
 {
-  // To enable parallelization, the modules have to be stateless - the
-  // process method should thus not modify internal variables!
+	// To enable parallelization, the modules have to be stateless - the
+	// process method should thus not modify internal variables!
 	std::cout << "MyPlugin::MyModule::process() called\n";
-  if(candidate->hasProperty(myPropertyName))
-  {
-    uint32_t v = candidate->getProperty(myPropertyName);
-	  std::cout << " My property: " << myPropertyName << " = " <<  v << std::endl;
-    candidate->setProperty(myPropertyName, crpropa::Variant::fromUInt32(v + 1));
-  }
+	if(candidate->hasProperty(myPropertyName)) {
+		uint32_t v = candidate->getProperty(myPropertyName);
+		std::cout << " My property: " << myPropertyName << " = " <<  v << std::endl;
+		candidate->setProperty(myPropertyName, crpropa::Variant::fromUInt32(v + 1));
+	}
 }
 
 // ------------------------------------------------------------------
@@ -31,5 +30,5 @@ AddMyProperty::AddMyProperty() : crpropa::SourceFeature()
 }
 void AddMyProperty::prepareCandidate(crpropa::Candidate &candidate) const
 {
-  candidate.setProperty(myPropertyName, crpropa::Variant::fromUInt32(0));
+	candidate.setProperty(myPropertyName, crpropa::Variant::fromUInt32(0));
 }

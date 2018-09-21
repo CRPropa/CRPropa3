dbeccb10fd0c1631c2d0c6909954cd51dcc98c8f
Antonius Frie <antonius.frie@rub.de>
2018-09-21 14:31:14 +0200

    Improve documentation

--- include/crpropa/magneticField/TD13Field.h
+++ include/crpropa/magneticField/TD13Field.h
@@ -72,14 +72,17 @@ std::vector<double> logspace(double start, double stop, size_t N);
  @brief Interpolation-free turbulent magnetic field based on the GJ99 and TD13 papers
 
  ## Overview
- This class provides a turbulent magnetic field that is generated as described by (Giacalone and Jokipii, 1999) and (Tautz and Dosch, 2013). Instead of using an inverse Fourier transform to generate the field on a grid -- which then needs to be interpolated to obtain in-between values -- this method only generates the wave modes making up the turbulent magnetic field ahead of time. At run time, when the field's value at a particular position is required, these plane waves are then evaluated analytically at that position. This guarantees that the resulting field is completely free of divergence, reproduces the mean field strength accurately, and does not suffer from other interpolation-induced problems. The disadvantage is that the number of wave modes is drastically smaller when compared with initTurbulence, which might have physical ramifications on the particles propagating through the field. Additionally, the implementation is somewhat slower.
+ This class provides a turbulent magnetic field that is generated as described by [(Giacalone and Jokipii, 1999)][gj99] and [(Tautz and Dosch, 2013)][td13]. Instead of using an inverse Fourier transform to generate the field on a grid -- which then needs to be interpolated to obtain in-between values -- this method only generates the wave modes making up the turbulent magnetic field ahead of time. At run time, when the field's value at a particular position is required, these plane waves are then evaluated analytically at that position. This guarantees that the resulting field is completely free of divergence, reproduces the mean field strength accurately, and does not suffer from other interpolation-induced problems. The disadvantage is that the number of wave modes is drastically smaller when compared with initTurbulence, which might have physical ramifications on the particles propagating through the field. Additionally, the implementation is somewhat slower.
 
  ## Using the SIMD optimization
- In order to mitigate some of the performance impact that is inherent in this method of field generation, an optimized version utilizing data-level parallelism through SIMD instructions. More specifically, this implementation uses the x86 extensions SSE1, SSE2 and SSE3, with SSE4.1 being optional. In order to use this optimized version, three conditions need to be met:
+ In order to mitigate some of the performance impact that is inherent in this method of field generation, an optimized version utilizing data-level parallelism through SIMD instructions is provided. More specifically, this implementation uses the x86 extensions SSE1, SSE2 and SSE3, with SSE4.1 being optional. In order to use this optimized version, three conditions need to be met:
 
-1. The vectorized math library SLEEF needs to be present in CRPropa's prefix path. This library provides a fast, vectorized cosine function required to evaluate the field using SIMD. When running CMake, the output will indicate whether SLEEF was found or not.
+1. The vectorized math library SLEEF needs to be present in CRPropa's prefix path. This library provides a fast, vectorized cosine function required by getField(). When running CMake, the output will indicate whether SLEEF was found or not.
 2. The `USE_SIMD` option needs to be explicitly enabled in CMake. Currently, this sets GCC flags that tell the compiler to allow SIMD instructions.
-3. Finally, the CPU that will actually run the code needs to support the abovementioned extensions: SSE1 through SSE3. These extensions are relatively old and quite common, so I do not expect this to be a problem.
+3. Finally, the CPU that will actually run the code needs to support the abovementioned extensions: SSE1 through SSE3. These extensions are relatively old and quite common, so this should not be a problem in practice.
+
+[gj99]: https://doi.org/10.1086/307452
+[td13]: https://doi.org/10.1063/1.4789861
  */
 class TD13Field: public MagneticField {
 private:
@@ -113,11 +116,12 @@ public:
   std::vector<float> avx_data;
   std::vector<double> Ak;
   std::vector<double> k;
-  /** Constructor
+  /**
+      Create a new instance of TD13Field with the specified parameters. This generates all of the wavemodes according to the given parameters.
       @param Brms root mean square field strength for generated field
       @param kmin wave number of the mode with the largest wavelength to be included in the spectrum
       @param kmax wave number of the mode with the smallest wavelength to be included in the spectrum
-      @param gamma spectral index
+      @param gamma the spectral index, such that a positive value here will give a negative power law. (So usually you'd want gamma=11/3.)
       @param bendoverScale the turbulence bend-over scale, used to scale the wavenumbers. As per the TD13 paper, this is set to 0.03AU by default.
       @param Nm number of wavemodes that will be used when computing the field. A higher value will give a more accurate representation of the turbulence, but increase the runtime for getField.
       @param seed can be used to seed the random number generator used to generate the field. This works just like in initTurbulence: a seed of 0 will lead to a randomly initialized RNG.

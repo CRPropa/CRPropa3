8ad3eff936f9a190084c546343fc21a19ef075a1
Tobias Winchen <tobias.winchen@rwth-aachen.de>
2018-11-09 23:41:26 +0100

    Fix trailing whitespace in checkNumpy

--- python/checkNumpy.py
+++ python/checkNumpy.py
@@ -3,11 +3,11 @@
 import sys
 try:
   import numpy
-  
+
   from pkg_resources import parse_version
   if parse_version(numpy.__version__) < parse_version('1.6.0'):
     sys.exit(-1)
-  
+
   sys.stdout.write(numpy.get_include())
 
 except ImportError:

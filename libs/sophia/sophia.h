99fbb85068953b4ec3e084a69e531c32c6f03331
Mario Hörbe <mario.hoerbe@rub.de>
2019-01-01 17:55:50 +0100

    - adapt input and output parameters of the new SOPHIA code - update docstrings

--- libs/sophia/sophia.h
+++ libs/sophia/sophia.h
@@ -1,37 +1,36 @@
 #ifndef _SOPHIA_H
 #define _SOPHIA_H
 
+
 extern "C" {
-void sophiaevent_(int& channel, double& inputenergy, double momentum[][2000],
-		int id[], int& n, double& redshift, int& photonbackground, double& maxz,
-		int&, double[], double[]);
+void sophiaevent_(int& type, double& inputEnergy, double& e, double momentum[2000],
+        int id[], int& n);
 }
 
+
+// void sophiaevent_(int& nature,  // channel: 0 -> p, 1 -> n
+//                double& Ein,  // input energy of nucleon in GeV
+//                double momentaList[][2000],  // list of 4-momenta + masses of output particles (in GeV)
+//                double particleList[2000],
+//                int& nParticles,  // number of output particles
+//                double& eps  // energy of target photon in eV
+//                );
+// }
+
+
 /*
- The arguments are the following
- - channel: 0 -> p, 1 -> n
- - input energy of nucleon in GeV
- - list of 4-momenta + masses of output particles (in GeV)
  - list of output particle ids
-	 13  proton
-	 14  neutron
-	 -13  antiproton
-	 -14  antineutron
-	 1   photon
-	 2   e+
-	 3   e-
-	 15  nu_e
-	 16  antinu_e
-	 17  nu_muon
-	 18  antinu_muon
- - number of output particles
- - redshift
- - photon background flag: 1 -> CMB, 2 -> IRB Kneiske
- (Primack et al. (1999) IRB is outcommented in sophia_interface.f on line 16320
- - maximum redshift: the photon density of IRB is null above this redshift
- - dummy1
- - dummy2
- - dummy3
+     13  proton
+     14  neutron
+    -13  antiproton
+    -14  antineutron
+     1   photon
+     2   e+
+     3   e-
+     15  nu_e
+     16  antinu_e
+     17  nu_muon
+     18  antinu_muon
  */
 
 #endif

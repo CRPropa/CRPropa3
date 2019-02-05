d8b7ed2d8d200bcd6f36b6b5f493cc7861918a4d
Patrick Reichherzer <mathe-abi-tutor@web.de>
2019-02-05 16:13:27 +0100

    Modified the Boris push code to match the programming style of CRPropa. In particular to match the module PropagationCK.

--- src/module/PropagationBP.cpp
+++ src/module/PropagationBP.cpp
@@ -6,79 +6,80 @@
 
 namespace crpropa {
 
-PropagationBP::PropagationBP(ref_ptr<MagneticField> field,
-		double Step):Step(1* Mpc)
-		 {
-	setField(field);
-	setStep(Step);
-}
-
-
-void PropagationBP::process(Candidate *c) const {
-	
-	// update
-	c->previous = c->current;
-	
-
-	double step = getStep();
-	c->setCurrentStep(step);
-	double h = step / c_light;
-	
-	//get particle properties
-	double q = c->current.getCharge();                    
-	double m = c->current.getEnergy()/(c_light*c_light);  
-	Vector3d x = c->current.getPosition();                
-	Vector3d v = c->current.getDirection();
-	
-	// half leap frog step in the position
-	x += c_light * v * h /2. ;
-	
-	// get B field at particle position
-	//Vector3d B(1 * nG, 0, 0);
-	Vector3d B(0, 0, 0);
-	//~ double z = 1; // <- Lukas fragen!
-	double z = c->getRedshift();
-	try {
-		B = field->getField(x, z);
-	} catch (std::exception &e) {
-		std::cerr << "PropagationBP: Exception in getField." << std::endl;
-		std::cerr << e.what() << std::endl;
+	PropagationBP::PropagationBP(ref_ptr<MagneticField> field,
+								 double propStep) : propStep(1 * Mpc)
+	{
+		setField(field);
+		setStep(propStep);
 	}
-	// Boris help vectors 
-	Vector3d t = B * q/2/m * h;
-	Vector3d s = t *2. /(1+t.dot(t));
-	Vector3d v_help;
-	
-	// Boris Push
-	v_help = v + v.cross(t);
-	v = v + v_help.cross(s);
-	
-	// full leap frog step in the velocity 
-	c->current.setDirection(v);
-	
-	//~ // the other half leap frog step in the position
-	x += c_light * v * h /2. ;
-	c->current.setPosition(x);
-	
-	c->setCurrentStep(h);
-	c->setNextStep(h);
-	
-}
 
 
-void PropagationBP::setField(ref_ptr<MagneticField> f) {
-	field = f;
-}
+	void PropagationBP::process(Candidate *candidate) const {
+		// save the new previous particle state
+		ParticleState &current = candidate->current;
+		candidate->previous = current;
 
-void PropagationBP::setStep(double min) {
-	if (min < 0.)
-		throw std::runtime_error("PropagationBP: Step < 0 ");
-	Step = min;
-}
+		double step = getStep();
+		candidate->setCurrentStep(step);
+		double h = step / c_light;
 
-double PropagationBP::getStep() const {
-	return Step;
-}
+		//get particle properties
+		double q = current.getCharge();
+		double m = current.getEnergy()/(c_light * c_light);
+		double z = candidate->getRedshift();
+		Vector3d pos = current.getPosition();
+		Vector3d dir = current.getDirection();
 
-}
+		// half leap frog step in the position
+		pos += c_light * dir * h / 2.;
 
+		// get B field at particle position
+		Vector3d B(0, 0, 0);
+		try {
+			B = field->getField(pos, z);
+		} catch (std::exception &e) {
+			std::cerr << "PropagationBP: Exception in getField." << std::endl;
+			std::cerr << e.what() << std::endl;
+		}
+		// Boris help vectors
+		Vector3d t = B * q / 2 / m * h;
+		Vector3d s = t * 2 / (1 + t.dot(t));
+		Vector3d v_help;
+
+		// Boris push
+		v_help = dir + dir.cross(t);
+		dir = dir + v_help.cross(s);
+
+		// full leap frog step in the velocity
+		candidate->current.setDirection(dir);
+
+		// the other half leap frog step in the position
+		pos += c_light * dir * h / 2. ;
+		candidate->current.setPosition(pos);
+		candidate->setCurrentStep(h);
+		candidate->setNextStep(h);
+	}
+
+
+	void PropagationBP::setField(ref_ptr<MagneticField> f) {
+		field = f;
+	}
+
+	void PropagationBP::setStep(double step) {
+		if (step < 0.)
+			throw std::runtime_error("PropagationBP: step < 0");
+		propStep = step;
+	}
+
+	double PropagationBP::getStep() const {
+		return propStep;
+	}
+
+	std::string PropagationBP::getDescription() const {
+		std::stringstream s;
+		s << "Propagation in magnetic fields using the Boris push method.";
+		s << ", step: " << propStep / kpc << " kpc";
+		return s.str();
+	}
+
+} // namespace crpropa

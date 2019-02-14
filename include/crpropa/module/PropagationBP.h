ccbec106ecf2ab08206eb102e498094e957f47f0
Tobias Winchen <tobias.winchen@rwth-aachen.de>
2019-02-14 19:28:12 +0100

    Fixed some whitespace

--- include/crpropa/module/PropagationBP.h
+++ include/crpropa/module/PropagationBP.h
@@ -22,114 +22,114 @@ namespace crpropa {
  Additionally a minimum and maximum size for the steps can be set.
  For neutral particles a rectilinear propagation is applied and a next step of the maximum step size proposed.
  */
-    class PropagationBP: public Module {
+class PropagationBP: public Module {
 
-    public:
-        class Y {
-        public:
-            Vector3d x, u; /*< phase-point: position and direction */
+public:
+	class Y {
+	public:
+		Vector3d x, u; /*< phase-point: position and direction */
 
-            Y() {
-            }
+		Y() {
+		}
 
-            Y(const Vector3d &x, const Vector3d &u) :
-                    x(x), u(u) {
-            }
+		Y(const Vector3d &x, const Vector3d &u) :
+				x(x), u(u) {
+		}
 
-            Y(double f) :
-                    x(Vector3d(f, f, f)), u(Vector3d(f, f, f)) {
-            }
+		Y(double f) :
+				x(Vector3d(f, f, f)), u(Vector3d(f, f, f)) {
+		}
 
-            Y operator *(double f) const {
-                return Y(x * f, u * f);
-            }
+		Y operator *(double f) const {
+			return Y(x * f, u * f);
+		}
 
-            Y &operator +=(const Y &y) {
-                x += y.x;
-                u += y.u;
-                return *this;
-            }
-        };
+		Y &operator +=(const Y &y) {
+			x += y.x;
+			u += y.u;
+			return *this;
+		}
+	};
 
-    private:
-        ref_ptr<MagneticField> field;
-        double tolerance; /** target relative error of the numerical integration */
-        double minStep; /** minimum step size of the propagation */
-        double maxStep; /** maximum step size of the propagation */
+private:
+	ref_ptr<MagneticField> field;
+	double tolerance; /** target relative error of the numerical integration */
+	double minStep; /** minimum step size of the propagation */
+	double maxStep; /** maximum step size of the propagation */
 
-    public:
-        /** Default constructor for the Boris push. It is constructed with a fixed step size.
-         * @param field
-         * @param minStep */
-        PropagationBP(ref_ptr<MagneticField> field = NULL, double minStep = (1. * kpc));
+public:
+	/** Default constructor for the Boris push. It is constructed with a fixed step size.
+	 * @param field
+	 * @param minStep */
+	PropagationBP(ref_ptr<MagneticField> field = NULL, double minStep = (1. * kpc));
 
-        /** Constructor for the adaptive Boris push.
-         * @param field
-         * @param minStep       minStep/c_light is the minimum integration timestep
-         * @param maxStep       maxStep/c_light is the maximum integration timestep
-         * @param tolerance     tolerance is criterion for step adjustment. Step adjustment takes place only if minStep < maxStep. */
-        PropagationBP(ref_ptr<MagneticField> field, double minStep, double maxStep, double tolerance = 1e-4);
+	/** Constructor for the adaptive Boris push.
+	 * @param field
+	 * @param minStep	   minStep/c_light is the minimum integration timestep
+	 * @param maxStep	   maxStep/c_light is the maximum integration timestep
+	 * @param tolerance	 tolerance is criterion for step adjustment. Step adjustment takes place only if minStep < maxStep. */
+	PropagationBP(ref_ptr<MagneticField> field, double minStep, double maxStep, double tolerance = 1e-4);
 
-        /** Propagates the particle. Is called once per iteration.
-         * @param candidate     The Candidate is a passive object, that holds the information about the state of the cosmic ray and the simulation itself. */
-        void process(Candidate *candidate) const;
+	/** Propagates the particle. Is called once per iteration.
+	 * @param candidate	 The Candidate is a passive object, that holds the information about the state of the cosmic ray and the simulation itself. */
+	void process(Candidate *candidate) const;
 
-        /** Calculates the new position and direction of the particle based on the solution of the Lorentz force
-         * @param pos   current position of the candidate
-         * @param dir   current direction of the candidate
-         * @param step  current step size of the candidate
-         * @param z     current redshift is needed to calculate the magnetic field
-         * @param q     current charge of the candidate
-         * @param m     current mass of the candidate
-         * @return      return the new calculated position and direction of the candidate */
-        Y dY(Vector3d  pos, Vector3d  dir, double step, double z, double q, double m) const;
+	/** Calculates the new position and direction of the particle based on the solution of the Lorentz force
+	 * @param pos   current position of the candidate
+	 * @param dir   current direction of the candidate
+	 * @param step  current step size of the candidate
+	 * @param z	 current redshift is needed to calculate the magnetic field
+	 * @param q	 current charge of the candidate
+	 * @param m	 current mass of the candidate
+	 * @return	  return the new calculated position and direction of the candidate */
+	Y dY(Vector3d  pos, Vector3d  dir, double step, double z, double q, double m) const;
 
-        /** comparison of the position after one step with the position after two steps with step/2.
-         * @param x1    position after one step of size step
-         * @param x2    position after two steps of size step/2
-         * @param step  current step size
-         * @return      measurement of the error of the step */
-        double errorEstimation(const Vector3d x1, const Vector3d x2, double step) const;
+	/** comparison of the position after one step with the position after two steps with step/2.
+	 * @param x1	position after one step of size step
+	 * @param x2	position after two steps of size step/2
+	 * @param step  current step size
+	 * @return	  measurement of the error of the step */
+	double errorEstimation(const Vector3d x1, const Vector3d x2, double step) const;
 
-        /** get magnetic field vector at current candidate position
-         * @param pos   current position of the candidate
-         * @param z     current redshift is needed to calculate the magnetic field
-         * @return      magnetic field vector at the position pos */
-        Vector3d getFieldAtPosition(Vector3d pos, double z) const;
+	/** get magnetic field vector at current candidate position
+	 * @param pos   current position of the candidate
+	 * @param z	 current redshift is needed to calculate the magnetic field
+	 * @return	  magnetic field vector at the position pos */
+	Vector3d getFieldAtPosition(Vector3d pos, double z) const;
 
-        /** Adapte step size if required and calculates the new position and direction of the particle with the usage of the function dY
-         * @param y         current position and direction of candidate
-         * @param out       position and direction of candidate after the step
-         * @param error     error for the current step
-         * @param h         current step size
-         * @param p         current particle state
-         * @param z         current red shift
-         * @param m         current mass of the candidate
-         * @param q         current charge of the candidate */
-        void tryStep(const Y &y, Y &out, Y &error, double h, ParticleState &p, double z, double m, double q) const;
+	/** Adapte step size if required and calculates the new position and direction of the particle with the usage of the function dY
+	 * @param y		 current position and direction of candidate
+	 * @param out	   position and direction of candidate after the step
+	 * @param error	 error for the current step
+	 * @param h		 current step size
+	 * @param p		 current particle state
+	 * @param z		 current red shift
+	 * @param m		 current mass of the candidate
+	 * @param q		 current charge of the candidate */
+	void tryStep(const Y &y, Y &out, Y &error, double h, ParticleState &p, double z, double m, double q) const;
 
-        /** set functions for the parameters of the class PropagationBP */
+	/** set functions for the parameters of the class PropagationBP */
 
-        /** set a specific magnetic field
-         * @param field     specific magnetic field */
-        void setField(ref_ptr<MagneticField> field);
-        /** set a specific tolerance for the step size adaption
-         * @param tolerance     tolerance is criterion for step adjustment. Step adjustment takes place only if minStep < maxStep. */
-        void setTolerance(double tolerance);
-        /** set the minimum step for the Boris push
-         * @param minStep       minStep/c_light is the minimum integration time step */
-        void setMinimumStep(double minStep);
-        /** set the maximum step for the Boris push
-         * @param maxStep       maxStep/c_light is the maximum integration time step */
-        void setMaximumStep(double maxStep);
+	/** set a specific magnetic field
+	 * @param field	 specific magnetic field */
+	void setField(ref_ptr<MagneticField> field);
+	/** set a specific tolerance for the step size adaption
+	 * @param tolerance	 tolerance is criterion for step adjustment. Step adjustment takes place only if minStep < maxStep. */
+	void setTolerance(double tolerance);
+	/** set the minimum step for the Boris push
+	 * @param minStep	   minStep/c_light is the minimum integration time step */
+	void setMinimumStep(double minStep);
+	/** set the maximum step for the Boris push
+	 * @param maxStep	   maxStep/c_light is the maximum integration time step */
+	void setMaximumStep(double maxStep);
 
-         /** get functions for the parameters of the class PropagationBP, similar to the set functions */
-        ref_ptr<MagneticField> getField() const;
-        double getTolerance() const;
-        double getMinimumStep() const;
-        double getMaximumStep() const;
-        std::string getDescription() const;
-    };
+	 /** get functions for the parameters of the class PropagationBP, similar to the set functions */
+	ref_ptr<MagneticField> getField() const;
+	double getTolerance() const;
+	double getMinimumStep() const;
+	double getMaximumStep() const;
+	std::string getDescription() const;
+};
 /** @}*/
 
 } // namespace crpropa
--- src/module/PropagationBP.cpp
+++ src/module/PropagationBP.cpp
@@ -5,204 +5,212 @@
 #include <vector>
 
 namespace crpropa {
+	void PropagationBP::tryStep(const Y &y, Y &out, Y &error, double h,
+			ParticleState &particle, double z, double m, double q) const {
+		out = dY(y.x, y.u, h, z, q, m);  // 1 step with h
+
+		Y outHelp = dY(y.x, y.u, h/2, z, q, m);  // 2 steps with h/2
+		Y outCompare = dY(outHelp.x, outHelp.u, h/2, z, q, m);
+
+		error = errorEstimation(out.x , outCompare.x , h);
+	}
 
 
-    void PropagationBP::tryStep(const Y &y, Y &out, Y &error, double h,
-                                ParticleState &particle, double z, double m, double q) const {
-        out = dY(y.x, y.u, h, z, q, m);  // 1 step with h
+	PropagationBP::Y PropagationBP::dY(Vector3d pos, Vector3d dir, double step,
+			double z, double q, double m) const {
+		// half leap frog step in the position
+		pos += dir * step / 2.;
 
-        Y outHelp = dY(y.x, y.u, h/2, z, q, m);  // 2 steps with h/2
-        Y outCompare = dY(outHelp.x, outHelp.u, h/2, z, q, m);
+		// get B field at particle position
+		Vector3d B = getFieldAtPosition(pos, z);
 
-        error = errorEstimation(out.x , outCompare.x , h);
-    }
+		// Boris help vectors
+		Vector3d t = B * q / 2 / m * step / c_light;
+		Vector3d s = t * 2 / (1 + t.dot(t));
+		Vector3d v_help;
 
-    PropagationBP::Y PropagationBP::dY(Vector3d pos, Vector3d dir, double step, double z, double q, double m) const {
+		// Boris push
+		v_help = dir + dir.cross(t);
+		dir = dir + v_help.cross(s);
 
-        // half leap frog step in the position
-        pos += dir * step / 2.;
-
-        // get B field at particle position
-        Vector3d B = getFieldAtPosition(pos, z);
-
-        // Boris help vectors
-        Vector3d t = B * q / 2 / m * step / c_light;
-        Vector3d s = t * 2 / (1 + t.dot(t));
-        Vector3d v_help;
-
-        // Boris push
-        v_help = dir + dir.cross(t);
-        dir = dir + v_help.cross(s);
-
-        // the other half leap frog step in the position
-        pos += dir * step / 2.;
-        return Y(pos, dir);
-    }
-
-    // with a fixed step size
-    PropagationBP::PropagationBP(ref_ptr<MagneticField> field, double minStep) :
-            minStep(0) {
-        setField(field);
-        setTolerance(0.42);
-        setMaximumStep(minStep);
-        setMinimumStep(minStep);
-
-    }
-
-    // with adaptive step size
-    PropagationBP::PropagationBP(ref_ptr<MagneticField> field, double minStep, double maxStep, double tolerance) :
-            minStep(0) {
-        setField(field);
-        setTolerance(tolerance);
-        setMaximumStep(maxStep);
-        setMinimumStep(minStep);
-    }
+		// the other half leap frog step in the position
+		pos += dir * step / 2.;
+		return Y(pos, dir);
+	}
 
 
-    void PropagationBP::process(Candidate *candidate) const {
-        // save the new previous particle state
-        ParticleState &current = candidate->current;
-        candidate->previous = current;
-
-        // calculate charge of particle
-        double q = current.getCharge();
-
-        // rectilinear propagation for neutral particles
-        if (q == 0) {
-            double step = clip(candidate->getNextStep(), minStep, maxStep);
-            Vector3d pos = current.getPosition();
-            Vector3d dir = current.getDirection();
-            current.setPosition(pos + dir * step);
-            candidate->setCurrentStep(step);
-            candidate->setNextStep(maxStep);
-            return;
-        }
-
-        // further particle parameters
-        double z = candidate->getRedshift();
-        double m = current.getEnergy()/(c_light * c_light);
+	// with a fixed step size
+	PropagationBP::PropagationBP(ref_ptr<MagneticField> field, double minStep) :
+			minStep(0) {
+		setField(field);
+		setTolerance(0.42);
+		setMaximumStep(minStep);
+		setMinimumStep(minStep);
+	}
 
 
-        // if minStep is the same as maxStep the adaptive algorithm with its error estimation is not needed and the computation time can be saved:
-        if (minStep == maxStep){
-            double step = minStep;
-            Vector3d pos = current.getPosition();
-            Vector3d dir = current.getDirection();
-            // half leap frog step in the position
-            Y yOut = dY(pos, dir, step, z, q, m);
+	// with adaptive step size
+	PropagationBP::PropagationBP(ref_ptr<MagneticField> field, double minStep, double maxStep, double tolerance) :
+			minStep(0) {
+		setField(field);
+		setTolerance(tolerance);
+		setMaximumStep(maxStep);
+		setMinimumStep(minStep);
+	}
 
 
-            // full leap frog step in the velocity
-            candidate->current.setDirection(yOut.u);
-            candidate->current.setPosition(yOut.x);
-            candidate->setCurrentStep(step);
-            candidate->setNextStep(step);
-            return;
-        }
+	void PropagationBP::process(Candidate *candidate) const {
+		// save the new previous particle state
+		ParticleState &current = candidate->current;
+		candidate->previous = current;
 
-        double step = clip(candidate->getNextStep(), minStep, maxStep);
-        double newStep = step;
-        double r = 42;  // arbitrary value > 1
-        Y yIn(current.getPosition(), current.getDirection());
-        Y yOut, yErr;
+		// calculate charge of particle
+		double q = current.getCharge();
 
-        // try performing step until the target error (tolerance) or the minimum step size has been reached
-        while (true) {
-            tryStep(yIn, yOut, yErr, step, current, z, m, q);
-            r = yErr.u.getR() / tolerance;  // ratio of absolute direction error and tolerance
-            if (r > 1) {  // large direction error relative to tolerance, try to decrease step size
-                if (step == minStep)  // already minimum step size
-                    break;
-                else {
-                    newStep = step * 0.95 * pow(r, -0.2);
-                    newStep = std::max(newStep, 0.1 * step); // limit step size decrease
-                    newStep = std::max(newStep, minStep); // limit step size to minStep
-                    step = newStep;
-                }
-            } else {  // small direction error relative to tolerance, try to increase step size
-                if (step != maxStep) {  // already maximum step size
-                    newStep = step * 0.95 * pow(r, -0.2);
-                    newStep = std::min(newStep, 5 * step); // limit step size increase
-                    newStep = std::min(newStep, maxStep); // limit step size to max Step
-                }
-                break;
-            }
-        }
+		// rectilinear propagation for neutral particles
+		if (q == 0) {
+			double step = clip(candidate->getNextStep(), minStep, maxStep);
+			Vector3d pos = current.getPosition();
+			Vector3d dir = current.getDirection();
+			current.setPosition(pos + dir * step);
+			candidate->setCurrentStep(step);
+			candidate->setNextStep(maxStep);
+			return;
+		}
 
-        current.setPosition(yOut.x);
-        current.setDirection(yOut.u.getUnitVector());
-        candidate->setCurrentStep(step);
-        candidate->setNextStep(newStep);
-    }
+		// further particle parameters
+		double z = candidate->getRedshift();
+		double m = current.getEnergy()/(c_light * c_light);
 
-    void PropagationBP::setField(ref_ptr<MagneticField> f) {
-        field = f;
-    }
+		// if minStep is the same as maxStep the adaptive algorithm with its error
+		// estimation is not needed and the computation time can be saved:
+		if (minStep == maxStep){
+			double step = minStep;
+			Vector3d pos = current.getPosition();
+			Vector3d dir = current.getDirection();
+			// half leap frog step in the position
+			Y yOut = dY(pos, dir, step, z, q, m);
 
-    ref_ptr<MagneticField> PropagationBP::getField() const {
-        return field;
-    }
+			// full leap frog step in the velocity
+			candidate->current.setDirection(yOut.u);
+			candidate->current.setPosition(yOut.x);
+			candidate->setCurrentStep(step);
+			candidate->setNextStep(step);
+			return;
+		}
 
-    Vector3d PropagationBP::getFieldAtPosition(Vector3d pos, double z) const {
-        Vector3d B(0, 0, 0);
-        // check if field is valid and use the field vector at the
-        // position pos with the redshift z
-        if (field.valid())
-            B = field->getField(pos, z);
+		double step = clip(candidate->getNextStep(), minStep, maxStep);
+		double newStep = step;
+		double r = 42;  // arbitrary value > 1
+		Y yIn(current.getPosition(), current.getDirection());
+		Y yOut, yErr;
 
-        return B;
-    }
+		// try performing step until the target error (tolerance) or the minimum step size has been reached
+		while (true) {
+			tryStep(yIn, yOut, yErr, step, current, z, m, q);
+			r = yErr.u.getR() / tolerance;  // ratio of absolute direction error and tolerance
+			if (r > 1) {  // large direction error relative to tolerance, try to decrease step size
+				if (step == minStep)  // already minimum step size
+					break;
+				else {
+					newStep = step * 0.95 * pow(r, -0.2);
+					newStep = std::max(newStep, 0.1 * step); // limit step size decrease
+					newStep = std::max(newStep, minStep); // limit step size to minStep
+					step = newStep;
+				}
+			} else {  // small direction error relative to tolerance, try to increase step size
+				if (step != maxStep) {  // already maximum step size
+					newStep = step * 0.95 * pow(r, -0.2);
+					newStep = std::min(newStep, 5 * step); // limit step size increase
+					newStep = std::min(newStep, maxStep); // limit step size to max Step
+				}
+				break;
+			}
+		}
 
-    double PropagationBP::errorEstimation(const Vector3d x1, const Vector3d x2, double step) const {
+		current.setPosition(yOut.x);
+		current.setDirection(yOut.u.getUnitVector());
+		candidate->setCurrentStep(step);
+		candidate->setNextStep(newStep);
+	}
 
-        // compare the position after one step with the position after two steps with step/2.
-        Vector3d diff = (x1 - x2);
 
-        double S = diff.getR() / (step * (1 - 1/4.) );    // 1/4 = (1/2)²  number of steps for x1 divided by number of steps for x2 to the power of p (order)
+	void PropagationBP::setField(ref_ptr<MagneticField> f) {
+		field = f;
+	}
 
-        return S;
-    }
 
-    void PropagationBP::setTolerance(double tol) {
-        if ((tol > 1) or (tol < 0))
-            throw std::runtime_error(
-                    "PropagationBP: target error not in range 0-1");
-        tolerance = tol;
-    }
+	ref_ptr<MagneticField> PropagationBP::getField() const {
+		return field;
+	}
 
-    void PropagationBP::setMinimumStep(double min) {
-        if (min < 0)
-            throw std::runtime_error("PropagationBP: minStep < 0 ");
-        if (min > maxStep)
-            throw std::runtime_error("PropagationBP: minStep > maxStep");
-        minStep = min;
-    }
 
-    void PropagationBP::setMaximumStep(double max) {
-        if (max < minStep)
-            throw std::runtime_error("PropagationBP: maxStep < minStep");
-        maxStep = max;
-    }
+	Vector3d PropagationBP::getFieldAtPosition(Vector3d pos, double z) const {
+		Vector3d B(0, 0, 0);
+		// check if field is valid and use the field vector at the
+		// position pos with the redshift z
+		if (field.valid())
+			B = field->getField(pos, z);
 
-    double PropagationBP::getTolerance() const {
-        return tolerance;
-    }
+		return B;
+	}
 
-    double PropagationBP::getMinimumStep() const {
-        return minStep;
-    }
 
-    double PropagationBP::getMaximumStep() const {
-        return maxStep;
-    }
+	double PropagationBP::errorEstimation(const Vector3d x1, const Vector3d x2, double step) const {
+		// compare the position after one step with the position after two steps with step/2.
+		Vector3d diff = (x1 - x2);
 
-    std::string PropagationBP::getDescription() const {
-        std::stringstream s;
-        s << "Propagation in magnetic fields using the adaptive Boris push method.";
-        s << " Target error: " << tolerance;
-        s << ", Minimum Step: " << minStep / kpc << " kpc";
-        s << ", Maximum Step: " << maxStep / kpc << " kpc";
-        return s.str();
-    }
+		double S = diff.getR() / (step * (1 - 1/4.) );	// 1/4 = (1/2)²  number of steps for x1 divided by number of steps for x2 to the power of p (order)
 
+		return S;
+	}
+
+
+	void PropagationBP::setTolerance(double tol) {
+		if ((tol > 1) or (tol < 0))
+			throw std::runtime_error(
+					"PropagationBP: target error not in range 0-1");
+		tolerance = tol;
+	}
+
+
+	void PropagationBP::setMinimumStep(double min) {
+		if (min < 0)
+			throw std::runtime_error("PropagationBP: minStep < 0 ");
+		if (min > maxStep)
+			throw std::runtime_error("PropagationBP: minStep > maxStep");
+		minStep = min;
+	}
+
+
+	void PropagationBP::setMaximumStep(double max) {
+		if (max < minStep)
+			throw std::runtime_error("PropagationBP: maxStep < minStep");
+		maxStep = max;
+	}
+
+
+	double PropagationBP::getTolerance() const {
+		return tolerance;
+	}
+
+
+	double PropagationBP::getMinimumStep() const {
+		return minStep;
+	}
+
+
+	double PropagationBP::getMaximumStep() const {
+		return maxStep;
+	}
+
+
+	std::string PropagationBP::getDescription() const {
+		std::stringstream s;
+		s << "Propagation in magnetic fields using the adaptive Boris push method.";
+		s << " Target error: " << tolerance;
+		s << ", Minimum Step: " << minStep / kpc << " kpc";
+		s << ", Maximum Step: " << maxStep / kpc << " kpc";
+		return s.str();
+	}
 } // namespace crpropa

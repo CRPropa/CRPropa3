fb065ae83dd902f4f7ca62feac27313c9b5f2287
Mario Hörbe <mario.hoerbe@rub.de>
2019-02-18 16:41:03 +0000

    implement custom photon fields

--- include/crpropa/module/PhotoPionProduction.h
+++ include/crpropa/module/PhotoPionProduction.h
@@ -4,8 +4,6 @@
 #include "crpropa/Module.h"
 #include "crpropa/PhotonBackground.h"
 
-#include <vector>
-
 namespace crpropa {
 /**
  * \addtogroup EnergyLosses
@@ -19,8 +17,9 @@ namespace crpropa {
 class PhotoPionProduction: public Module {
 protected:
 	PhotonField photonField;
+	CustomPhotonField customPhotonField;
 	std::vector<double> tabLorentz; ///< Lorentz factor of nucleus
-	std::vector<double> tabRedshifts;  ///< redshifts (optional for haveRedshiftDependence)
+	std::vector<double> tabRedshifts;  ///< redshifts
 	std::vector<double> tabProtonRate; ///< interaction rate in [1/m] for protons
 	std::vector<double> tabNeutronRate; ///< interaction rate in [1/m] for neutrons
 	double limit; ///< fraction of mean free path to limit the next step
@@ -28,7 +27,6 @@ protected:
 	bool haveNeutrinos;
 	bool haveElectrons;
 	bool haveAntiNucleons;
-	bool haveRedshiftDependence;
 
 public:
 	PhotoPionProduction(
@@ -37,21 +35,18 @@ public:
 		bool neutrinos = false,
 		bool electrons = false,
 		bool antiNucleons = false,
-		double limit = 0.1,
-		bool haveRedshiftDependence = false);
+		double limit = 0.1);
 	void setPhotonField(PhotonField photonField);
 	void setHavePhotons(bool b);
 	void setHaveNeutrinos(bool b);
 	void setHaveElectrons(bool b);
 	void setHaveAntiNucleons(bool b);
-	void setHaveRedshiftDependence(bool b);
 	void setLimit(double limit);
 	void initRate(std::string filename);
 	double nucleonMFP(double gamma, double z, bool onProton) const;
 	double nucleiModification(int A, int X) const;
 	void process(Candidate *candidate) const;
 	void performInteraction(Candidate *candidate, bool onProton) const;
-
 	/**
 	 Calculates the loss length E dx/dE in [m].
 	 This is not used in the simulation.
--- src/module/PhotoPionProduction.cpp
+++ src/module/PhotoPionProduction.cpp
@@ -2,47 +2,46 @@
 #include "crpropa/Units.h"
 #include "crpropa/ParticleID.h"
 #include "crpropa/Random.h"
+#include "crpropa/PhotonBackground.h"
 
 #include <kiss/convert.h>
 #include <kiss/logger.h>
 #include "sophia.h"
+#include <stdlib.h>
 
-#include <limits>
+#include <algorithm>
 #include <cmath>
-#include <sstream>
 #include <fstream>
+#include <limits>
+#include <sstream>
 #include <stdexcept>
+#include <string>
+#include <vector>
 
 namespace crpropa {
 
-PhotoPionProduction::PhotoPionProduction(PhotonField field, bool photons, bool neutrinos, bool electrons, bool antiNucleons, double l, bool redshift) {
+PhotoPionProduction::PhotoPionProduction(PhotonField field,
+		bool photons, bool neutrinos, bool electrons, bool antiNucleons, double l) {
 	havePhotons = photons;
 	haveNeutrinos = neutrinos;
 	haveElectrons = electrons;
 	haveAntiNucleons = antiNucleons;
-	haveRedshiftDependence = redshift;
 	limit = l;
 	setPhotonField(field);
+	this->customPhotonField = CustomPhotonField(getDataPath("Scaling/" + photonFieldName(field) + ".txt"));
 }
 
 void PhotoPionProduction::setPhotonField(PhotonField field) {
 	photonField = field;
-	if (haveRedshiftDependence) {
-		std::cout << "PhotoPionProduction: tabulated redshift dependence not needed for CMB, switching off" << std::endl;
-		haveRedshiftDependence = false;
-	}
 	std::string fname = photonFieldName(field);
 	setDescription("PhotoPionProduction: " + fname);
-	if (haveRedshiftDependence)
-		initRate(getDataPath("PhotoPionProduction/rate_" + fname.replace(0, 3, "IRBz") + ".txt"));
-	else
-		initRate(getDataPath("PhotoPionProduction/rate_" + fname + ".txt"));
+	initRate(getDataPath("PhotoPionProduction/rate_" + fname + ".txt"));
 }
 
 void PhotoPionProduction::setHavePhotons(bool b) {
 	havePhotons = b;
 }
-	
+
 void PhotoPionProduction::setHaveElectrons(bool b) {
 	haveElectrons = b;
 }
@@ -53,11 +52,6 @@ void PhotoPionProduction::setHaveNeutrinos(bool b) {
 
 void PhotoPionProduction::setHaveAntiNucleons(bool b) {
 	haveAntiNucleons = b;
-}
-
-void PhotoPionProduction::setHaveRedshiftDependence(bool b) {
-	haveRedshiftDependence = b;
-	setPhotonField(photonField);
 }
 
 void PhotoPionProduction::setLimit(double l) {
@@ -75,42 +69,26 @@ void PhotoPionProduction::initRate(std::string filename) {
 	if (!infile.good())
 		throw std::runtime_error("PhotoPionProduction: could not open file " + filename);
 
-	if (haveRedshiftDependence) {
-		double zOld = -1, aOld = -1;
-		while (infile.good()) {
-			if (infile.peek() == '#') {
-				infile.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-				continue;
-			}
-			double z, a, b, c;
-			infile >> z >> a >> b >> c;
-			if (!infile)
-				break;
-			if (z > zOld) {
-				tabRedshifts.push_back(z);
-				zOld = z;
-			}
-			if (a > aOld) {
-				tabLorentz.push_back(pow(10, a));
-				aOld = a;
-			}
-			tabProtonRate.push_back(b / Mpc);
-			tabNeutronRate.push_back(c / Mpc);
+	double zOld = -1, aOld = -1;
+	while (infile.good()) {
+		if (infile.peek() == '#') {
+			infile.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			continue;
 		}
-	} else {
-		while (infile.good()) {
-			if (infile.peek() == '#') {
-				infile.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-				continue;
-			}
-			double a, b, c;
-			infile >> a >> b >> c;
-			if (!infile)
-				break;
+		double z, a, b, c;
+		infile >> z >> a >> b >> c;
+		if (!infile)
+			break;
+		if (z > zOld) {
+			tabRedshifts.push_back(z);
+			zOld = z;
+		}
+		if (a > aOld) {
 			tabLorentz.push_back(pow(10, a));
-			tabProtonRate.push_back(b / Mpc);
-			tabNeutronRate.push_back(c / Mpc);
+			aOld = a;
 		}
+		tabProtonRate.push_back(b / Mpc);
+		tabNeutronRate.push_back(c / Mpc);
 	}
 
 	infile.close();
@@ -124,11 +102,7 @@ double PhotoPionProduction::nucleonMFP(double gamma, double z, bool onProton) co
 	if (gamma < tabLorentz.front() or (gamma > tabLorentz.back()))
 		return std::numeric_limits<double>::max();
 
-	double rate;
-	if (haveRedshiftDependence)
-		rate = interpolate2d(z, gamma, tabRedshifts, tabLorentz, tabRate);
-	else
-		rate = interpolate(gamma, tabLorentz, tabRate) * photonFieldScaling(photonField, z);
+	double rate = interpolate2d(z, gamma, tabRedshifts, tabLorentz, tabRate);
 
 	// cosmological scaling
 	rate *= pow(1 + z, 2);
@@ -182,14 +156,15 @@ void PhotoPionProduction::process(Candidate *candidate) const {
 				onProton = false;
 			}
 		}
-
 		// check if interaction does not happen
+		if (meanFreePath == std::numeric_limits<double>::max())
+			return;
+		
 		if (step < randDistance) {
 			if (totalRate > 0.)
 				candidate->limitNextStep(limit / totalRate);
 			return;
 		}
-
 		// interact and repeat with remaining step
 		performInteraction(candidate, onProton);
 		step -= randDistance;
@@ -209,130 +184,118 @@ void PhotoPionProduction::performInteraction(Candidate *candidate, bool onProton
 	// interaction products from particle <--> anti-particle
 	int sign = (id > 0) ? 1 : -1;
 
-	// arguments for SOPHIA
-	int nature = 1 - int(onProton); // interacting particle: 0 for proton, 1 for neutron
-	double Ein = EpA / GeV; // energy of in-going nucleon in GeV
-	double momentaList[5][2000]; // momentum list, what are the five components?
-	int particleList[2000]; // particle id list
-	int nParticles; // number of outgoing particles
-	double maxRedshift = 100; // IR photon density is zero above this redshift
-	int dummy1; // not needed
-	double dummy2[2]; // not needed
-	int background = (photonField == CMB) ? 1 : 2; // photon background: 1 for CMB, 2 for Kneiske IRB
+	// SOPHIA - input:
+	int nature = 1 - static_cast<int>(onProton);  // 0=proton, 1=neutron
+	double Ein = EpA / GeV;
+	double eps = customPhotonField.sampleEps(onProton, EpA, z);
 
-	// check if below SOPHIA's energy threshold
-	double E_threshold = (photonField == CMB) ? 3.72e18 * eV : 5.83e15 * eV;
-	if (EpA * (1 + z) < E_threshold)
-		return;
+	// SOPHIA - output:
+	double outputEnergy[2000];
+	int outPartID[2000];
+	int nOutPart;
 
-#pragma omp critical
+	#pragma omp critical
 	{
-		sophiaevent_(nature, Ein, momentaList, particleList, nParticles, z, background, maxRedshift, dummy1, dummy2, dummy2);
+		sophiaevent_(nature, Ein, eps, outputEnergy, outPartID, nOutPart);
 	}
 
+	// output particle treatment
 	Random &random = Random::instance();
 	Vector3d pos = random.randomInterpolatedPosition(candidate->previous.getPosition(), candidate->current.getPosition());
-	for (int i = 0; i < nParticles; i++) { // loop over out-going particles
-		double Eout = momentaList[3][i] * GeV; // only the energy is used; could be changed for more detail
-		int pType = particleList[i];
-		switch (pType) {
-		case 13: // proton
-		case 14: // neutron
-			if (A == 1) {
-				// single interacting nucleon
-				candidate->current.setEnergy(Eout);
-				try
-				{
-					candidate->current.setId(sign * nucleusId(1, 14 - pType));
-				}
-				catch (std::runtime_error &e)
-				{
-					KISS_LOG_ERROR<< "Something went wrong in the PhotoPionProduction\n" << "Please report this error on https://github.com/CRPropa/CRPropa3/issues including your simulation setup and the following random seed:\n" << Random::instance().getSeed_base64();
-					throw;
-				}
-			} else {
-				// interacting nucleon is part of nucleus: it is emitted from the nucleus
-				candidate->current.setEnergy(E - EpA);
-				try
-				{
-					candidate->current.setId(sign * nucleusId(A - 1, Z - int(onProton)));
-					candidate->addSecondary(sign * nucleusId(1, 14 - pType), Eout, pos);
-				}
-				catch (std::runtime_error &e)
-				{
-					KISS_LOG_ERROR<< "Something went wrong in the PhotoPionProduction\n" << "Please report this error on https://github.com/CRPropa/CRPropa3/issues including your simulation setup and the following random seed:\n" << Random::instance().getSeed_base64();
-					throw;
-				}
+	bool isPrimary = true;  // the first hadron in output is declared the primary
+	for (int i = 0; i < nOutPart; i++) { // loop over out-going particles
+		double Eout = outputEnergy[i] * GeV;  // only the energy is used; could be changed for more detail
+		int pType = outPartID[i];
+		try
+		{
+			switch (pType) {
+				case 13: // proton
+				case 14: // neutron
+					if (isPrimary) {
+						if (A == 1) {
+							// single interacting nucleon
+							candidate->current.setEnergy(Eout);
+							candidate->current.setId(sign * nucleusId(1, 14 - pType));
+						} else {
+							// interacting nucleon is part of nucleus: it is emitted from the nucleus
+							candidate->current.setEnergy(E - EpA);
+							candidate->current.setId(sign * nucleusId(A - 1, Z - int(onProton)));
+							candidate->addSecondary(sign * nucleusId(1, 14 - pType), Eout, pos);
+						}
+						isPrimary = false;
+					} else { 
+						candidate->addSecondary(sign * nucleusId(1, 14 - pType), Eout, pos);
+					}
+					break;
+				case -13: // anti-proton
+					if (haveAntiNucleons)
+						candidate->addSecondary(-sign * nucleusId(1, 1), Eout, pos);
+					break;
+				case -14: // anti-neutron
+					if (haveAntiNucleons)
+						candidate->addSecondary(-sign * nucleusId(1, 0), Eout, pos);
+					break;
+				case 1: // photon
+					if (havePhotons)
+						candidate->addSecondary(22, Eout, pos);
+					break;
+				case 2: // positron
+					if (haveElectrons)
+						candidate->addSecondary(sign * -11, Eout, pos);
+					break;
+				case 3: // electron
+					if (haveElectrons)
+						candidate->addSecondary(sign * 11, Eout, pos);
+					break;
+				case 15: // nu_e
+					if (haveNeutrinos)
+						candidate->addSecondary(sign * 12, Eout, pos);
+					break;
+				case 16: // antinu_e
+					if (haveNeutrinos)
+						candidate->addSecondary(sign * -12, Eout, pos);
+					break;
+				case 17: // nu_muon
+					if (haveNeutrinos)
+						candidate->addSecondary(sign * 14, Eout, pos);
+					break;
+				case 18: // antinu_muon
+					if (haveNeutrinos)
+						candidate->addSecondary(sign * -14, Eout, pos);
+					break;
+				default:
+					throw std::runtime_error("PhotoPionProduction: unexpected particle " + kiss::str(pType));
 			}
-			break;
-		case -13: // anti-proton
-		case -14: // anti-neutron
-			if (haveAntiNucleons)
-				try
-				{
-					candidate->addSecondary(-sign * nucleusId(1, 14 + pType), Eout, pos);
-				}
-				catch (std::runtime_error &e)
-				{
-					KISS_LOG_ERROR<< "Something went wrong in the PhotoPionProduction\n" << "Something went wrong in the PhotoPionProduction\n"<< "Please report this error on https://github.com/CRPropa/CRPropa3/issues including your simulation setup and the following random seed:\n" << Random::instance().getSeed_base64();
-					throw;
-				}
-			break;
-		case 1: // photon
-			if (havePhotons)
-				candidate->addSecondary(22, Eout, pos);
-			break;
-		case 2: // positron
-			if (haveElectrons)
-				candidate->addSecondary(sign * -11, Eout, pos);
-			break;
-		case 3: // electron
-			if (haveElectrons)
-				candidate->addSecondary(sign * 11, Eout, pos);
-			break;
-		case 15: // nu_e
-			if (haveNeutrinos)
-				candidate->addSecondary(sign * 12, Eout, pos);
-			break;
-		case 16: // antinu_e
-			if (haveNeutrinos)
-				candidate->addSecondary(sign * -12, Eout, pos);
-			break;
-		case 17: // nu_muon
-			if (haveNeutrinos)
-				candidate->addSecondary(sign * 14, Eout, pos);
-			break;
-		case 18: // antinu_muon
-			if (haveNeutrinos)
-				candidate->addSecondary(sign * -14, Eout, pos);
-			break;
-		default:
-			throw std::runtime_error("PhotoPionProduction: unexpected particle " + kiss::str(pType));
+		}
+		catch (std::runtime_error &e)
+		{
+			KISS_LOG_ERROR<< "Something went wrong in the PhotoPionProduction\n" << "Please report this error on https://github.com/CRPropa/CRPropa3/issues including your simulation setup and the following random seed:\n" << Random::instance().getSeed_base64();
+			throw;
 		}
 	}
 }
 
 double PhotoPionProduction::lossLength(int id, double gamma, double z) {
-	int A = massNumber(id);
-	int Z = chargeNumber(id);
-	int N = A - Z;
+    int A = massNumber(id);
+    int Z = chargeNumber(id);
+    int N = A - Z;
 
-	double lossRate = 0;
-	if (Z > 0)
-		lossRate += 1 / nucleonMFP(gamma, z, true) * nucleiModification(A, Z);
-	if (N > 0)
-		lossRate += 1 / nucleonMFP(gamma, z, false) * nucleiModification(A, N);
+    double lossRate = 0;
+    if (Z > 0)
+        lossRate += 1 / nucleonMFP(gamma, z, true) * nucleiModification(A, Z);
+    if (N > 0)
+        lossRate += 1 / nucleonMFP(gamma, z, false) * nucleiModification(A, N);
 
-	// approximate the relative energy loss
-	// - nucleons keep the fraction of mass to delta-resonance mass
-	// - nuclei lose the energy 1/A the interacting nucleon is carrying
-	double relativeEnergyLoss = (A == 1) ? 1 - 938. / 1232. : 1. / A;
-	lossRate *= relativeEnergyLoss;
+    // approximate the relative energy loss
+    // - nucleons keep the fraction of mass to delta-resonance mass
+    // - nuclei lose the energy 1/A the interacting nucleon is carrying
+    double relativeEnergyLoss = (A == 1) ? 1 - 938. / 1232. : 1. / A;
+    lossRate *= relativeEnergyLoss;
 
-	// scaling factor: interaction rate --> energy loss rate
-	lossRate *= (1 + z);
+    // scaling factor: interaction rate --> energy loss rate
+    lossRate *= (1 + z);
 
-	return 1. / lossRate;
+    return 1. / lossRate;
 }
 
 } // namespace crpropa

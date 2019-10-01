c61d582198d749a84c1a23ea5eace72f77c04838
Thomas Fitoussi <tfitoussi@mailoo.org>
2019-10-01 12:10:49 -0500

    [TF17] Add a selection between the different models

--- include/crpropa/magneticField/TF17Field.h
+++ include/crpropa/magneticField/TF17Field.h
@@ -2,27 +2,33 @@
 #define CRPROPA_TF17FIELD_H
 
 #include "crpropa/magneticField/MagneticField.h"
+#include <string>
 
 namespace crpropa {
+using namespace std;
 
 /**
  Implements the TF2017 galactic magnetic field model, consisting of large-scale regular disk and halo fields.
-
- Currently only best fit values of the field parameters are implemented: Halo model C1; disk model Ad1.
  The field is defined in the usual galactocentric coordinate system with the Galactic center at the origin,
  the x-axis pointing in the opposite direction of  the Sun, and the z-axis pointing towards Galactic north.
-
  See: Terral, Ferriere 2017 - Constraints from Faraday rotation on the magnetic field structure in the galactic halo, DOI: 10.1051/0004-6361/201629572, arXiv:1611.10222
+
  */
 
 class TF17Field: public MagneticField {
 private:
+    // model definition
+    string disk_model;
+    string halo_model;
 
 	// disk parameters
 	bool useDiskField;
 	double a_disk;
+	double z1_disk;
 	double r1_disk;
+	double n_disk;
 	double B1_disk;
+	double L_disk;
 	double phi_star_disk;
 	double H_disk;
 
@@ -44,24 +50,24 @@ private:
 	void SetParams();
 
 public:
-	TF17Field();
+	TF17Field(string halo_mod="C0", string disk_mod="Ad1");
 
 	void setUseDiskField(bool use);
 	void setUseHaloField(bool use);
 	bool isUsingDiskField();
 	bool isUsingHaloField();
 
+	Vector3d getField(const Vector3d& pos) const;
+	Vector3d getDiskField(const double& r, const double& z, const double& phi, const double& sinPhi, const double& cosPhi) const;
+	Vector3d getHaloField(const double& r, const double& z, const double& phi, const double& sinPhi, const double& cosPhi) const;
+
 	double zscale(const double& z) const;
 	double shiftedWindingFunction(const double& r, const double& z) const;
 	double radialFieldScale(const double& B1, const double& r1, const double& z1, const double& phi1) const;
 	double verticalFieldScale(const double& B1, const double& r1, const double& z1, const double& phi1) const;
 	double azimuthalFieldComponent(const double& r, const double& z, const double& B_r, const double& B_z) const;
-
-	Vector3d getHaloField(const double& r, const double& z, const double& phi, const double& sinPhi, const double& cosPhi) const;
-	Vector3d getDiskField(const double& r, const double& z, const double& phi, const double& sinPhi, const double& cosPhi) const;
-	Vector3d getField(const Vector3d& pos) const;
 };
 
-} // namespace crpropa
+} // CRPROPA NAMESPACE
 
 #endif // CRPROPA_TF17FIELD_H
--- src/magneticField/TF17Field.cpp
+++ src/magneticField/TF17Field.cpp
@@ -2,32 +2,153 @@
 #include "crpropa/Units.h"
 
 #include <algorithm>
+#include <string>
 
 namespace crpropa {
+using namespace std;
 
-TF17Field::TF17Field() {
-	// disk parameters
-	useDiskField = true;
-	a_disk = 0.031 / kpc / kpc;
-	r1_disk = 3 * kpc;
-	B1_disk = 32.0 * muG;
-	phi_star_disk = -31 * M_PI / 180;
-	H_disk = 0.054 * kpc;
+TF17Field::TF17Field(string halo_mod, string disk_mod){
+    halo_model = halo_mod;
+    useHaloField = true;
+    disk_model = disk_mod;
+    useDiskField = true;
 
-	// halo parameters
-	useHaloField = true;
-	a_halo = 0.33 / kpc / kpc;
-	z1_halo = 0 * kpc;
-	B1_halo = 9.0 * muG;
-	L_halo = 1.2 * kpc;
-	phi_star_halo = 198 * M_PI / 180;
-	m = 1; // for bisymmetric halo model
+    if (( halo_model == "C0" ) && ( disk_model == "Ad1" )){
+	    // disk parameters
+	    r1_disk = 3 * kpc;
+	    B1_disk = 19.0 * muG;
+	    H_disk = 0.055 * kpc;
+	    phi_star_disk = -54 * M_PI / 180;
+	    a_disk = 0.9 / kpc / kpc;
 
-	// shared parameters
-	p_0 = -9.1 * M_PI / 180;
-	cot_p0 = cos(p_0) / sin(p_0);
-	H_p = 1.2 * kpc;
-	L_p = 50 * kpc;		// best fit simply says L_p > 40 kpc:  L_p ≫ L_e can hardly be constrained by the FDobs map.
+	    // halo parameters
+	    z1_halo = 0 * kpc;
+	    B1_halo = 0.36 * muG;
+	    L_halo = 3.0 * kpc;
+	    a_halo = 1.17 / kpc / kpc;
+	    m = 1; // for bisymmetric halo model
+
+	    // shared parameters
+	    p_0 = -7.9 * M_PI / 180;
+	    cot_p0 = cos(p_0) / sin(p_0);
+	    H_p = 5 * kpc;
+	    L_p = 18 * kpc;	
+
+    } else if (( halo_model == "C0" ) && ( disk_model == "Bd1" )){
+	    // disk parameters
+	    r1_disk = 3 * kpc;
+        n_disk = 2;
+	    B1_disk = 2.0 * muG;
+	    H_disk = 0.32 * kpc;
+	    phi_star_disk = -31 * M_PI / 180;
+
+	    // halo parameters
+	    z1_halo = 0 * kpc;
+	    B1_halo = 9.0 * muG;
+	    L_halo = 3.4 * kpc;
+	    a_halo = 0.88 / kpc / kpc;
+	    m = 1; // for bisymmetric halo model
+
+	    // shared parameters
+	    p_0 = -7.2 * M_PI / 180;
+	    cot_p0 = cos(p_0) / sin(p_0);
+	    H_p = 9 * kpc;
+	    L_p = 16 * kpc;		
+
+    } else if (( halo_model == "C0" ) && ( disk_model == "Dd1" )){
+	    // disk parameters
+	    z1_disk = 1.5 * kpc;
+        n_disk = 0.5;
+	    B1_disk = 0.065 * muG;
+	    L_disk = 9.8 * kpc;
+	    phi_star_disk = 14 * M_PI / 180;
+
+	    // halo parameters
+	    z1_halo = 0 * kpc;
+	    B1_halo = 0.18 * muG;
+	    L_halo = 4.8 * kpc;
+	    a_halo = 0.61 / kpc / kpc;
+	    m = 1; // for bisymmetric halo model
+
+	    // shared parameters
+	    p_0 = -7.4 * M_PI / 180;
+	    cot_p0 = cos(p_0) / sin(p_0);
+	    H_p = 4.2 * kpc;
+	    L_p = 22 * kpc;		
+
+    } else if (( halo_model == "C1" ) && ( disk_model == "Ad1" )){
+	    // disk parameters
+	    r1_disk = 3 * kpc;
+	    B1_disk = 32.0 * muG;
+	    H_disk = 0.054 * kpc;
+	    phi_star_disk = -31 * M_PI / 180;
+	    a_disk = 0.031 / kpc / kpc;
+
+	    // halo parameters
+	    z1_halo = 0 * kpc;
+	    B1_halo = 9.0 * muG;
+	    L_halo = 2.1 * kpc;
+	    phi_star_halo = 198 * M_PI / 180;
+	    a_halo = 0.33 / kpc / kpc;
+	    m = 1; // for bisymmetric halo model
+
+	    // shared parameters
+	    p_0 = -9.1 * M_PI / 180;
+	    cot_p0 = cos(p_0) / sin(p_0);
+	    H_p = 1.2 * kpc;
+	    L_p = 38 * kpc;	
+
+    } else if (( halo_model == "C1" ) && ( disk_model == "Bd1" )){
+	    // disk parameters
+	    r1_disk = 3 * kpc;
+        n_disk = 2;
+	    B1_disk = 24 * muG;
+	    H_disk = 0.090 * kpc;
+	    phi_star_disk = -34 * M_PI / 180;
+
+	    // halo parameters
+	    z1_halo = 0 * kpc;
+	    B1_halo = 8.2 * muG;
+	    L_halo = 2.2 * kpc;
+	    phi_star_halo = 197 * M_PI / 180;
+	    a_halo = 0.38 / kpc / kpc;
+	    m = 1; // for bisymmetric halo model
+
+	    // shared parameters
+	    p_0 = -9.0 * M_PI / 180;
+	    cot_p0 = cos(p_0) / sin(p_0);
+	    H_p = 1.2 * kpc;
+	    L_p = 38 * kpc;		
+
+    } else if (( halo_model == "C1" ) && ( disk_model == "Dd1" )){
+	    // disk parameters
+	    z1_disk = 1.5 * kpc;
+        n_disk = 0.5;
+	    B1_disk = 0.40 * muG;
+	    L_disk = 2.9 * kpc;
+	    phi_star_disk = 120 * M_PI / 180;
+
+	    // halo parameters
+	    z1_halo = 0 * kpc;
+	    B1_halo = 9.5 * muG;
+	    L_halo = 2.1 * kpc;
+	    phi_star_halo = 197 * M_PI / 180;
+	    a_halo = 0.45 / kpc / kpc;
+	    m = 1; // for bisymmetric halo model
+
+	    // shared parameters
+	    p_0 = -8.4 * M_PI / 180;
+	    cot_p0 = cos(p_0) / sin(p_0);
+	    H_p = 1.2 * kpc;
+	    L_p = 30 * kpc;	    
+    
+    } else { // wrong model
+        cerr << "ERROR in TD17Field declaration:" << endl;
+        cerr << "   Disk model (" << disk_model << ") can only be : Ad1, Bd1 or Dd1" << endl;
+        cerr << "   Halo model (" << halo_model << ") can only be : C0 or C1" << endl;
+        exit(-1);
+
+    }
 }
 
 void TF17Field::setUseDiskField(bool use) {
@@ -46,46 +167,19 @@ bool TF17Field::isUsingHaloField() {
 	return useHaloField;
 }
 
-double TF17Field::zscale(const double& z) const {
-	return 1 + (fabs(z) / H_p) * (fabs(z) / H_p);
-}
+Vector3d TF17Field::getField(const Vector3d& pos) const {
+	double r = sqrt(pos.x * pos.x + pos.y * pos.y);  // in-plane radius
+	double phi = M_PI - pos.getPhi(); // azimuth in our convention
+	// double cosPhi = pos.x / r;
+	double cosPhi = cos(phi);
+	// double sinPhi = pos.y / r;
+	double sinPhi = sin(phi);
 
-double TF17Field::shiftedWindingFunction(const double& r, const double& z) const {
-	return cot_p0 * log(1 - exp(-r / L_p)) / zscale(z);
-}
-
-double TF17Field::radialFieldScale(const double& B1, const double& r1, const double& z1, const double& phi1) const {
-	// This term occures is parameterizations of models A and B
-	return B1 * exp(-fabs(z1) / H_disk) * cos(m * (phi1 - shiftedWindingFunction(r1, z1) - phi_star_disk));
-}
-
-double TF17Field::verticalFieldScale(const double& B1, const double& r1, const double& z1, const double& phi1) const {
-	// This term occures is parameterizations of models C and D
-	return B1 * exp(-r1 / L_halo) * cos(m * (phi1 - shiftedWindingFunction(r1, z1) - phi_star_halo));
-}
-
-double TF17Field::azimuthalFieldComponent(const double& r, const double& z, const double& B_r, const double& B_z) const {
-	double r_ = r / L_p;
-	double B_phi = cot_p0 / zscale(z) * r_ * exp(-r_) / (1 - exp(-r_)) * B_r - \
-								 2 * cot_p0 * z / (H_p * H_p) / (zscale(z) * zscale(z)) * r * log(1 - exp(-r_)) * B_z;
-	return B_phi;
-}
-
-Vector3d TF17Field::getHaloField(const double& r, const double& z, const double& phi, const double& sinPhi, const double& cosPhi) const {
-	// Model C1, bisymmetric (m=1)
 	Vector3d b(0.);
-
-	double r1_halo = r / (1 + a_halo * z * z);
-	double phi1_halo = phi - shiftedWindingFunction(r, z) + shiftedWindingFunction(r1_halo, z1_halo);
-	// B components in (r, phi, z)
-	double r_ = r > std::numeric_limits<double>::epsilon() ? (r1_halo / r) : 1;		// avoid zero division
-	double B_r = 2 * a_halo * r_ * r_ * r1_halo * z * verticalFieldScale(B1_halo, r1_halo, z1_halo, phi1_halo);
-	double B_z = r_ * r_ * verticalFieldScale(B1_halo, r1_halo, z1_halo, phi1_halo);
-	double B_phi = azimuthalFieldComponent(r, z, B_r, B_z);
-	// Convert to (x, y, z) components
-	b.x = - (B_r * cosPhi - B_phi * sinPhi);	// flip x-component at the end
-	b.y = B_r * sinPhi + B_phi * cosPhi;
-	b.z = B_z;
+	if (useDiskField)
+		b += getDiskField(r, pos.z, phi, sinPhi, cosPhi);	// disk field
+	if (useHaloField)
+		b += getHaloField(r, pos.z, phi, sinPhi, cosPhi);	// halo field
 	return b;
 }
 
@@ -119,20 +213,47 @@ Vector3d TF17Field::getDiskField(const double& r, const double& z, const double&
 	return b;
 }
 
-Vector3d TF17Field::getField(const Vector3d& pos) const {
-	double r = sqrt(pos.x * pos.x + pos.y * pos.y);  // in-plane radius
-	double phi = M_PI - pos.getPhi(); // azimuth in our convention
-	// double cosPhi = pos.x / r;
-	double cosPhi = cos(phi);
-	// double sinPhi = pos.y / r;
-	double sinPhi = sin(phi);
-
+Vector3d TF17Field::getHaloField(const double& r, const double& z, const double& phi, const double& sinPhi, const double& cosPhi) const {
+	// Model C1, bisymmetric (m=1)
 	Vector3d b(0.);
-	if (useDiskField)
-		b += getDiskField(r, pos.z, phi, sinPhi, cosPhi);	// disk field
-	if (useHaloField)
-		b += getHaloField(r, pos.z, phi, sinPhi, cosPhi);	// halo field
+
+	double r1_halo = r / (1 + a_halo * z * z);
+	double phi1_halo = phi - shiftedWindingFunction(r, z) + shiftedWindingFunction(r1_halo, z1_halo);
+	// B components in (r, phi, z)
+	double r_ = r > std::numeric_limits<double>::epsilon() ? (r1_halo / r) : 1;		// avoid zero division
+	double B_r = 2 * a_halo * r_ * r_ * r1_halo * z * verticalFieldScale(B1_halo, r1_halo, z1_halo, phi1_halo);
+	double B_z = r_ * r_ * verticalFieldScale(B1_halo, r1_halo, z1_halo, phi1_halo);
+	double B_phi = azimuthalFieldComponent(r, z, B_r, B_z);
+	// Convert to (x, y, z) components
+	b.x = - (B_r * cosPhi - B_phi * sinPhi);	// flip x-component at the end
+	b.y = B_r * sinPhi + B_phi * cosPhi;
+	b.z = B_z;
 	return b;
+}
+
+double TF17Field::zscale(const double& z) const {
+	return 1 + (fabs(z) / H_p) * (fabs(z) / H_p);
+}
+
+double TF17Field::shiftedWindingFunction(const double& r, const double& z) const {
+	return cot_p0 * log(1 - exp(-r / L_p)) / zscale(z);
+}
+
+double TF17Field::radialFieldScale(const double& B1, const double& r1, const double& z1, const double& phi1) const {
+	// This term occures is parameterizations of models A and B
+	return B1 * exp(-fabs(z1) / H_disk) * cos(m * (phi1 - shiftedWindingFunction(r1, z1) - phi_star_disk));
+}
+
+double TF17Field::verticalFieldScale(const double& B1, const double& r1, const double& z1, const double& phi1) const {
+	// This term occures is parameterizations of models C and D
+	return B1 * exp(-r1 / L_halo) * cos(m * (phi1 - shiftedWindingFunction(r1, z1) - phi_star_halo));
+}
+
+double TF17Field::azimuthalFieldComponent(const double& r, const double& z, const double& B_r, const double& B_z) const {
+	double r_ = r / L_p;
+	double B_phi = cot_p0 / zscale(z) * r_ * exp(-r_) / (1 - exp(-r_)) * B_r - \
+								 2 * cot_p0 * z / (H_p * H_p) / (zscale(z) * zscale(z)) * r * log(1 - exp(-r_)) * B_z;
+	return B_phi;
 }
 
 } // namespace crpropa

c3897bff8df645bac7f37ea1d62f2c21e47117f6
Rafa <8rafael@gmail.com>
2020-04-14 16:06:55 +0200

    remove EleCa/DINT

--- libs/dint/include/dint/gauleg.h
+++
@@ -1,7 +0,0 @@
-#ifndef DINT__GAULEG_H
-#define DINT__GAULEG_H
-
-void Gauleg(const double x1, const double x2, double x[], double w[], 
-            const int n);
-
-#endif
--- libs/dint/include/dint/io_util.h
+++
@@ -1,8 +0,0 @@
-#ifndef DINT__IO_UTIL_H
-#define DINT__IO_UTIL_H
-
-#include <stdio.h>
-
-FILE* SafeFOpen(const char* filename, const char* mode);
-
-#endif

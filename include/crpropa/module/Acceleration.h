d91fe8bcda14e8f96b3a225eb423c7cb286a5fc1
Tobias Winchen <tobias.winchen@rwth-aachen.de>
2021-02-03 10:07:11 +0100

    Updated clang-format

--- include/crpropa/module/Acceleration.h
+++ include/crpropa/module/Acceleration.h
@@ -2,144 +2,148 @@
 #define ACCELERATION_MODULE
 
 #include <crpropa/Candidate.h>
-#include <crpropa/Module.h>
-#include <crpropa/Vector3.h>
-#include <crpropa/Units.h>
 #include <crpropa/Geometry.h>
+#include <crpropa/Module.h>
+#include <crpropa/Units.h>
+#include <crpropa/Vector3.h>
 
 #include <string>
 
-namespace crpropa
-{
+namespace crpropa {
 /** @addtogroup Acceleration
  *  @{
  */
 
 /// @class StepLengthModifier
 /// @brief Modifies the steplength of an acceleration module.
-class StepLengthModifier : public Referenced
-{
-	public:
-		/// Returns an update of the steplength
-		virtual double modify(double steplength, Candidate* candidate) = 0;
+class StepLengthModifier : public Referenced {
+  public:
+	/// Returns an update of the steplength
+	virtual double modify(double steplength, Candidate *candidate) = 0;
 };
 
 
 /// @class AbstractAccelerationModule
-/// @brief Core functionallity for acceleration by scattering with scatter centers
-///  moving in a velocity field.
+/// @brief Core functionallity for acceleration by scattering with scatter
+///  centers moving in a velocity field.
 /// @details The velocity field is implicity implemented in the derived classes
 ///  for performance reasons. Models for the dependence of the step length of
 ///  the scatter process are set via modifiers.
-class AbstractAccelerationModule : public Module
-{
+class AbstractAccelerationModule : public Module {
 	double stepLength;
-	std::vector<ref_ptr<StepLengthModifier> > modifiers;
+	std::vector<ref_ptr<StepLengthModifier>> modifiers;
 
-	public:
-		/// The parent's constructor need to be called on initialization!
-		AbstractAccelerationModule(double _stepLength = 1. * parsec);
-		// add a step length modifier to the model
-		void add(StepLengthModifier *modifier);
-		// update the candidate
-		void process(Candidate *candidate) const;
+  public:
+	/// The parent's constructor need to be called on initialization!
+	AbstractAccelerationModule(double _stepLength = 1. * parsec);
+	// add a step length modifier to the model
+	void add(StepLengthModifier *modifier);
+	// update the candidate
+	void process(Candidate *candidate) const;
 
-		/// Returns the velocity vector of the scatter centers in the rest frame of the
-		/// candidate.
-		/// Needs to be implemented in inheriting classes.
-		virtual Vector3d scatterCenterVelocity(Candidate *candidate) const = 0;
+	/// Returns the velocity vector of the scatter centers in the rest frame of
+	/// the candidate. Needs to be implemented in inheriting classes.
+	virtual Vector3d scatterCenterVelocity(Candidate *candidate) const = 0;
 
-		/// Scatter the candidate with a center with given scatter center
-		/// velocity into a random direction. Assumes that the
-		/// candidate is ultra-relativistic (m = 0).
-		void scatter(Candidate* candidate, const Vector3d& scatter_center_velocity) const;
+	/// Scatter the candidate with a center with given scatter center
+	/// velocity into a random direction. Assumes that the
+	/// candidate is ultra-relativistic (m = 0).
+	void scatter(Candidate *candidate,
+	             const Vector3d &scatter_center_velocity) const;
 };
 
 
 /// @class SecondOrderFermi
 /// @brief  Implements scattering with centers moving in isotropic directions.
 ///   All scatter centers have the same velocity.
-class SecondOrderFermi : public AbstractAccelerationModule
-{
+class SecondOrderFermi : public AbstractAccelerationModule {
 	double scatterVelocity;
 	std::vector<double> angle;
 	std::vector<double> angleCDF;
 
-	public:
-		SecondOrderFermi(double _scatterVelocity=.1 * crpropa::c_light, double stepLength = 1. * crpropa::parsec, unsigned int size_of_pitchangle_table=10000);
-		virtual crpropa::Vector3d scatterCenterVelocity(crpropa::Candidate *candidate) const;
+  public:
+	SecondOrderFermi(double _scatterVelocity = .1 * crpropa::c_light,
+	                 double stepLength = 1. * crpropa::parsec,
+	                 unsigned int size_of_pitchangle_table = 10000);
+	virtual crpropa::Vector3d
+	scatterCenterVelocity(crpropa::Candidate *candidate) const;
 };
 
 
 /// @class DirectedFlowScattering
 /// @brief Scattering in a directed flow of scatter centers.
 /// @details Two of these region with different
-///		velocities can be used to create first order Fermi scenario.
-///		Thanks to Aritra Ghosh, Groningn University, for first work in 2017 on the
-///		shock acceleration in CRPropa leading to this module.
-class DirectedFlowScattering : public AbstractAccelerationModule
-{
-	private:
-		crpropa::Vector3d __scatterVelocity;
-	public:
-		DirectedFlowScattering(crpropa::Vector3d scatterCenterVelocity, double stepLength=1. * parsec);
-		virtual crpropa::Vector3d scatterCenterVelocity(crpropa::Candidate *candidate) const;
+///    velocities can be used to create first order Fermi scenario.
+///    Thanks to Aritra Ghosh, Groningn University, for first work in 2017 on
+///    the shock acceleration in CRPropa leading to this module.
+class DirectedFlowScattering : public AbstractAccelerationModule {
+  private:
+	crpropa::Vector3d __scatterVelocity;
+
+  public:
+	DirectedFlowScattering(crpropa::Vector3d scatterCenterVelocity,
+	                       double stepLength = 1. * parsec);
+	virtual crpropa::Vector3d
+	scatterCenterVelocity(crpropa::Candidate *candidate) const;
 };
 
 
 /// @class DirectedFlowOfScatterCenters
-/// @brief In a directed flow, the step length depend on the direction of the particles as headon collisions are more likely than tail=on collisions - propagating against the flow is harder.
-class DirectedFlowOfScatterCenters: public StepLengthModifier
-{
-	private:
-		Vector3d __scatterVelocity;
-	public:
+/// @brief In a directed flow, the step length depend on the direction of the
+/// particles as headon collisions are more likely than tail=on collisions -
+/// propagating against the flow is harder.
+class DirectedFlowOfScatterCenters : public StepLengthModifier {
+  private:
+	Vector3d __scatterVelocity;
+
+  public:
 	DirectedFlowOfScatterCenters(const Vector3d &scatterCenterVelocity);
-	double modify(double steplength, Candidate* candidate);
+	double modify(double steplength, Candidate *candidate);
 };
 
 
 /// @class QuasiLinearTheory
 /// @brief Scales the steplength according to quasi linear theory.
-/// @details Following quasi-linear theory [Schlickeiser1989], the mean free path \f$\lambda\f$ of a
-///  particle with energy \f$E\f$ and charge \f$Z\f$ in a field with turbulence spectrum
-///  \f$\frac{k}{k_{\min}}^{-q}\f$ is
-///   \f[ \lambda = {\left(\frac{B}{\delta B}\right)}^2 {\left(R_G\;  k_{\min}\right)}^{1-q} R_G \equiv \lambda_0 {\left( \frac{E}{1 EeV}\frac{1}{Z} \right)}^{2-q} \f]
+/// @details Following quasi-linear theory [Schlickeiser1989], the mean free
+/// path \f$\lambda\f$ of a
+///  particle with energy \f$E\f$ and charge \f$Z\f$ in a field with turbulence
+///  spectrum \f$\frac{k}{k_{\min}}^{-q}\f$ is
+///   \f[ \lambda = {\left(\frac{B}{\delta B}\right)}^2 {\left(R_G\;
+///   k_{\min}\right)}^{1-q} R_G \equiv \lambda_0 {\left( \frac{E}{1
+///   EeV}\frac{1}{Z} \right)}^{2-q} \f]
 ///  where \f$R_G = \frac{E}{B Z}\f$ is the gyro-radius of the
 ///  particles.
 /// This class implements the rigidity dependent scaling factor used to modify
 /// the base step length.
 /// @par
-/// \b [Schlickeiser1989] R. Schlickeiser, Cosmic-Ray Transport and Acceleration. II.
-///  Cosmic Rays in Moving Cold Media with Application to Diffu-
-///  sive Shock Wave Acceleration, The Astrophysical Journal 336
-///  (1989) 264. doi:10.1086/167010.
-class QuasiLinearTheory : public StepLengthModifier
-{
+/// \b [Schlickeiser1989] R. Schlickeiser, Cosmic-Ray Transport and
+///      Acceleration. II. Cosmic Rays in Moving Cold Media with Application to
+///      Diffusive Shock Wave Acceleration,
+///      The Astrophysical Journal 336 (1989) 264. doi:10.1086/167010.
+class QuasiLinearTheory : public StepLengthModifier {
 	private:
-		double __referenceEnergy;
-		double __turbulenceIndex;
-		double __minimumRigidity;
+	double __referenceEnergy;
+	double __turbulenceIndex;
+	double __minimumRigidity;
 
-	public:
-		QuasiLinearTheory(double referenecEnergy=1.*EeV, double turbulence_index=5./3, double minimumRigidity=0);
-		double modify(double steplength, Candidate* candidate);
+  public:
+	QuasiLinearTheory(double referenecEnergy = 1. * EeV,
+	                  double turbulence_index = 5. / 3,
+	                  double minimumRigidity = 0);
+	double modify(double steplength, Candidate *candidate);
 };
-
-
 
 
 /// @class ParticleSplitting
 /// @brief  Implements particle splitting, i.e. inverse thinning, to speed up
-//acceleration
+///  the simulation.
 /// @details After crossing a surface a given number of times, the particle is
 /// split to N partilces with weight 1/N. This eases performance constraints in
 /// acceleration simulations due to the power law nature of many acceleration
 /// mechanisms.
 /// Thanks to Matthew Weiss, Penn State University for the first work on this
 /// feature in 2017.
-class ParticleSplitting: public Module
-{
+class ParticleSplitting : public Module {
 	int num_splits;
 	int crossing_threshold;
 	double min_weight;
@@ -147,19 +151,20 @@ class ParticleSplitting: public Module
 	std::string counterid;
 
 	public:
-		/// @params surface							 The surface to monitor
-		/// @params crossing_threshold   Number of crossings after which a particle is split
-		/// @params num_splits					 Number of particles the candidate is split into
-		/// @params min_weight					 Minimum weight to consider. PArticles with
-		///															 a lower weight are not split again.
-		/// @params counterid						 An unique string to identify the particle
-		///															 property used for counting. Useful if
-		///                              multiple splitting modules are present.
-		ParticleSplitting(Surface *surface, int crossing_threshold=50,
-				int num_splits=5, double min_weight=0.01, std::string counterid="ParticleSplittingCounter");
+	/// @params surface               The surface to monitor
+	/// @params crossing_threshold    Number of crossings after which a particle is split
+	/// @params num_splits            Number of particles the candidate is split into
+	/// @params min_weight            Minimum weight to consider. Particles with
+	///                               a lower weight are not split again.
+	/// @params counterid             An unique string to identify the particle
+	///                               property used for counting. Useful if
+	///                               multiple splitting modules are present.
+	ParticleSplitting(Surface *surface, int crossing_threshold = 50,
+	                  int num_splits = 5, double min_weight = 0.01,
+	                  std::string counterid = "ParticleSplittingCounter");
 
-		// update the candidate
-		void process(Candidate *candidate) const;
+	// update the candidate
+	void process(Candidate *candidate) const;
 };
 
 
--- src/module/Acceleration.cpp
+++ src/module/Acceleration.cpp
@@ -3,22 +3,20 @@
 #include <crpropa/Random.h>
 #include <cmath>
 
-namespace crpropa
-{
+namespace crpropa {
 
-AbstractAccelerationModule::AbstractAccelerationModule(double _stepLength) : crpropa::Module(), stepLength(_stepLength)
-{
-}
+AbstractAccelerationModule::AbstractAccelerationModule(double _stepLength)
+    : crpropa::Module(), stepLength(_stepLength) {}
 
 
-void AbstractAccelerationModule::add(StepLengthModifier *modifier)
-{
+void AbstractAccelerationModule::add(StepLengthModifier *modifier) {
 	modifiers.push_back(modifier);
 }
 
 
-void AbstractAccelerationModule::scatter(crpropa::Candidate *candidate, const crpropa::Vector3d &scatter_center_velocity) const
-{
+void AbstractAccelerationModule::scatter(
+    crpropa::Candidate *candidate,
+    const crpropa::Vector3d &scatter_center_velocity) const {
 	// particle momentum in lab frame
 	const double E = candidate->current.getEnergy();
 	const crpropa::Vector3d p = candidate->current.getMomentum();
@@ -27,14 +25,16 @@ void AbstractAccelerationModule::scatter(crpropa::Candidate *candidate, const cr
 	const double beta = scatter_center_velocity.getR() / crpropa::c_light;
 	const double gamma = 1. / sqrt(1 - beta * beta);
 	const double Ep = gamma * (E - scatter_center_velocity.dot(p));
-	const crpropa::Vector3d pp = (p - scatter_center_velocity* E / (crpropa::c_light * crpropa::c_light)) * gamma;
+	const crpropa::Vector3d pp = (p - scatter_center_velocity* E /
+	    (crpropa::c_light * crpropa::c_light)) * gamma;
 
 	// scatter into random direction
 	const crpropa::Vector3d pp_new = crpropa::Random::instance().randVector() * pp.getR();
 
 	// transform back
 	const double E_new = gamma * (Ep + scatter_center_velocity.dot(pp_new));
-	const crpropa::Vector3d p_new = (pp_new + scatter_center_velocity * Ep / (crpropa::c_light * crpropa::c_light)) * gamma;
+	const crpropa::Vector3d p_new = (pp_new + scatter_center_velocity * Ep /
+	    (crpropa::c_light * crpropa::c_light)) * gamma;
 
 	// update candidate properties
 	candidate->current.setEnergy(E_new);
@@ -42,39 +42,36 @@ void AbstractAccelerationModule::scatter(crpropa::Candidate *candidate, const cr
 }
 
 
-void AbstractAccelerationModule::process(crpropa::Candidate *candidate) const
-{
+void AbstractAccelerationModule::process(crpropa::Candidate *candidate) const {
 	double currentStepLength = stepLength;
-	for (auto m : modifiers)
-	{
-			currentStepLength = m->modify(currentStepLength, candidate);
+	for (auto m : modifiers) {
+		currentStepLength = m->modify(currentStepLength, candidate);
 	}
 
 	double step = candidate->getCurrentStep();
-	while (step > 0)
-	{
-			double randDistance = -1. * log(crpropa::Random::instance().rand()) * currentStepLength;
+	while (step > 0) {
+		double randDistance = -1. * log(crpropa::Random::instance().rand()) * currentStepLength;
 
-			if (step < randDistance)
-			{
-					candidate->limitNextStep(0.1 * currentStepLength);
-					return;
-			}
-			scatter(candidate, scatterCenterVelocity(candidate));
-			step -= randDistance;
+		if (step < randDistance) {
+			candidate->limitNextStep(0.1 * currentStepLength);
+			return;
+		}
+		scatter(candidate, scatterCenterVelocity(candidate));
+		step -= randDistance;
 	}
 }
 
 
-SecondOrderFermi::SecondOrderFermi(double _scatterVelocity, double stepLength, unsigned int size_of_pitchangle_table) : AbstractAccelerationModule(stepLength), scatterVelocity(_scatterVelocity)
-{
+SecondOrderFermi::SecondOrderFermi(double _scatterVelocity, double stepLength,
+                                   unsigned int size_of_pitchangle_table)
+    : AbstractAccelerationModule(stepLength),
+      scatterVelocity(_scatterVelocity) {
 	setDescription("SecondOrderFermi Acceleration");
 	angle.resize(size_of_pitchangle_table);
 	angleCDF.resize(size_of_pitchangle_table);
 
 	// have a discretized table of beamed pitch angles
-	for (unsigned int i =0; i < size_of_pitchangle_table; i++)
-	{
+	for (unsigned int i =0; i < size_of_pitchangle_table; i++) {
 		angle[i] = i * M_PI / (size_of_pitchangle_table-1);
 		angleCDF[i] = (angle[i] +scatterVelocity / crpropa::c_light * sin(angle[i])) / M_PI;
 	}
@@ -92,9 +89,9 @@ crpropa::Vector3d SecondOrderFermi::scatterCenterVelocity(crpropa::Candidate *ca
 }
 
 
-DirectedFlowOfScatterCenters::DirectedFlowOfScatterCenters(const Vector3d &scatterCenterVelocity) : __scatterVelocity(scatterCenterVelocity)
-{
-}
+DirectedFlowOfScatterCenters::DirectedFlowOfScatterCenters(
+    const Vector3d &scatterCenterVelocity)
+    : __scatterVelocity(scatterCenterVelocity) {}
 
 
 double DirectedFlowOfScatterCenters::modify(double steplength, Candidate* candidate)
@@ -104,8 +101,10 @@ double DirectedFlowOfScatterCenters::modify(double steplength, Candidate* candid
 }
 
 
-DirectedFlowScattering::DirectedFlowScattering(crpropa::Vector3d scatterCenterVelocity, double stepLength) : __scatterVelocity(scatterCenterVelocity), AbstractAccelerationModule(stepLength)
-{
+DirectedFlowScattering::DirectedFlowScattering(
+    crpropa::Vector3d scatterCenterVelocity, double stepLength)
+    : __scatterVelocity(scatterCenterVelocity),
+      AbstractAccelerationModule(stepLength) {
 
 	// In a directed field of scatter centers, the probability to encounter a
 	// scatter center depends on the direction of the candidate.
@@ -114,68 +113,72 @@ DirectedFlowScattering::DirectedFlowScattering(crpropa::Vector3d scatterCenterVe
 }
 
 
-crpropa::Vector3d DirectedFlowScattering::scatterCenterVelocity(crpropa::Candidate *candidate) const
-{ // does not depend on candidate here.
+crpropa::Vector3d DirectedFlowScattering::scatterCenterVelocity(
+    crpropa::Candidate *candidate) const { // does not depend on candidate here.
 	return __scatterVelocity;
 }
 
 
-QuasiLinearTheory::QuasiLinearTheory(double referenecEnergy, double turbulence_index, double minimumRigidity) : __referenceEnergy(referenecEnergy), __turbulenceIndex(turbulence_index), __minimumRigidity(minimumRigidity)
-{
-}
+QuasiLinearTheory::QuasiLinearTheory(double referenecEnergy,
+                                     double turbulence_index,
+                                     double minimumRigidity)
+    : __referenceEnergy(referenecEnergy), __turbulenceIndex(turbulence_index),
+      __minimumRigidity(minimumRigidity) {}
 
 
 double QuasiLinearTheory::modify(double steplength, Candidate* candidate)
 {
 	if (candidate->current.getRigidity() < __minimumRigidity)
 	{
-		return steplength * std::pow(__minimumRigidity / (__referenceEnergy / eV), 2. - __turbulenceIndex);
+		return steplength * std::pow(__minimumRigidity /
+		    (__referenceEnergy / eV), 2. - __turbulenceIndex);
 	}
 	else
 	{
-		return steplength * std::pow(candidate->current.getRigidity() / (__referenceEnergy / eV), 2. - __turbulenceIndex);
+		return steplength * std::pow(candidate->current.getRigidity() /
+		    (__referenceEnergy / eV), 2. - __turbulenceIndex);
 	}
 }
 
 
-ParticleSplitting::ParticleSplitting(Surface *surface, int crossing_threshold, int num_splits, double min_weight, std::string counterid): surface(surface),
-	crossing_threshold(crossing_threshold), num_splits(num_splits),
-	min_weight(min_weight), counterid(counterid)
-{
-};
+ParticleSplitting::ParticleSplitting(Surface *surface, int crossing_threshold,
+                                     int num_splits, double min_weight,
+                                     std::string counterid)
+    : surface(surface), crossing_threshold(crossing_threshold),
+      num_splits(num_splits), min_weight(min_weight), counterid(counterid){};
 
-void ParticleSplitting::process(Candidate *candidate) const
-{
-	const double currentDistance = surface->distance(candidate->current.getPosition());
-	const double previousDistance = surface->distance(candidate->previous.getPosition());
+void ParticleSplitting::process(Candidate *candidate) const {
+	const double currentDistance =
+	    surface->distance(candidate->current.getPosition());
+	const double previousDistance =
+	    surface->distance(candidate->previous.getPosition());
 
 	if (currentDistance * previousDistance > 0)
 		// candidate remains on the same side
 		return;
 
-  if (candidate->getWeight() < min_weight)
+	if (candidate->getWeight() < min_weight)
 		return;
 
 	int num_crossings = 1;
-  if (candidate->hasProperty(counterid))
-        num_crossings = candidate->getProperty(counterid).toInt32() + 1;
-  candidate->setProperty(counterid, num_crossings);
+	if (candidate->hasProperty(counterid))
+		num_crossings = candidate->getProperty(counterid).toInt32() + 1;
+	candidate->setProperty(counterid, num_crossings);
 
 	if (num_crossings % crossing_threshold != 0)
 		return;
 
 	candidate->setWeight(candidate->getWeight() / num_splits);
 
-  for (size_t i=1; i < num_splits; i++)
-	{
+	for (size_t i = 1; i < num_splits; i++) {
 		// No recursive split as the weights of the secondaries created
 		// before the split are not affected
-    ref_ptr<Candidate> new_candidate = candidate->clone(false);
-    new_candidate->parent = candidate;
-    uint64_t snr = Candidate::getNextSerialNumber();
-    Candidate::setNextSerialNumber(snr+1);
-    new_candidate->setSerialNumber(snr);
-    candidate->addSecondary(new_candidate);
+		ref_ptr<Candidate> new_candidate = candidate->clone(false);
+		new_candidate->parent = candidate;
+		uint64_t snr = Candidate::getNextSerialNumber();
+		Candidate::setNextSerialNumber(snr + 1);
+		new_candidate->setSerialNumber(snr);
+		candidate->addSecondary(new_candidate);
 	}
 };
 

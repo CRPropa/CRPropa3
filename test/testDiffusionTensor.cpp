8502e860278c3a6f693216172dd640cff99b3157
Julien Dörner <julien.doerner@rub.de>
2021-07-23 16:06:58 +0200

    add new tests for diffusion tensors

--- test/testDiffusionTensor.cpp
+++ test/testDiffusionTensor.cpp
@@ -1,4 +1,7 @@
 #include "crpropa/DiffusionTensor.h"
+#include "crpropa/magneticField/turbulentField/SimpleGridTurbulence.h"
+#include "crpropa/magneticField/MagneticFieldGrid.h"
+#include "crpropa/Grid.h"
 
 #include "gtest/gtest.h"
 
@@ -49,3 +52,63 @@ TEST(testQLTDiffusion, TestForEnergyScaling){
     EXPECT_NEAR(Tens-> getKappaPerpendicular2(&cand),  9.47706e25, 1e20);
 }
 
+TEST(testQLTTurbulence, testQLTTurbulence){
+    // background field
+    double B = 10;  // field strenght
+    UniformMagneticField* background = new UniformMagneticField(Vector3d(B, 0, 0));
+
+    // turbulent field
+    double b = 1;
+    unsigned int seed = 42; // random number
+    SimpleGridTurbulence* turbulentField = new SimpleGridTurbulence(SimpleTurbulenceSpectrum(b, 10*pc, 100*pc), GridProperties(Vector3d(0.), 256, 5*pc),seed);
+
+    // diffusion tensor
+    QLTTurbulent* Tens = new QLTTurbulent(background, turbulentField);
+ 
+    // check position for turbulence scaling
+    Vector3d pos1(-1*kpc, 0., -20*pc);
+    double locTurb = turbulentField -> getField(pos1).getR() / B;
+    double normTurb = turbulentField->getField(Vector3d(-8.5*kpc, 0., 0.)).getR()/B; // norm Value for the turbulence at earth
+
+    // check default values
+    EXPECT_DOUBLE_EQ(Tens ->getKappa0(), 6.1e24);
+    EXPECT_DOUBLE_EQ(Tens ->getAlphaPara(), 1./3.);
+    EXPECT_DOUBLE_EQ(Tens ->getAlphaPerp(), 1./3.);
+    EXPECT_DOUBLE_EQ(Tens ->getNormTurbulence(), normTurb);
+    
+    normTurb = turbulentField->getField(Vector3d(0.)).getR()/B;
+    Tens ->normToEarthPosition(Vector3d(0.));
+    EXPECT_DOUBLE_EQ(Tens ->getNormTurbulence(), normTurb);
+
+    // Candidate for checking
+    int id = 1000010010; // protons
+    double energy = 4*GeV; // checking at scaling energy -> no energy dependence 
+    Candidate cand(id, energy);
+    cand.current.setPosition(pos1);
+
+    // check values
+    double kappaPara = 6.1e24 * pow(locTurb/normTurb, -2);
+    double kappaPerp = 6.1e24 * pow(locTurb*normTurb, 2);
+    EXPECT_DOUBLE_EQ(Tens -> getKappaParallel(&cand), kappaPara);
+    EXPECT_DOUBLE_EQ(Tens -> getKappaPerpendicular(&cand), kappaPerp);
+    EXPECT_DOUBLE_EQ(Tens -> getKappaPerpendicular2(&cand), kappaPerp);
+
+    // check energy scaling
+    cand.current.setEnergy(4*PeV);  
+    EXPECT_DOUBLE_EQ(Tens -> getKappaParallel(&cand), 100*kappaPara);
+    EXPECT_DOUBLE_EQ(Tens -> getKappaPerpendicular(&cand), 100*kappaPerp);
+    EXPECT_DOUBLE_EQ(Tens -> getKappaPerpendicular2(&cand), 100*kappaPerp);
+
+    // check set function
+    Tens ->setKappa0(1e15);
+    EXPECT_DOUBLE_EQ(Tens ->getKappa0(), 1e15);
+    Tens ->setAlphaPara(0.3);
+    EXPECT_DOUBLE_EQ(Tens ->getAlphaPara(), 0.3);
+    Tens ->setAlphaPerp(0.7);
+    EXPECT_DOUBLE_EQ(Tens ->getAlphaPerp(), 0.7);
+    Tens ->setAlpha(0.5);
+    EXPECT_DOUBLE_EQ(Tens ->getAlphaPara(), 0.5);
+    EXPECT_DOUBLE_EQ(Tens ->getAlphaPerp(), 0.5);
+    Tens ->setNormTurbulence(0.01);
+    EXPECT_DOUBLE_EQ(Tens ->getNormTurbulence(), 0.01);
+}

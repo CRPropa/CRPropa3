cb0c4e95d64474c10d3ecc0e33711d36b5796a8c
asavelie <33399520+asavelie@users.noreply.github.com>
2021-11-04 13:49:05 +0100

    Add files via upload

--- src/magneticField/PolarizedSingleModeMagneticField.cpp
+++ src/magneticField/PolarizedSingleModeMagneticField.cpp
@@ -2,7 +2,7 @@
 
 namespace crpropa {
 
-Vector3d PolarizedSingleModeMagneticField::getField(const Vector3d &position) const {
+PolarizedSingleModeMagneticField::PolarizedSingleModeMagneticField( const double &B_0, const double &wavelength, const double &sigma, const Vector3d &r_0, const Vector3d &e_1, const Vector3d &e_2, std::string flagAmplitudeRms, std::string flagPolarizationHelicity, std::string flagMode ) {
 	if (flagMode == "elliptical") {
 		if (abs(sigma) > 1)
 			throw std::runtime_error("PolarizedSingleModeMagneticField: The value of the  polarization parameter has to lie in the range [-1;+1].");
@@ -21,45 +21,46 @@ Vector3d PolarizedSingleModeMagneticField::getField(const Vector3d &position) co
 	if (e_1.dot(e_2) != 0)
 		throw std::runtime_error("PolarizedSingleModeMagneticField: e_1 and e_2 have to be orthogonal to each other.");
 
-	Vector3d unitVector_1;
 	if (e_1.getR() == 0)
 		throw std::runtime_error("PolarizedSingleModeMagneticField: Vector e_1 cannot be zero.");
 	unitVector_1 = e_1 / e_1.getR();
 
-	Vector3d unitVector_2;
 	if (e_2.getR() == 0)
 		throw std::runtime_error("PolarizedSingleModeMagneticField: Vector e_2 cannot be zero.");
 	unitVector_2 = e_2 / e_2.getR();
 
-	Vector3d wavevector = e_2.cross(e_1);
+	wavevector = e_2.cross(e_1);
 
 	//This check is necessary if a polarization with non-orthogonal spanning vectors is desired. This is not implemented in this version, so any corresponding modifications should be made with caution.
-	//if (wavevector.getR() == 0) 
-	//	throw std::runtime_error("PolarizedSingleModeMagneticField: e_1 cannot be parallel to e_2.");
+	//if (wavevector.getR() == 0)
+	//        throw std::runtime_error("PolarizedSingleModeMagneticField: e_1 cannot be parallel to e_2.");
 
 	wavevector = wavevector / wavevector.getR();
 
 	if (wavelength == 0)
 		throw std::runtime_error("PolarizedSingleModeMagneticField: The correlation length cannot be zero.");
-	wavevector = wavevector/wavevector.getR() * 2 * M_PI / wavelength;
+	wavevector = wavevector * 2 * M_PI / wavelength;
 
 	if (!((flagPolarizationHelicity == "helicity") || (flagPolarizationHelicity == "polarization")))
 		throw std::runtime_error("PolarizedSingleModeMagneticField: Wrong value for flagPolarizationHelicity. Please choose \"polarization\" or \"helicity\".");
 	if (flagPolarizationHelicity == "helicity" && abs(sigma) != 1)
 		throw std::runtime_error("PolarizedSingleModeMagneticField: In helicity mode only the maximum helicity case (sigma = +1 or sigma = -1) may be chosen.");
 
-	double prefactor;
-
 	if (flagAmplitudeRms == "amplitude")
-		prefactor = 1;
+		B_max = B_0;
 	else if (flagAmplitudeRms == "rms")
-		prefactor = sqrt(2 / (1 + sigma * sigma));
+		B_max = sqrt(2 / (1 + sigma * sigma)) * B_0;
 	else
 		throw std::runtime_error("PolarizedSingleModeMagneticField: Wrong value for flagAmplitudeRms. Please choose \"amplitude\" or \"rms\".");
 
+	this->r_0 = r_0;
+	this->sigma = sigma;
+}
+
+Vector3d PolarizedSingleModeMagneticField::getField(const Vector3d &position) const {
 	Vector3d delta_r = position - r_0;
-  
-  return prefactor * B_0 * (unitVector_1 * cos(wavevector.dot(delta_r)) + unitVector_2 * sigma * sin(wavevector.dot(delta_r)));
+
+	return B_max * (unitVector_1 * cos(wavevector.dot(delta_r)) + unitVector_2 * sigma * sin(wavevector.dot(delta_r)));
 }
 
 } //end namespace crpropa

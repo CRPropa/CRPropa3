ea159bea58918a78568aa12b52859a54463f43c2
mertelx <lukas.merten@rub.de>
2022-07-08 15:55:03 +0200

    Update weight-setters, due to automatic weight inheritance for secondaries.

--- src/module/EMPairProduction.cpp
+++ src/module/EMPairProduction.cpp
@@ -205,14 +205,13 @@ void EMPairProduction::performInteraction(Candidate *candidate) const {
 
 	// sample random position along current step
 	Vector3d pos = random.randomInterpolatedPosition(candidate->previous.getPosition(), candidate->current.getPosition());
-	double w0 = candidate->getWeight();
 	// apply sampling
 	if (random.rand() < pow(f, thinning)) {
-		double w = w0 / pow(f, thinning);
+		double w = 1. / pow(f, thinning);
 		candidate->addSecondary(11, Ep / (1 + z), pos, w);
 	}
 	if (random.rand() < pow(1 - f, thinning)){
-		double w = w0 / pow(1 - f, thinning);
+		double w = 1. / pow(1 - f, thinning);
 		candidate->addSecondary(-11, Ee / (1 + z), pos, w);	
 	}
 }

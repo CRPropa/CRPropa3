70c634dfb075c3254cd9f5aeaf0f7ad3d4e170df
Jan-Niklas Bohnensack <jan-niklas.bohnensack@ruhr-uni-bochum.de>
2024-05-02 15:21:42 +0200

    Added names to omp critical statements

--- src/module/HDF5Output.cpp
+++ src/module/HDF5Output.cpp
@@ -248,7 +248,7 @@ void HDF5Output::close() {
 }
 
 void HDF5Output::process(Candidate* candidate) const {
-	#pragma omp critical
+	#pragma omp critical(HDF5Output_access_file)
 	{
 	if (file == -1)
 		// This is ugly, but necesary as otherwise the user has to manually open the
@@ -316,7 +316,7 @@ void HDF5Output::process(Candidate* candidate) const {
 			pos += v.copyToBuffer(&r.propertyBuffer[pos]);
 	}
 
-	#pragma omp critical
+	#pragma omp critical(HDF5Output_access_file)
 	{
 		const_cast<HDF5Output*>(this)->candidatesSinceFlush++;
 		Output::process(candidate);
